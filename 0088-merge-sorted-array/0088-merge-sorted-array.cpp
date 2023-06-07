a1a2b463cba4c89e8c2c217c31ab35623f118ab6
유승선 <yssgood@naver.com>
2023-06-07 23:18:34 +0900

    Time: 3 ms (61.65%), Space: 9.2 MB (19.84%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = 0; i < nums2.size(); ++i){
+            nums1[m] = nums2[i];
+            m++;
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
