d660c8bee9191aa3f75625ee1778a07595462dd9
유승선 <yssgood@naver.com>
2023-06-29 21:42:02 +0900

    Time: 7 ms (93.85%), Space: 13.5 MB (81.06%) - LeetHub

---
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for(int i = 0; i < nums.size()-1; i++){
+            if(i%2 == 0 && nums[i] > nums[i+1])
+                swap(nums[i],nums[i+1]); 
+            if(i%2 == 1 && nums[i] < nums[i+1])
+                swap(nums[i],nums[i+1]); 
+        }
+    }
+};
+
+
