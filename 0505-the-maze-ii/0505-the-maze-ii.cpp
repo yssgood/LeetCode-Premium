b8853d94e08ce53b5892be1d196ba04d87ade80f
유승선 <yssgood@naver.com>
2023-09-14 21:20:25 +0900

    Time: 30 ms (86.12%), Space: 20.7 MB (54.8%) - LeetHub

--- 0505-the-maze-ii/0505-the-maze-ii.cpp
+++ 0505-the-maze-ii/0505-the-maze-ii.cpp
@@ -2,49 +2,96 @@ class Solution {
     struct Ball{
         int x, y, dist; 
     };
-    vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}}; 
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
 public:
     int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-        int visited[maze.size()][maze[0].size()]; 
-        memset(visited,10000,sizeof(visited)); 
+        vector<vector<int>> visited(maze.size(), vector<int>(maze[0].size(),INT_MAX)); 
         queue<Ball> q; 
         q.push({start[0],start[1],0}); 
         visited[start[0]][start[1]] = 0; 
-
         while(!q.empty()){
-            int size = q.size();
+            int size = q.size(); 
             for(int i = 0; i < size; i++){
-                Ball ball = q.front();
+                Ball first = q.front(); 
                 q.pop(); 
-                int x = ball.x, y = ball.y, dist = ball.dist; 
+
+                int x = first.x, y = first.y, dist = first.dist; 
+
+                //if(x == destination[0] && y == destination[1]) return dist; 
 
                 for(pair<int,int>& p : dir){
-                    int nX = x, nY = y; 
-                    int nDist = 0; 
+                    int nX = x + p.first; 
+                    int nY = y + p.second; 
+                    int nDist = dist; 
                     while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] == 0){
-                        ++nDist; 
-                        nX += p.first; 
+                        nX += p.first;
                         nY += p.second; 
+                        nDist += 1; 
                     }
 
-                    
-
-                    nX -= p.first; 
+                    nX -= p.first;
                     nY -= p.second; 
-                    nDist--; 
+                    //nDist--; 
 
-                    //cout << nX << ' ' << nY << ' ' << nDist << endl; 
-                    
-                    if(visited[nX][nY] > nDist + dist){
-                        q.push({nX, nY, nDist + dist}); 
-                        visited[nX][nY] = nDist + dist; 
+                    if(visited[nX][nY] > nDist){
+                        visited[nX][nY] = nDist;
+                        //cout << nX << ' ' << nY <<  ' ' << nDist << endl; 
+                        q.push({nX,nY,nDist});
                     }
-
                 }
-
             }
         }
-
-        return visited[destination[0]][destination[1]] == 269488144 ? -1 : visited[destination[0]][destination[1]]; 
+        return visited[destination[0]][destination[1]] == INT_MAX ? -1 : visited[destination[0]][destination[1]];
     }
 };
+
+// class Solution {
+//     struct Ball{
+//         int x, y, dist; 
+//     };
+//     vector<pair<int,int>> dir = {{0,1},{1,0},{0,-1},{-1,0}}; 
+// public:
+//     int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+//         int visited[maze.size()][maze[0].size()]; 
+//         memset(visited,10000,sizeof(visited)); 
+//         queue<Ball> q; 
+//         q.push({start[0],start[1],0}); 
+//         visited[start[0]][start[1]] = 0; 
+
+//         while(!q.empty()){
+//             int size = q.size();
+//             for(int i = 0; i < size; i++){
+//                 Ball ball = q.front();
+//                 q.pop(); 
+//                 int x = ball.x, y = ball.y, dist = ball.dist; 
+
+//                 for(pair<int,int>& p : dir){
+//                     int nX = x, nY = y; 
+//                     int nDist = 0; 
+//                     while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] == 0){
+//                         ++nDist; 
+//                         nX += p.first; 
+//                         nY += p.second; 
+//                     }
+
+                    
+
+//                     nX -= p.first; 
+//                     nY -= p.second; 
+//                     nDist--; 
+
+//                     //cout << nX << ' ' << nY << ' ' << nDist << endl; 
+                    
+//                     if(visited[nX][nY] > nDist + dist){
+//                         q.push({nX, nY, nDist + dist}); 
+//                         visited[nX][nY] = nDist + dist; 
+//                     }
+
+//                 }
+
+//             }
+//         }
+
+//         return visited[destination[0]][destination[1]] == 269488144 ? -1 : visited[destination[0]][destination[1]]; 
+//     }
+// };
