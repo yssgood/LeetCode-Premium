ce5be6f621761ec321103b12a10bf0d556752a87
유승선 <yssgood@naver.com>
2024-01-29 22:06:13 +0900

    Time: 25 ms (31.88%), Space: 19.1 MB (8.44%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    
-int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-vector<vector<pair<int,int>>>g(n);
-        for(auto& i:flights) g[i[0]].push_back({i[1],i[2]});
-        vector<int>dist(n,INT_MAX);
-        dist[src]=0;
-        queue<pair<int,int>>pq;
-        pq.push({0,src});
-        k+=1;
-        while(!pq.empty() && k--){
-            int n=pq.size();
-            while(n--){
-                int d=pq.front().first,s=pq.front().second;
-                pq.pop();
-                for(auto& i:g[s]){
-                    if(dist[i.first]>d+i.second){
-                        dist[i.first]=d+i.second;
-                        pq.push({dist[i.first],i.first});
-                    } 
-                }                
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto e : flights) {
+            adj[e[0]].push_back({e[1], e[2]});
+        }
+        vector<int> stops(n, numeric_limits<int>::max());
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        // {dist_from_src_node, node, number_of_stops_from_src_node}
+        pq.push({0, src, 0});
+
+        while (!pq.empty()) {
+            auto temp = pq.top();
+            pq.pop();
+            int dist = temp[0];
+            int node = temp[1];
+            int steps = temp[2];
+            // We have already encountered a path with a lower cost and fewer stops,
+            // or the number of stops exceeds the limit.
+            if (steps > stops[node] || steps > k + 1) continue;
+            stops[node] = steps;
+            if (node == dst) return dist;
+            for (auto& [neighbor, price] : adj[node]) {
+                pq.push({dist + price, neighbor, steps + 1});
             }
         }
-        return dist[dst]==INT_MAX? -1:dist[dst];
-        
+        return -1;
     }
 };
