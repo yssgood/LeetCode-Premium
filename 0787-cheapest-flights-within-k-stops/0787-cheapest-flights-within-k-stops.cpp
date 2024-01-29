f6675fe7a78a25d58f91d350cfc654e5efe861a5
유승선 <yssgood@naver.com>
2024-01-29 21:36:35 +0900

    Time: 15 ms (84.33%), Space: 16.5 MB (24.17%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> graph(n); 
-        int INF = 987654321; 
-        for(vector<int>& v : flights){
-            int node = v[0], dest = v[1], dist = v[2]; 
-            graph[node].push_back({dest,dist}); 
-        }
-        priority_queue<vector<int>> pq; 
-        vector<int> distance(n,INF); 
-        distance[src] = 0;
-        pq.push({0,src,k+1}); //distance, curr_node, number of stops 
-        
-        while(!pq.empty()){
-            int size = pq.size();
-            for(int i = 0; i < size; i++){
-                vector<int> first = pq.top(); 
+    
+int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+vector<vector<pair<int,int>>>g(n);
+        for(auto& i:flights) g[i[0]].push_back({i[1],i[2]});
+        vector<int>dist(n,INT_MAX);
+        dist[src]=0;
+        queue<pair<int,int>>pq;
+        pq.push({0,src});
+        k+=1;
+        while(!pq.empty() && k--){
+            int n=pq.size();
+            while(n--){
+                int d=pq.front().first,s=pq.front().second;
                 pq.pop();
-                
-                int dist = first[0], curr_node = first[1], stops = first[2]; 
-                
-                if(stops > 0){
-                    for(pair<int,int>& p : graph[curr_node]){
-                        int dest = p.first, weights = p.second; 
-                        if(distance[curr_node] + weights < distance[dest]){
-                            distance[dest] = distance[curr_node] + weights;
-                            int newDist = distance[dest]; 
-                            pq.push({newDist,dest,stops-1}); 
-                        }
-                    }
-                }
+                for(auto& i:g[s]){
+                    if(dist[i.first]>d+i.second){
+                        dist[i.first]=d+i.second;
+                        pq.push({dist[i.first],i.first});
+                    } 
+                }                
             }
         }
+        return dist[dst]==INT_MAX? -1:dist[dst];
         
-        if(distance[dst] != INF) return distance[dst]; 
-        
-        return -1; 
     }
 };
