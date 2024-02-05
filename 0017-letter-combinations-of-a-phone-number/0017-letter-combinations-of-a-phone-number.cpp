370a984a73520671085c93784c3350428a0b13e1
유승선 <yssgood@naver.com>
2024-02-05 23:09:07 +0900

    Time: 4 ms (8.85%), Space: 8.3 MB (7.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,35 @@
 class Solution {
 
-public:
-    void dfs(string& digits, vector<string>& container, map<char,vector<char>>& hashMap, string& empty, int index){
+map<char, vector<char>> numMap = {{'2', {'a','b','c'}},
+                                {'3', {'d','e','f'}},
+                                {'4', {'g','h','i'}},
+                                {'5', {'j','k','l'}},
+                                {'6', {'m','n','o'}},
+                                {'7', {'p','q','r','s'}},
+                                {'8', {'t','u','v'}},
+                                {'9', {'w','x','y','z'}}};
 
-        if(empty.length() > 0 && empty.length() == digits.length()){
-            container.push_back(empty);  
+
+public:
+    void dfs(string& digits, string& empty, vector<string>& answer, int index){
+        if(empty.length() >= digits.length()){
+            answer.push_back(empty); 
             return; 
         }
 
-        for(int i = 0; i < hashMap[digits[index]].size(); i++){
-            empty += hashMap[digits[index]][i]; 
-            dfs(digits, container, hashMap, empty, index+1); 
+        for(int i = 0; i < numMap[digits[index]].size(); i++){
+            empty += numMap[digits[index]][i]; 
+            dfs(digits,empty,answer,index+1); 
             empty.pop_back(); 
         }
     }
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> container; 
-        map<char,vector<char>> hashMap = {{'2',{'a','b','c'}},{'3',{'d','e','f'}},{'4',{'g','h','i'}},{'5',{'j','k','l'}},{'6',{'m','n','o'}},
-                                            {'7',{'p','q','r', 's'}},{'8',{'t','u','v'}},{'9',{'w','x','y','z'}}}; 
-
-
+        if(digits.empty()) return {}; 
+        vector<string> answer; 
         string empty = ""; 
-        dfs(digits,container,hashMap,empty,0); 
+        dfs(digits,empty,answer,0); 
 
-
-        return container; 
+        return answer; 
     }
 };
