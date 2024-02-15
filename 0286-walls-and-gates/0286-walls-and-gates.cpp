3ba36d74f149452d7a2080d123d6e1f7374f3035
유승선 <yssgood@naver.com>
2024-02-16 00:02:07 +0900

    Time: 26 ms (66.86%), Space: 17.3 MB (45.4%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,39 +1,39 @@
 class Solution {
-    struct Gates{
-        int x, y, dist; 
-    };
-    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+struct Node{
+    int x, y, currDist; 
+}; 
+
+vector<pair<int,int>> dir = {{0,1},{0,-1}, {1,0}, {-1,0}}; 
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        queue<Gates> q; 
+        queue<Node> q; 
         for(int i = 0; i < rooms.size(); i++){
-            for(int j = 0; j < rooms[0].size(); j++){
-                if(rooms[i][j] == 0){
-                    q.push({i,j,0}); 
-                }
+            for(int j = 0; j < rooms[i].size(); j++){
+                if(rooms[i][j] == 0) q.push({i,j,0}); 
             }
         }
 
         while(!q.empty()){
             int size = q.size(); 
+
             for(int i = 0; i < size; i++){
-                Gates first = q.front(); 
+                Node node = q.front();
                 q.pop(); 
 
-                int x = first.x, y = first.y, dist = first.dist; 
+                int x = node.x, y = node.y, currDist = node.currDist; 
 
-                for(pair<int,int> p : dir){
+                for(pair<int,int>& p : dir){
                     int nX = x + p.first;
                     int nY = y + p.second; 
 
-                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size() || rooms[nX][nY] == -1) continue; 
+                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size() || rooms[nX][nY] == -1) continue;
 
-                    if(rooms[nX][nY] > dist + 1){
-                        rooms[nX][nY] = dist + 1; 
-                        q.push({nX,nY,dist+1}); 
+                    if(rooms[nX][nY] > currDist + 1){
+                        rooms[nX][nY] = currDist + 1; 
+                        q.push({nX, nY, currDist + 1}); 
                     }
                 }
             }
-        }
+        }     
     }
 };
