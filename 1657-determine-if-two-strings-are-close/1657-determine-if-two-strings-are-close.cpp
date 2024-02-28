e78f63cfbb9a3c3c92f6826f4cf48f610c73021d
유승선 <yssgood@naver.com>
2024-02-28 21:20:18 +0900

    Time: 67 ms (70.59%), Space: 21.9 MB (8.58%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.length() != word2.length()) return false; 
-        map<char,int> map1,map2; 
-
-        for(int i = 0; i < word1.length(); i++){
-            map1[word1[i]]++; 
-            map2[word2[i]]++; 
+        if (word1.size() != word2.size()) return false;
+        vector<int> word1Map(26, 0);
+        vector<int> word2Map(26, 0);
+        for (auto c : word1) {
+            word1Map[c - 'a']++;
         }
-
-        for(auto& it : map1){
-            if(!map2.count(it.first)) return false; 
+        for (auto c : word2) {
+            word2Map[c - 'a']++;
         }
-        
-        vector<int> v1, v2; 
-        for(auto& it : map1){
-            v1.push_back(it.second); 
-            v2.push_back(map2[it.first]); 
+        for (int i = 0; i < 26; i++) {
+            if ((word1Map[i] == 0 && word2Map[i] > 0) ||
+                (word2Map[i] == 0 && word1Map[i] > 0)) {
+                return false;
+            }
         }
-
-        sort(v1.begin(),v1.end()); 
-        sort(v2.begin(),v2.end()); 
-
-
-        return (v1 == v2); 
+        sort(word1Map.begin(), word1Map.end());
+        sort(word2Map.begin(), word2Map.end());
+        return (word1Map == word2Map);
     }
 };
