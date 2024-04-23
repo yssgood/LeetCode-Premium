8056a1304977def67260d1b65e598014c5dc83a8
유승선 <yssgood@naver.com>
2024-04-23 18:09:29 +0900

    Time: 0 ms (100%), Space: 9.4 MB (5.04%) - LeetHub

--- 1427-perform-string-shifts/1427-perform-string-shifts.cpp
+++ 1427-perform-string-shifts/1427-perform-string-shifts.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     string stringShift(string s, vector<vector<int>>& shift) {
         for(vector<int>& v : shift){
-            int dir = v[0], amount = v[1]; 
-            if(dir == 0) s = s.substr(amount % s.length()) + s.substr(0,amount % s.length()); 
-            if(dir == 1) s = s.substr(s.length() - (amount % s.length())) + s.substr(0,s.length() - amount % s.length()); 
-            //cout << s << endl; 
+            int dir = v[0], steps = v[1] % s.length(); 
+            if(dir == 0){
+                s = s.substr(steps) + s.substr(0,steps); 
+            } else{
+                s = s.substr(s.length() - steps) + s.substr(0, s.length() - steps); 
+            }
         }
+
         return s; 
     }
 };
