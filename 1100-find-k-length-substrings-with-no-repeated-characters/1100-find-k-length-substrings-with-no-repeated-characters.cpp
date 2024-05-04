018fdba5398358ff24ef20fc7827823d5220c2e0
유승선 <yssgood@naver.com>
2024-05-04 10:24:32 +0900

    Time: 16 ms (12.64%), Space: 12.9 MB (5.17%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -6,21 +6,16 @@ public:
         
         while(end < s.length()){
             if(hashMap.count(s[end])){
-                //do something 
                 while(start <= hashMap[s[end]]){
                     hashMap.erase(s[start]); 
                     start++; 
                 }
-                //start = hashMap[s[end]]; 
-                //start++; 
                 hashMap[s[end]] = end; 
-                //cout << end << ' ' << start << ' ' << endl; 
             } else{
                 hashMap[s[end]] = end; 
             }
 
             if(end - start + 1 == k){
-                //cout << end << ' ' << start << ' ' << endl; 
                 answer++;
                 hashMap.erase(s[start]);  
                 start++; 
