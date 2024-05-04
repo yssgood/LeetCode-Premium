a7a87cd31806a9e712c373814016eec8bcce55eb
유승선 <yssgood@naver.com>
2024-05-04 09:36:52 +0900

    Time: 18 ms (8.23%), Space: 11.3 MB (6.65%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
         map<char,int> hashMap; 
-        int start = 0, end = 0, answer = 0; 
+        int answer = INT_MIN, start = 0, end = 0; 
 
         while(end < s.length()){
             hashMap[s[end]]++; 
-            if(hashMap.size() <= k) answer = max(answer, end - start + 1);
+
             while(hashMap.size() > k){
-                hashMap[s[start]]--;
+                hashMap[s[start]]--; 
                 if(hashMap[s[start]] == 0) hashMap.erase(s[start]); 
                 start++; 
-            } 
+            }
+
+            if(hashMap.size() <= k){
+                answer = max(answer, end - start + 1); 
+            }
             end++; 
         }
+
 
         return answer; 
     }
