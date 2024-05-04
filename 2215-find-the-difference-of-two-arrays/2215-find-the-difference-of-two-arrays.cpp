7ba3621e4a13fde46bd2110753713d668f44ff11
유승선 <yssgood@naver.com>
2024-05-04 15:30:29 +0900

    Time: 37 ms (70.62%), Space: 39.9 MB (17.45%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // Returns the elements in the first arg nums1 that don't exist in the second arg nums2.
+    vector<int> getElementsOnlyInFirstList(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> onlyInNums1;
+        unordered_set<int> existsInNums2;
+        for (int num : nums2) {
+            existsInNums2.insert(num);
+        }
+        
+        for (int num : nums1) {
+            if (existsInNums2.find(num) == existsInNums2.end()) {
+                onlyInNums1.insert(num);
+            }
+        }
+
+        return vector<int> (onlyInNums1.begin(), onlyInNums1.end());
+    }
+    
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+        return {getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1)};
+    }
+};
