3304c024d5f4c0a4fa7419f7f87a87ec052a03c8
유승선 <yssgood@naver.com>
2024-05-11 20:30:46 +0900

    Time: 181 ms (35.27%), Space: 99 MB (22.12%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int,int>> v;
-        int n = nums2.size(); 
-
-        for(int i = 0; i < n; i++){
-            v.push_back({nums2[i],i}); 
+        vector<pair<int,int>>v;
+        int n=nums2.size();
+        // to track indexs after sorting
+        for(int i=0;i<n;i++){
+            v.push_back({nums2[i],i});
         }
-
-        sort(v.rbegin(), v.rend()); 
-         
-        long long sum = 0; 
-        long long answer = 0; 
-
-        priority_queue<int, vector<int>, greater<int>> pq; 
-        for(int i = 0; i < v.size(); i++){
-            pair<int,int> p = v[i]; 
-            sum += nums1[p.second]; 
-            pq.push(nums1[p.second]); 
-            if(pq.size() == k){
-                answer = max(answer, sum * p.first); 
-                sum -= pq.top(); 
-                pq.pop(); 
+        sort(v.rbegin(),v.rend());
+        long long sum=0;
+        long long maxi=0;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<v.size();i++){
+            auto &[a,b]=v[i];
+            sum+=nums1[b];
+            pq.push(nums1[b]);
+            //update maxi if the heap size is k
+            if(pq.size()==k)maxi=max(maxi,sum*a);       
+            //maintaining the k-1 maximum element
+            if(pq.size()>k-1){
+                sum-=pq.top();
+                pq.pop();
             }
-            // if(pq.size()  > k -1){
-            //     sum -= pq.top();
-            //     pq.pop(); 
-            // }
         }
-
-        return answer; 
+        return maxi;
     }
 };
