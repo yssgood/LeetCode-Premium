c78171011464b04d66b419f0bf6a95a9472c90e7
유승선 <yssgood@naver.com>
2024-05-19 19:46:28 +0900

    Time: 30 ms (44.91%), Space: 23.9 MB (65.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> answer; 
+        map<string,vector<string>> hashMap; 
+        for(string& s : strs){
+            string tmp = s; 
+            sort(tmp.begin(), tmp.end()); 
+            if(hashMap.count(tmp)){
+                hashMap[tmp].push_back(s); 
+            } else{
+                hashMap[tmp].push_back(s); 
+            }
+        }
+
+        for(auto& it : hashMap){
+            answer.push_back(it.second); 
+        }
+
+
+
+        return answer; 
+    }
+};
