c3d59d1567185a85188229ce2ea7de27e82f4b80
유승선 <yssgood@naver.com>
2024-05-21 22:45:21 +0900

    Time: 119 ms (34.2%), Space: 27.4 MB (31.19%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,39 +1,49 @@
 class Solution {
-    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
-    int visited[101][101]; 
-struct Node{
-    int x, y,dist;
-};
 public:
+    struct Node{
+        int x, y, dist; 
+    };
+    struct compare{
+        bool operator()(Node& a, Node &b){
+            return a.dist > b.dist; 
+        }
+    };
+    vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}}; 
     int minimumEffortPath(vector<vector<int>>& heights) {
-        memset(visited,9876543,sizeof(visited)); 
-        queue<Node> q; 
-        q.push({0,0,INT_MIN}); 
-        visited[0][0] = 0; 
-        while(!q.empty()){
-            int size = q.size(); 
+        priority_queue<Node, vector<Node>, compare> pq; 
+        bool visited[101][101]; 
+        memset(visited,false,sizeof(visited)); 
+        pq.push({0,0,0});  
+        visited[0][0] = true;  
+        while(!pq.empty()){
+            int size = pq.size(); 
+
             for(int i = 0; i < size; i++){
-                Node node = q.front(); 
-                q.pop();
+                Node node = pq.top();
+                pq.pop(); 
+
                 int x = node.x, y = node.y, dist = node.dist; 
-                
-                
-                
-                for(pair<int,int>& p : dir){
-                    int nX = x + p.first;
-                    int nY = y + p.second;
-                    
-                    if(nX < 0 || nX >= heights.size() || nY < 0 || nY >= heights[0].size()) continue; 
-                    
-                    int nDist = max(dist,abs(heights[x][y] - heights[nX][nY])); 
-                    if(visited[nX][nY] > nDist){
-                        visited[nX][nY] = nDist; 
-                        q.push({nX,nY,nDist}); 
-                    }
+                //cout << x << ' ' << y << ' ' << dist << endl; 
+
+                if(x == heights.size()-1 && y == heights[0].size()-1){
+                    return dist; 
                 }
+
+                for(pair<int,int>& p : dir){
+                    int nX = x + p.first; 
+                    int nY = y + p.second; 
+                    
+                    if(nX < 0 || nY < 0 || nX >= heights.size() || nY >= heights[0].size() || visited[nX][nY]) continue; 
+
+                    int nDist = abs(heights[nX][nY] - heights[x][y]); 
+                    //visited[nX][nY] = true; 
+                    pq.push({nX,nY,max(dist,nDist)}); 
+                }
+                visited[x][y] = true; 
+                
             }
         }
 
-        return visited[heights.size()-1][heights[0].size()-1]; 
+        return -1; 
     }
 };
