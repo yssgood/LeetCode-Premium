23b96dda6b4cd643c7d2caed3c26804a32ceb0da
유승선 <yssgood@naver.com>
2024-05-22 16:21:55 +0900

    Time: 40 ms (78.32%), Space: 24.5 MB (33.94%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,43 +1,44 @@
 class Solution {
-
-struct Node{
-    int x, y, dist; 
-};
-vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
 public:
+    struct Node{
+        int x, y, dist; 
+    };
+    bool visited[101][101]; 
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};  
     int maxDistance(vector<vector<int>>& grid) {
+        memset(visited, false, sizeof(visited)); 
         int answer = -1; 
         queue<Node> q; 
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false)); 
         for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[i].size(); j++){
+            for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 1){
-                    q.push({i,j,0}); 
+                    q.push({i,j,0});
                     visited[i][j] = true; 
                 }
             }
         }
 
         while(!q.empty()){
-            int size = q.size();
+            int size = q.size(); 
             for(int i = 0; i < size; i++){
-                Node node = q.front();
+                Node node = q.front(); 
                 q.pop(); 
+
                 int x = node.x, y = node.y, dist = node.dist; 
+
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first; 
                     int nY = y + p.second; 
 
                     if(nX < 0 || nY < 0 || nX >= grid.size() || nY >= grid[0].size() || visited[nX][nY]) continue; 
 
-                    visited[nX][nY] = true;
-                    q.push({nX,nY,dist+1}); 
-                    answer = max(answer, dist+1); 
+                    visited[nX][nY] = true; 
+                    answer = max(answer, dist + 1); 
+                    q.push({nX, nY, dist +1}); 
                 }
             }
         }
 
         return answer; 
-        
     }
 };
