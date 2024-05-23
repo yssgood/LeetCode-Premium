de2a694823b4cea595f078350ecbbb1bb7baa9c8
유승선 <yssgood@naver.com>
2024-05-23 23:08:34 +0900

    Time: 30 ms (39.11%), Space: 40.2 MB (20.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> answer(nums.size(), 0); 
-        answer[0] = 1; 
-        for(int i = 1; i < nums.size(); i++){
-            answer[i] += answer[i-1] * nums[i-1]; 
+        vector<int> prefix(nums.size()+1,1);
+        vector<int> suffix(nums.size()+1,1); 
+        for(int i = 1; i <= nums.size(); i++){
+            prefix[i] = prefix[i-1] * nums[i-1]; 
         }
 
-        int suffix = 1; 
-        for(int i =  nums.size()-1; i >= 0; i--){
-            answer[i] = suffix * answer[i];  
-            suffix *= nums[i]; 
-        }
+        for(int i = nums.size()-1; i >= 0; i--){
+            suffix[i] = suffix[i+1] * nums[i]; 
+        }        
 
+        vector<int> answer(nums.size(),0); 
+        // for(int i = 1; i < prefix.size(); i++){
+        //     cout << prefix[i] << ' '; 
+        // }
+        // cout << endl; 
+        // for(int i = 0; i <= nums.size()-1; i++){
+        //     cout << suffix[i] << ' '; 
+        // }        
+
+        for(int i = 0; i < nums.size(); i++){
+            int pre = 1; 
+            int suf = 1; 
+            if(i-1 >= 0) pre = prefix[i + 1 -1]; 
+            if(i + 1 < nums.size()) suf = suffix[i+1]; 
+
+            answer[i] = pre * suf; 
+        }
         return answer; 
     }
 };
