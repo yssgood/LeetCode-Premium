d3422339845c9eb9f8d427516b19f1654a9a25c0
유승선 <yssgood@naver.com>
2024-05-24 21:58:37 +0900

    Time: 5 ms (22.14%), Space: 9.4 MB (5.34%) - LeetHub

--- 1427-perform-string-shifts/1427-perform-string-shifts.cpp
+++ 1427-perform-string-shifts/1427-perform-string-shifts.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     string stringShift(string s, vector<vector<int>>& shift) {
+        string answer = ""; 
         for(vector<int>& v : shift){
-            int dir = v[0], steps = v[1] % s.length(); 
+            int dir = v[0], amt = v[1] % s.length(); 
             if(dir == 0){
-                s = s.substr(steps) + s.substr(0,steps); 
+                s = s.substr(amt) + s.substr(0,amt); 
+                //cout << s << ' '; 
             } else{
-                s = s.substr(s.length() - steps) + s.substr(0, s.length() - steps); 
-            }
+                s = s.substr(s.length() - amt) + s.substr(0,s.length() - amt); 
+            }  
         }
 
         return s; 
