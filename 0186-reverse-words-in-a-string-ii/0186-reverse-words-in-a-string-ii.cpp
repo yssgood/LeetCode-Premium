34c01307b5df6c6bbb3e02cfaff8ace0102157a6
유승선 <yssgood@naver.com>
2024-05-24 23:00:48 +0900

    Time: 14 ms (78.95%), Space: 19.8 MB (27.49%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.cpp
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     void reverseWords(vector<char>& s) {
-        reverse(s.begin(),s.end()); 
         int start = 0, end = 0; 
-        bool flag = false; 
         while(end < s.size()){
-            
             if(s[end] == ' '){
-                flag = true; 
-                int tmpStart = start;
                 int tmpEnd = end - 1; 
-                while(tmpStart < tmpEnd){
-                    swap(s[tmpStart], s[tmpEnd]); 
-                    tmpStart++;
-                    tmpEnd--;
+                
+                while(start < tmpEnd){
+                    //cout << s[start] << ' ' << s[tmpEnd]; 
+                    swap(s[start], s[tmpEnd]); 
+                    start++; 
+                    tmpEnd--; 
                 }
 
                 start = end + 1; 
+
             }
 
+            if(end == s.size()-1){
+                int tmpEnd = end; 
+
+                while(start < tmpEnd){
+                    swap(s[start], s[tmpEnd]); 
+                    start++;
+                    tmpEnd--; 
+                }
+            }
 
             end++; 
         }
 
-        if(start > 0){
-            int tmpStart = start;
-            int tmpEnd = end - 1; 
-            while(tmpStart < tmpEnd){
-                swap(s[tmpStart], s[tmpEnd]); 
-                tmpStart++;
-                tmpEnd--;
-            }
-        } else{
-            reverse(s.begin(), s.end()); 
-        }
+        reverse(s.begin(), s.end()); 
     }
 };
