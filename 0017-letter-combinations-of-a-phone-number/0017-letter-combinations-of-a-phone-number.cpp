e06bd6869226f5585c77f4c8ce16716a4fda7483
유승선 <yssgood@naver.com>
2024-05-25 20:40:42 +0900

    Time: 0 ms (100%), Space: 8.3 MB (29.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,32 @@
 class Solution {
-
-map<char, vector<char>> numMap = {{'2', {'a','b','c'}},
-                                {'3', {'d','e','f'}},
-                                {'4', {'g','h','i'}},
-                                {'5', {'j','k','l'}},
-                                {'6', {'m','n','o'}},
-                                {'7', {'p','q','r','s'}},
-                                {'8', {'t','u','v'}},
-                                {'9', {'w','x','y','z'}}};
-
-
 public:
-    void dfs(string& digits, string& empty, vector<string>& answer, int index){
-        if(empty.length() >= digits.length()){
-            answer.push_back(empty); 
+    map<char, vector<char>> hashMap = {{'2', {'a','b','c'}}, 
+                                    {'3', {'d','e','f'}}, 
+                                    {'4', {'g','h','i'}}, 
+                                    {'5', {'j','k','l'}}, 
+                                    {'6', {'m','n','o'}}, 
+                                    {'7', {'p','q','r','s'}}, 
+                                    {'8', {'t','u','v'}}, 
+                                    {'9', {'w','x','y','z'}}};
+
+    void dfs(vector<string>& answer, string& digits, string& tmp, int index){
+        if(tmp.length() >= digits.length()){
+            answer.push_back(tmp);
             return; 
         }
 
-        for(int i = 0; i < numMap[digits[index]].size(); i++){
-            empty += numMap[digits[index]][i]; 
-            dfs(digits,empty,answer,index+1); 
-            empty.pop_back(); 
+        for(int i = 0; i < hashMap[digits[index]].size(); i++){
+            tmp += hashMap[digits[index]][i]; 
+            dfs(answer,digits,tmp,index+1); 
+            tmp.pop_back(); 
         }
     }
-public:
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {}; 
         vector<string> answer; 
-        string empty = ""; 
-        dfs(digits,empty,answer,0); 
+        string tmp = ""; 
+        
+        dfs(answer,digits, tmp, 0); 
 
         return answer; 
     }
