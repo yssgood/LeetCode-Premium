16942e5b36d0bac1e46bae22c24e5ebaed7c1ea2
유승선 <yssgood@naver.com>
2024-05-25 22:19:55 +0900

    Time: 18 ms (6.16%), Space: 12.9 MB (18.49%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.cpp
+++ 0723-candy-crush/0723-candy-crush.cpp
@@ -1,64 +1,67 @@
 class Solution {
-
-int N,M; 
 public:
-    bool check(int i, int j, int flag, vector<vector<int>>& board){
-        if(flag == 0) return (i + 1 < N && i + 2 < N && board[i][j] != 0 && board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j]); 
-        return (j + 1 < M && j + 2 < M && board[i][j] != 0 && board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2]); 
-    }
+    void fall(vector<vector<int>>& tmpBoard, int x, int y){
 
-    void dropTop(int x, int y, vector<vector<int>>& board){
-        for(int i = x; i >= 0; i--){
-            //board[i][y] = 0; 
-            if(i - 1 >= 0){
-                board[i][y] = board[i-1][y];
-                board[i-1][y] = 0; 
-            } else{
-                board[i][y] = 0; 
-            }
+        for(int i = x; i > 0; i--){
+            int save = tmpBoard[i-1][y]; 
+            tmpBoard[i][y] = save; 
+            tmpBoard[i-1][y] = 0; 
         }
     }
-
-public:
     vector<vector<int>> candyCrush(vector<vector<int>>& board) {
-        //vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(),false)); 
-        bool visited[51][51]; 
-        N = board.size(), M = board[0].size(); 
         bool flag = true; 
-
         while(flag){
             flag = false; 
-
-            memset(visited,false,sizeof(visited)); 
+            vector<vector<int>> tmpBoard = board; 
             for(int i = 0; i < board.size(); i++){
-                for(int j = 0; j < board[0].size(); j++){
-                    if(check(i,j,0, board)){
-                        visited[i][j] = true;
-                        visited[i+1][j] = true; 
-                        visited[i+2][j] = true; 
-                    }
-
-                    if(check(i,j,1, board)){
-                        visited[i][j] = true;
-                        visited[i][j+1] = true;
-                        visited[i][j+2] = true; 
-                    }
-                }
-            }
-
-            for(int i = 0; i < board.size(); i++){
-                for(int j = 0; j < board[i].size(); j++){
-                    if(visited[i][j]){
+                for(int j = 0; j < board[0].size()-2; j++){
+                    int left = board[i][j], middle = board[i][j+1], right = board[i][j+2];  
+                    if(left != 0 && left == middle && middle == right){
+                        tmpBoard[i][j] = 0; 
+                        tmpBoard[i][j+1] = 0; 
+                        tmpBoard[i][j+2] = 0; 
                         flag = true; 
-                        dropTop(i,j,board); 
                     }
                 }
             }
 
-    
+            for(int i = 0; i < board.size()-2; i++){
+                for(int j = 0; j < board[0].size(); j++){
+                    int left = board[i][j], mid_bottom = board[i+1][j], bottom = board[i+2][j]; 
+                    if(left != 0 && left == mid_bottom && mid_bottom == bottom){
+                        tmpBoard[i][j] = 0;
+                        tmpBoard[i+1][j] = 0;
+                        tmpBoard[i+2][j] = 0;
+                        flag = true; 
+                    }
+                }
+            }
 
+            for(int i = 1; i < board.size(); i++){
+                for(int j = 0; j < board[0].size(); j++){
+                    if(tmpBoard[i][j] == 0){
+                        fall(tmpBoard,i,j); 
+                    }
+                }
+            }
+
+
+
+            board = tmpBoard; 
+            if(!flag) break; 
         }
+
         return board; 
     }
 };
 
+//[[110,0,0,0,0],
+// [210,0,0,113,114],
+// [310,0,0,213,214],
+// [410,0,112,313,314],
+// [5,5,5,5,414],
+// [610,211,3,3,3],
+// [710,311,412,613,614],
+// [810,411,512,713,714],
+// [1,1,1,1,1],
+// [4,4,4,4,1014]]...
