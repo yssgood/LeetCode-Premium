288cab88616b03248080559303af8f2194cd77d5
유승선 <yssgood@naver.com>
2024-05-25 14:33:01 +0900

    Time: 86 ms (72.65%), Space: 23.6 MB (68.7%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,42 +4,47 @@ public:
         int x,y, dist; 
     };
 
+    struct Compare{
+        bool operator()(Node& a, Node& b){
+            return a.dist > b.dist; 
+        }
+    };
+
     vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}}; 
     int minimumEffortPath(vector<vector<int>>& heights) {
         vector<vector<int>> visited(heights.size(), vector<int>(heights[0].size(), INT_MAX)); 
-        queue<Node> q; 
-        q.push({0,0,0}); 
+        priority_queue<Node, vector<Node>, Compare> pq; 
+        pq.push({0,0,0}); 
         visited[0][0] = 0; 
-        
-        while(!q.empty()){
-            int size = q.size(); 
+
+        while(!pq.empty()){
+            int size = pq.size(); 
             for(int i = 0; i < size; i++){
-                Node node = q.front();
-                q.pop(); 
+                Node node = pq.top(); 
+                pq.pop(); 
+
 
                 int x = node.x, y = node.y, dist = node.dist; 
 
                 if(x == heights.size()-1 && y == heights[0].size()-1){
-                    int curr = visited[heights.size()-1][heights[0].size()-1];
-                    visited[heights.size()-1][heights[0].size()-1] = min(curr,dist); 
+                    return dist; 
                 }
 
                 for(pair<int,int>& p : dir){
-                    int nX = x + p.first;
+                    int nX = x + p.first; 
                     int nY = y + p.second; 
 
                     if(nX < 0 || nY < 0 || nX >= heights.size() || nY >= heights[0].size()) continue; 
 
-                    int next_max = max(dist, abs(heights[nX][nY] - heights[x][y]));  
+                    int new_dist = max(dist, abs(heights[nX][nY] - heights[x][y])); 
 
-                    if(visited[nX][nY] > next_max){
-                        visited[nX][nY] = next_max; 
-                        q.push({nX,nY,next_max}); 
+                    if(visited[nX][nY] > new_dist){
+                        visited[nX][nY] = new_dist; 
+                        pq.push({nX,nY,new_dist}); 
                     }
                 }
             }
         }
-
-        return visited[heights.size()-1][heights[0].size()-1]; 
+        return -1; 
     }
 };
