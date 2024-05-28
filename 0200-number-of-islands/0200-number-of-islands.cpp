b1c954909ee3549d69f0ebe6443392d257a7e8d9
유승선 <yssgood@naver.com>
2024-05-28 20:34:31 +0900

    Time: 29 ms (47.58%), Space: 17 MB (30.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1') return; 
-
-        grid[i][j] = '0'; 
-
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1); 
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int answer = 0; 
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[i].size(); j++){
-                if(grid[i][j] == '1'){
-                    answer++;
-                    dfs(grid,i,j); 
-                }
+    queue<pair<int,int>> Q;
+    int visited[303][303];
+    int dy[4] = {1,0,-1,0};
+    int dx[4] = {0,1,0,-1};
+    int maxH, maxW;
+    
+    void bfs(int i, int j, vector<vector<char>>& grid){
+        Q.push({i,j});
+        int x, y, xx, yy;
+        while(!Q.empty()){
+            y = Q.front().first; x = Q.front().second; Q.pop();
+            
+            for(int i=0; i<4; i++){
+                yy = y+dy[i]; xx = x+dx[i];
+                if(yy<0||yy>=maxH||xx<0||xx>=maxW) continue;
+                if(visited[yy][xx] == 1 || grid[yy][xx] == '0') continue;
+                
+                visited[yy][xx] = 1;
+                Q.push({yy,xx});
             }
         }
-        return answer; 
+        return;
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int answer =0;
+        maxH = grid.size();
+        maxW = grid[0].size();
+        
+        for(int i=0; i<303; i++){
+            for(int j=0; j<303; j++){
+                visited[i][j] = 0;
+            }
+        }
+        
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[i].size(); j++){
+                if(grid[i][j] == '1' && visited[i][j]==0){
+                    bfs(i,j, grid);
+                    answer++;
+                }        
+            }
+        }
+        return answer;
     }
 };
