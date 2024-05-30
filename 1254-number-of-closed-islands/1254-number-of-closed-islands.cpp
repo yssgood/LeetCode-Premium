35bbe466559bd9a1ff4375aa1ac37a95855f0f8b
유승선 <yssgood@naver.com>
2024-05-30 19:20:45 +0900

    Time: 7 ms (86%), Space: 11.8 MB (92.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,11 +1,11 @@
 class Solution {
-    vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}}; 
-
 public:
     bool dfs(vector<vector<int>>& grid, int i, int j){
         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return false; 
         if(grid[i][j] == 1) return true; 
-        
+
+        // grid[i][j] = 1; 
+        // bool flag = (dfs(grid,i+1,j) && dfs(grid,i-1,j) && dfs(grid,i,j+1) && dfs(grid,i,j-1)); 
         grid[i][j] = 1; 
         bool a = dfs(grid,i+1,j); 
         bool b = dfs(grid,i-1,j); 
@@ -13,17 +13,50 @@ public:
         bool d = dfs(grid,i,j-1); 
 
         return a && b && c && d;  
+
+
+        // return flag; 
     }
-public:
     int closedIsland(vector<vector<int>>& grid) {
-        int count = 0; 
+        int answer = 0; 
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 0 && dfs(grid,i,j)){
-                    count++; 
+                    answer++; 
                 }
             }
         }
-        return count; 
+
+        return answer; 
     }
 };
+
+// class Solution {
+//     vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}}; 
+
+// public:
+//     bool dfs(vector<vector<int>>& grid, int i, int j){
+//         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return false; 
+//         if(grid[i][j] == 1) return true; 
+        
+//         grid[i][j] = 1; 
+//         bool a = dfs(grid,i+1,j); 
+//         bool b = dfs(grid,i-1,j); 
+//         bool c = dfs(grid,i,j+1); 
+//         bool d = dfs(grid,i,j-1); 
+
+//         return a && b && c && d;  
+//     }
+// public:
+//     int closedIsland(vector<vector<int>>& grid) {
+//         int count = 0; 
+//         for(int i = 0; i < grid.size(); i++){
+//             for(int j = 0; j < grid[0].size(); j++){
+//                 if(grid[i][j] == 0 && dfs(grid,i,j)){
+//                     count++; 
+//                 }
+//             }
+//         }
+//         return count; 
+//     }
+// };
