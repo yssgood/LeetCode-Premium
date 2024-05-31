20df1a85b5faf5b26bf8a621351f21a2533c0b7d
유승선 <yssgood@naver.com>
2024-05-31 22:56:15 +0900

    Time: 8 ms (74.88%), Space: 16.4 MB (41.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //for 
-        //...for
-        //.......if(0) for ...
-        //................for......
-
-        //row map 
-        //col map 
-
-        //rowmap => 1 
-        //colmap => 1 
-
-        //{1,1} 
-        //{0,1} / {1,0} / {1,2}
-        //{2,0} / {2,1} 
-
-
-        map<int,bool> rowMap, colMap; 
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(matrix[i][j] == 0){
-                    rowMap[i] = true; 
-                    colMap[j] = true; 
+        bool isCol = false;
+        int R = matrix.size();
+        int C = matrix[0].size();
+        for (int i = 0; i < R; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(rowMap.count(i) || colMap.count(j)){
-                    matrix[i][j] = 0; 
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j < C; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < C; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (isCol) {
+            for (int i = 0; i < R; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
