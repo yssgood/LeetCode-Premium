abae303545255553999061f436c0d889d6911fd3
유승선 <yssgood@naver.com>
2024-06-01 00:26:52 +0900

    Time: 273 ms (29.65%), Space: 75 MB (15.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int answer = 0; 
         map<int,int> hashMap; 
-        
-        for(int n : nums){
-            if(!hashMap.count(n)) hashMap[n]++; 
+        for(int i = 0; i < nums.size(); i++){
+            if(!hashMap.count(nums[i])) hashMap[nums[i]]++; 
         }
 
         for(auto& it : hashMap){
-            if(hashMap.count(it.first - 1)){
-                hashMap[it.first]+=hashMap[it.first-1]; 
+            if(hashMap.count(it.first-1)){
+                hashMap[it.first] += hashMap[it.first - 1]; 
             }
         }
 
+        int answer = 0; 
         for(auto& it : hashMap){
-            answer = max(answer, it.second); 
+            answer = max(answer,it.second); 
         }
 
-        
-        
         return answer; 
     }
 };
+
+
