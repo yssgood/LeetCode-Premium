6cf4b2e5fd15ddfb2b95ff007af7560c3261f3ba
유승선 <yssgood@naver.com>
2024-06-01 22:34:22 +0900

    Time: 53 ms (90.18%), Space: 27.2 MB (79.79%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> answer; 
-        set<vector<int>> set; 
-        sort(nums.begin(), nums.end()); 
-        
-        for(int i = 0; i < nums.size() - 2; i++){
-            int start = i + 1;  
-            int end = nums.size()-1; 
-            while(start < end){
-                int res = nums[i] + nums[start] + nums[end];
-                if(res == 0){
-                    //answer.push_back({nums[i], nums[start], nums[end]}); 
-                    set.insert({nums[i], nums[start], nums[end]});
-                    start++; 
-                    end--;
-                    //break; 
-                }
-
-                if(res > 0) end--; 
-                if(res < 0) start++; 
-
-
+        sort(begin(nums), end(nums));
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
+            }
+        return res;
+    }
+    void twoSumII(vector<int>& nums, int i, vector<vector<int>>& res) {
+        int lo = i + 1, hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.push_back({nums[i], nums[lo++], nums[hi--]});
+                while (lo < hi && nums[lo] == nums[lo - 1]) ++lo;
             }
         }
-        for(auto& it : set){
-            answer.push_back(it); 
-        }
-        return answer; 
     }
 };
