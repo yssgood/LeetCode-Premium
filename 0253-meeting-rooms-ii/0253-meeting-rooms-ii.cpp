fe215994d199ff8c71bb3c4c10cdc43ae689e3e4
유승선 <yssgood@naver.com>
2024-06-02 21:47:36 +0900

    Time: 17 ms (26.28%), Space: 15.1 MB (97.86%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        priority_queue<int, vector<int>, greater<int>> pq; 
-        sort(intervals.begin(), intervals.end()); 
-
+        if(intervals.size() < 2) return 1; 
+        int answer = 1; 
+        sort(intervals.begin(),intervals.end()); 
+        priority_queue<int,vector<int>,greater<int>> pq; 
         pq.push(intervals[0][1]); 
 
         for(int i = 1; i < intervals.size(); i++){
-            int currTime = intervals[i][0]; 
-            int currTimeEnd = intervals[i][1]; 
-            int prevTime = pq.top(); 
-            
-            if(currTime >= prevTime){
-                pq.pop(); //if the next start time is bigger than the prevTime, we can use that room immediately
+            int next = intervals[i][0], nextStart = intervals[i][1]; 
+            if(pq.top() > next){
+                pq.push(nextStart); 
+            } else{
+                pq.pop(); 
+                pq.push(nextStart); 
             }
-            pq.push(currTimeEnd); 
         }
-
+        
         return pq.size(); 
     }
 };
