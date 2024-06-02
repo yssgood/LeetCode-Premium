343320105096263002d41c49ca43000d9b7fe094
유승선 <yssgood@naver.com>
2024-06-02 23:20:37 +0900

    Time: 475 ms (12.29%), Space: 53.7 MB (7.4%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> visited(nums.size(),false); 
+        if(nums.size() == 1) return true; 
+        vector<bool> visited(nums.size(), false); 
         queue<int> q; 
         q.push(0); 
         visited[0] = true; 
 
         while(!q.empty()){
             int size = q.size(); 
+
             for(int i = 0; i < size; i++){
-                int first = q.front();
+                int curr = q.front();
                 q.pop(); 
 
-                if(first == nums.size() - 1) return true; 
+                
+                for(int i = 1; i <= nums[curr]; i++){
+                    int next = curr + i; 
+                    if(next >= nums.size() || next != nums.size() -1 && nums[next] == 0 || visited[next]) continue; 
 
-                int ele = nums[first]; 
-
-                for(int j = 1; j <= ele; j++){
-                    if(first + j < nums.size() && !visited[first+j]){
-                        //cout << ele << ' ' <<  first + j << ' ' << nums[first+j] << endl; 
-                        if(first + j == nums.size() - 1) return true; 
-                        if(nums[first + j] != 0){
-                            q.push(first + j); 
-                            visited[first+j] = true; 
-                        }
-                    }
+                    if(next == nums.size() -1) return true; 
+                    //cout << next << ' '; 
+                    q.push(next); 
+                    visited[next] = true; 
                 }
             }
         }
-
         return false; 
     }
 };
+
+// class Solution {
+// public:
+//     bool visited[10001]; 
+//     bool canJump(vector<int>& nums) {
+//         queue<int> q; 
+//         memset(visited, false, sizeof(visited)); 
+//         q.push(0); 
+//         visited[0] = true; 
+        
+//         while(!q.empty()){
+//             int size = q.size();
+//             for(int i = 0; i < size; i++){
+//                 int index = q.front(); 
+//                 q.pop(); 
+
+//                 if(index >= nums.size()-1){
+//                     return true; 
+//                 }
+
+//                 for(int j = 1; j <= nums[index]; j++){
+//                     int new_index = index + j; 
+
+//                     if(new_index >= nums.size() || new_index != nums.size()-1 && nums[new_index] == 0 || visited[new_index]) continue; 
+//                     if(new_index == nums.size() -1) return true; 
+//                     visited[new_index] = true; 
+//                     q.push({new_index}); 
+//                 }
+//             }
+//         }
+
+//         return false; 
+//     }
+// };
