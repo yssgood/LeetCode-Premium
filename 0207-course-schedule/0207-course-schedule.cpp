22436a25eb855384b8762b7145e99da8a27c0e9c
유승선 <yssgood@naver.com>
2024-06-03 23:20:40 +0900

    Time: 17 ms (49.41%), Space: 16.9 MB (80.38%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int visited[2001]; 
+    bool dfs(vector<vector<int>>& adj, int node){
+        if(visited[node] == 1) return false; 
+        if(visited[node] == 2) return true; 
+        visited[node] = 1; 
+        for(int next : adj[node]){
+            if(!dfs(adj, next)) return false; 
+        }
+        visited[node] = 2; 
+        return true; 
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses); 
+        for(vector<int>& v : prerequisites){
+            int to = v[0], from = v[1]; 
+            adj[to].push_back(from); 
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            if(visited[i] == 0){
+                if(!dfs(adj,i)) return false; 
+            }
+        }
+        return true; 
+    }
+};
+
+
+//3 <- 1 <- 4 
+// 2 <- 4 
+// 
