206fa658faed225a4351ae33b0f4d02c2aa47967
유승선 <yssgood@naver.com>
2024-06-04 19:47:42 +0900

    Time: 0 ms (100%), Space: 7.9 MB (87.21%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -3,19 +3,25 @@ public:
     bool findDiff(string& a, string& b){
         int cnt = 0; 
         for(int i = 0; i < a.length(); i++){
-            if(a[i] != b[i]) cnt++; 
-
+            if(a[i] != b[i]) cnt++;
             if(cnt > 1) return false; 
         }
         return true; 
     }
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        map<string,bool> visited; 
-        for(string& s : bank) visited[s] = false; 
-
-        int answer = -1; 
+        //<string,int> pair -> q 
+        //if (diff(..) -> true) q.push(next string)
+        //map<string,bool> mp -> mp[next string] = true  
+        //bfs  ->  if curr string == endGene -> return dist 
+        map<string, bool> hashMap; 
         queue<pair<string,int>> q; 
-        q.push({startGene, 0}); 
+        q.push({startGene,0}); 
+        
+        for(string& s : bank) hashMap[s] = true; 
+        if(hashMap[endGene] == false) return -1; 
+
+        hashMap.clear(); 
+        hashMap[startGene] = true; 
 
         while(!q.empty()){
             int size = q.size(); 
@@ -23,26 +29,23 @@ public:
                 pair<string,int> p = q.front();
                 q.pop(); 
 
-                string s = p.first; 
+                string curr = p.first; 
                 int dist = p.second; 
 
-                if(s == endGene){
+                if(curr == endGene){
                     return dist; 
                 }
 
-                for(string& next : bank){
-                    if(visited[next]) continue; 
-
-                    if(findDiff(s, next)){
-                        visited[next] = true; 
-                        q.push({next, dist+1}); 
+                for(string& s : bank){
+                    if(!hashMap[s] && findDiff(curr,s)){
+                        hashMap[s] = true; 
+                        q.push({s, dist + 1}); 
                     }
                 }
             }
         }
 
-
-
-        return answer; 
+        return -1; 
+        
     }
 };
