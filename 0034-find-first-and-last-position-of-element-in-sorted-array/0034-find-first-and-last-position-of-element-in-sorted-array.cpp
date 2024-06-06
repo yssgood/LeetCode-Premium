ae9d179043bada11f2c3bf5bf1184ce0487b8ffc
유승선 <yssgood@naver.com>
2024-06-06 23:17:09 +0900

    Time: 11 ms (11.85%), Space: 16 MB (48.96%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,82 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> answer(2,-1); 
-
-        int start = 0, end = nums.size()-1; 
-
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            
+        if(nums.empty()) return {-1,-1}; 
+        vector<int> answer = {-1,-1}; 
+        int left = 0, right = nums.size()-1; 
+        while(left <= right){
+            int mid = (right + left) / 2; 
             if(nums[mid] == target){
                 answer[0] = mid; 
-                end = mid - 1; 
+                right = mid - 1; 
+            } else if(nums[mid] < target){
+                left = mid + 1; 
+            } else{
+                right = mid - 1; 
             }
-
-            else if(nums[mid] < target) start = mid + 1;
-            else end = mid - 1; 
         }
+        
+        // if(nums[left] != target){
+        //     return {-1,-1}; 
+        // } else{
+        //     answer[0] = left; 
+        // }
 
-        start = 0, end = nums.size()-1; 
+        left = 0; 
+        right = nums.size()-1; 
 
-        while(start <= end){
-            int mid = start + (end - start) / 2; 
-
+        while(left <= right){
+            int mid = (right + left) / 2; 
             if(nums[mid] == target){
                 answer[1] = mid; 
-                start = mid + 1; 
+                left = mid + 1; 
+            } else if(nums[mid] < target){
+                left = mid + 1; 
+            } else{
+                right = mid - 1; 
             }
-
-            else if(nums[mid] < target) start = mid + 1; 
-            else end = mid - 1; 
         }
-
 
         return answer; 
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+//         vector<int> answer{-1,-1}; 
+//         int start = 0; 
+//         int end = nums.size()-1; 
+        
+//         while(start <= end){
+//             int mid = start + (end-start)/2; 
+            
+//             if(nums[mid] == target){
+//                 answer[0] = mid; 
+//                 end = mid-1; 
+//             }
+            
+//             else if (nums[mid] < target) start = mid+1;
+//             else end = mid-1; 
+            
+//         }
+        
+//         start = 0, end = nums.size()-1; 
+        
+//         while(start <= end){
+//             int mid = start + (end-start)/2; 
+            
+//             if(nums[mid] == target){
+//                 answer[1] = mid; 
+//                 start = mid+1; 
+//             }
+//             else if (nums[mid] < target) start = mid+1;
+//             else end = mid-1;
+//         }
+        
+        
+        
+//         return answer; 
+//     }
+// };
