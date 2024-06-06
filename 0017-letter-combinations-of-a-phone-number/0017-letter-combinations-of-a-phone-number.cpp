b1806b4216e32afbaf0153e236f88d91b202663d
유승선 <yssgood@naver.com>
2024-06-06 18:48:27 +0900

    Time: 0 ms (100%), Space: 8.5 MB (21.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    map<char, vector<char>> hashMap = {{'2', {'a','b','c'}}, 
-                                    {'3', {'d','e','f'}}, 
-                                    {'4', {'g','h','i'}}, 
-                                    {'5', {'j','k','l'}}, 
-                                    {'6', {'m','n','o'}}, 
-                                    {'7', {'p','q','r','s'}}, 
-                                    {'8', {'t','u','v'}}, 
-                                    {'9', {'w','x','y','z'}}};
-
-    void dfs(vector<string>& answer, string& digits, string& tmp, int index){
+    vector<string> answer; 
+    void dfs(string& digits, int index, map<char,vector<char>>& hashMap, string& tmp){
         if(tmp.length() >= digits.length()){
             answer.push_back(tmp);
             return; 
         }
 
+
         for(int i = 0; i < hashMap[digits[index]].size(); i++){
-            tmp += hashMap[digits[index]][i]; 
-            dfs(answer,digits,tmp,index+1); 
+            tmp += hashMap[digits[index]][i];
+            dfs(digits,index+1,hashMap,tmp);
             tmp.pop_back(); 
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {}; 
-        vector<string> answer; 
-        string tmp = ""; 
-        
-        dfs(answer,digits, tmp, 0); 
+        map<char,vector<char>> hashMap = {{'2', {'a','b','c'}},
+                                            {'3', {'d','e','f'}},
+                                            {'4', {'g','h','i'}},
+                                            {'5', {'j','k','l'}},
+                                            {'6', {'m','n','o'}},
+                                            {'7', {'p','q','r','s'}},
+                                            {'8', {'t','u','v'}},
+                                            {'9', {'w','x','y','z'}}};
 
+        string tmp = ""; 
+        dfs(digits,0,hashMap,tmp); 
         return answer; 
     }
 };
