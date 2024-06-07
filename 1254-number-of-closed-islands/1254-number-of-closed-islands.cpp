d219b9843c4a3f7326758feae0b7ad39588f2263
유승선 <yssgood@naver.com>
2024-06-07 22:40:25 +0900

    Time: 4 ms (95.95%), Space: 11.8 MB (92.44%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& grid, int i, int j){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return false; 
-        if(grid[i][j] == 1) return true; 
+    void dfs(vector<vector<int>>& grid, int i, int j){
+        if(i < 0  || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 1) return; 
 
-        // grid[i][j] = 1; 
-        // bool flag = (dfs(grid,i+1,j) && dfs(grid,i-1,j) && dfs(grid,i,j+1) && dfs(grid,i,j-1)); 
         grid[i][j] = 1; 
-        bool a = dfs(grid,i+1,j); 
-        bool b = dfs(grid,i-1,j); 
-        bool c = dfs(grid,i,j+1); 
-        bool d = dfs(grid,i,j-1); 
 
-        return a && b && c && d;  
-
-
-        // return flag; 
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1); 
     }
     int closedIsland(vector<vector<int>>& grid) {
-        int answer = 0; 
+        //1. grid 밖에 있는 0을 찾는다. 
+        //-> i == 0 || j == 0 || i == grid.size()-1 || j == grid[0].size() -1 -> if (grid[i][y] == 0) dfs() 
+
+        //2. grid 를 다시 탐색한다 
+        //if grid[x][y] = 0 -> dfs() 
+        //answer++ 
+
+        //사전 작업 (1) 
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 0 && dfs(grid,i,j)){
-                    answer++; 
+                if(i == 0 || j == 0 || i == grid.size()-1 || j == grid[0].size()-1){
+                    if(grid[i][j] == 0){
+                        dfs(grid,i,j); 
+                    }
+                } 
+            }
+        }
+
+        int answer = 0; 
+
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == 0){
+                    answer++;
+                    dfs(grid,i,j); 
                 }
             }
         }
@@ -30,33 +43,3 @@ public:
         return answer; 
     }
 };
-
-// class Solution {
-//     vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}}; 
-
-// public:
-//     bool dfs(vector<vector<int>>& grid, int i, int j){
-//         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()) return false; 
-//         if(grid[i][j] == 1) return true; 
-        
-//         grid[i][j] = 1; 
-//         bool a = dfs(grid,i+1,j); 
-//         bool b = dfs(grid,i-1,j); 
-//         bool c = dfs(grid,i,j+1); 
-//         bool d = dfs(grid,i,j-1); 
-
-//         return a && b && c && d;  
-//     }
-// public:
-//     int closedIsland(vector<vector<int>>& grid) {
-//         int count = 0; 
-//         for(int i = 0; i < grid.size(); i++){
-//             for(int j = 0; j < grid[0].size(); j++){
-//                 if(grid[i][j] == 0 && dfs(grid,i,j)){
-//                     count++; 
-//                 }
-//             }
-//         }
-//         return count; 
-//     }
-// };
