1ed7123fd7975635996e9c94ad502f122601db4f
유승선 <yssgood@naver.com>
2024-06-08 20:38:05 +0900

    Time: 46 ms (28.32%), Space: 23.8 MB (29.31%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,39 +1,37 @@
 class Solution {
-struct Node{
-    int x, y, currDist; 
-}; 
-
-vector<pair<int,int>> dir = {{0,1},{0,-1}, {1,0}, {-1,0}}; 
 public:
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     void wallsAndGates(vector<vector<int>>& rooms) {
-        queue<Node> q; 
+        queue<vector<int>> q; 
+        
         for(int i = 0; i < rooms.size(); i++){
             for(int j = 0; j < rooms[i].size(); j++){
-                if(rooms[i][j] == 0) q.push({i,j,0}); 
+                if(rooms[i][j] == 0){
+                    q.push({i,j,0}); 
+                }
             }
         }
 
         while(!q.empty()){
             int size = q.size(); 
-
             for(int i = 0; i < size; i++){
-                Node node = q.front();
+                vector<int> node = q.front();
                 q.pop(); 
 
-                int x = node.x, y = node.y, currDist = node.currDist; 
+                int x = node[0], y = node[1], dist = node[2]; 
 
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first;
                     int nY = y + p.second; 
 
-                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size() || rooms[nX][nY] == -1) continue;
+                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size()) continue; 
 
-                    if(rooms[nX][nY] > currDist + 1){
-                        rooms[nX][nY] = currDist + 1; 
-                        q.push({nX, nY, currDist + 1}); 
+                    if(rooms[nX][nY] > dist + 1){
+                        rooms[nX][nY] = dist + 1; 
+                        q.push({nX,nY,dist + 1}); 
                     }
                 }
             }
-        }     
+        }
     }
 };
