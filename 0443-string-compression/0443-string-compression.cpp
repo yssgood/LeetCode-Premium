61c3ac28dd238657c07ffb0f3cfc2d9be49c9276
유승선 <yssgood@naver.com>
2024-06-08 22:21:13 +0900

    Time: 4 ms (63.53%), Space: 12.5 MB (58.42%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int start = 0, end = 0; 
         string answer = ""; 
-        char currChar = chars[start];
-        while(end < chars.size()){
-            char nextChar = chars[end]; 
-
-            if(nextChar != currChar){
-                int len = end - start; 
-                while(chars[start] == currChar){
-                    start++; 
-                }
-                
-                if(len > 1){
-                    answer = answer + currChar + to_string(len); 
-                } else{
-                    answer += currChar; 
-                    
-                }
-                currChar = chars[start]; 
-            }
-
-
-            if(end == chars.size()-1){
-                int len = end - start + 1; 
-                if(len > 1){
-                    answer = answer + currChar + to_string(len); 
-                } else{
-                    answer += currChar; 
-                }
-            }
-            end++; 
+        int len = chars.size(); 
+        for(int i = 0; i < chars.size(); i++){
+            char curr = chars[i]; 
+            answer += curr; 
+            int end = i; 
+            while(end < chars.size() && chars[end] == curr) end++; 
+            if(end - i > 1) answer += to_string(end - i); 
+            i = end - 1; 
         }
-        chars = {}; 
+
         for(char& c : answer){
             chars.push_back(c); 
         }
-
-        return chars.size();
+        chars.erase(chars.begin(), chars.begin() + len); 
+        return chars.size(); 
     }
 };
