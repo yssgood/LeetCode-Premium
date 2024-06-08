b23852ca39bdf5457498fe80d17e845fd1abfc83
유승선 <yssgood@naver.com>
2024-06-08 23:06:53 +0900

    Time: 0 ms (100%), Space: 7.5 MB (78.28%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int min_length = min(word1.length(), word2.length()); 
-        bool diff = false; 
-        string tail = "", answer = ""; 
-        if(word1.length() < word2.length()){
-            tail = word2; 
-        } else if(word1.length() > word2.length()){
-            tail = word1; 
+        int start1 = 0;
+        int start2 = 0; 
+        string answer = ""; 
+        while(start1 < word1.length() && start2 < word2.length()){
+            answer += word1[start1++]; 
+            answer += word2[start2++]; 
         }
-        
 
-        for(int i = 0; i < min_length; i++){
-            answer += word1[i];
-            answer += word2[i]; 
+        if(word1.length() > word2.length()){
+            answer += word1.substr(start1); 
+        } else{
+            answer += word2.substr(start2); 
         }
-        
-        if(!tail.empty()){
-            answer += tail.substr(min_length); 
-        }
-        
+
         return answer; 
     }
 };
