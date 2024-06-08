257b29404b794df8ddd9e4c6139754e1c55054b6
유승선 <yssgood@naver.com>
2024-06-08 20:32:42 +0900

    Time: 23 ms (86.35%), Space: 22.3 MB (75.97%) - LeetHub

--- 0490-the-maze/0490-the-maze.cpp
+++ 0490-the-maze/0490-the-maze.cpp
@@ -1,38 +1,37 @@
 class Solution {
-    struct Ball{
-        int x, y; 
-    };
-    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
 public:
+    bool visited[101][101]; 
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-        vector<vector<bool>> visited(maze.size(), vector<bool>(maze[0].size(),false)); 
-        queue<Ball> q; 
+        queue<pair<int,int>> q; 
         q.push({start[0],start[1]}); 
         visited[start[0]][start[1]] = true; 
-        while(!q.empty()){
-            int size = q.size(); 
-            for(int i = 0; i < size; i++){
-                Ball first = q.front(); 
-                q.pop(); 
 
-                int x = first.x, y = first.y; 
-                if(x == destination[0] && y == destination[1]) return true; 
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                pair<int,int> node = q.front();
+                q.pop(); 
+                
+                int x = node.first, y = node.second;
+
+                if(x == destination[0] && y == destination[1]){
+                    return true; 
+                }
 
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first; 
                     int nY = y + p.second; 
-
                     while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] == 0){
                         nX += p.first;
                         nY += p.second; 
                     }
-
-                    nX -= p.first;
+                    nX -= p.first; 
                     nY -= p.second; 
-
+                    
                     if(!visited[nX][nY]){
-                        visited[nX][nY] = true; 
                         q.push({nX,nY}); 
+                        visited[nX][nY] = true; 
                     }
                 }
             }
