4205c11d969dccd156faf3ae62ec87b98a056e89
유승선 <yssgood@naver.com>
2024-06-10 20:18:32 +0900

    Time: 26 ms (72.66%), Space: 15.8 MB (56.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -4,6 +4,7 @@ public:
         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1') return; 
 
         grid[i][j] = '0'; 
+
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
         dfs(grid,i,j+1);
@@ -14,13 +15,11 @@ public:
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j); 
                     answer++; 
+                    dfs(grid,i,j); 
                 }
             }
         }
         return answer; 
     }
 };
-
-//run time is O(m * n); 
