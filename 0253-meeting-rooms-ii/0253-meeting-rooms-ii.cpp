71622f7b631376ae88d556178467690c3f788b0d
유승선 <yssgood@naver.com>
2024-06-10 20:15:15 +0900

    Time: 8 ms (89.78%), Space: 15.2 MB (80.83%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        if(intervals.size() < 2) return 1; 
-        int answer = 1; 
-        sort(intervals.begin(),intervals.end()); 
+        if(intervals.size() == 1) return 1; 
+
+        sort(intervals.begin(), intervals.end()); 
         priority_queue<int,vector<int>,greater<int>> pq; 
         pq.push(intervals[0][1]); 
 
         for(int i = 1; i < intervals.size(); i++){
-            int next = intervals[i][0], nextStart = intervals[i][1]; 
-            if(pq.top() > next){
-                pq.push(nextStart); 
-            } else{
+            int next_start = intervals[i][0]; 
+            int next_end = intervals[i][1]; 
+            if(!pq.empty() && pq.top() <= next_start){
                 pq.pop(); 
-                pq.push(nextStart); 
-            }
+            } 
+            pq.push(next_end);
         }
-        
+
         return pq.size(); 
     }
 };
