76862a65f7ebe7d155311635ab161265bddc9af8
유승선 <yssgood@naver.com>
2024-06-10 14:30:57 +0900

    Time: 3 ms (37.74%), Space: 8.3 MB (12.92%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> answer; 
-
-        int top = 0, left = 0, right = matrix[0].size()-1, bottom = matrix.size()-1; 
+        int top = 0, left = 0, bottom = matrix.size()-1, right = matrix[0].size()-1; 
         int dir = 0; 
-
-        while(answer.size() < matrix.size() * matrix[0].size()){
-            if(dir == 0){ //to the right 
+        int m = matrix.size(), n = matrix[0].size(); 
+        while(answer.size() < m * n){
+            if(dir == 0){
                 for(int i = left; i <= right; i++){
                     answer.push_back(matrix[top][i]); 
                 }
                 top++; 
                 dir = 1; 
-            }  else if(dir == 1){ //to the bottom 
+            } else if(dir == 1){
                 for(int i = top; i <= bottom; i++){
                     answer.push_back(matrix[i][right]); 
                 }
-                right--; 
+                right--;
                 dir = 2; 
-            } else if(dir == 2){ //to the left 
-                for(int j = right; j >= left; j--){
-                    answer.push_back(matrix[bottom][j]); 
+            } else if(dir == 2){
+                for(int i = right; i >= left; i--){
+                    answer.push_back(matrix[bottom][i]); 
                 }
                 bottom--;
                 dir = 3; 
-            } else if(dir = 3){ //to the top 
+            } else if(dir == 3){
                 for(int i = bottom; i >= top; i--){
-                    answer.push_back(matrix[i][left]);  
+                    answer.push_back(matrix[i][left]); 
                 }
-
-                left++; 
+                left++;
                 dir = 0; 
             }
         }
