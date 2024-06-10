ac6e148446495ac63c4e7055dcf1a4c5f91603f6
유승선 <yssgood@naver.com>
2024-06-10 20:06:03 +0900

    Time: 0 ms (100%), Space: 13.6 MB (36.51%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,54 +2,40 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size()-1; 
+        int pivot = 0; 
 
         while(left < right){
             int mid = (right + left) / 2; 
 
+            if(nums[mid] == target) return mid; 
+            
             if(nums[mid] > nums[right]){
                 left = mid + 1; 
+                pivot = left; 
             } else{
                 right = mid; 
             }
         }
-        int pivot = left; 
-        left = pivot; 
-        right = nums.size()-1; 
+        
+        if(target <= nums[nums.size()-1]){
+            left = pivot; 
+            right = nums.size()-1; 
+        } else{
+            left = 0; 
+            right = pivot; 
+        }
         
         while(left <= right){
             int mid = (right + left) / 2; 
+            
+            if(nums[mid] == target) return mid; 
 
-            if(nums[mid] == target){
-                return mid; 
-            }
-
-            if(nums[mid] > target){
-                right = mid -1; 
-            } else{
+            if(nums[mid] < target){
                 left = mid + 1; 
+            } else{
+                right = mid - 1; 
             }
         }
-
-        left = 0;
-        right = pivot; 
-
-        while(left <= right){
-            int mid = (right + left) / 2; 
-
-            if(nums[mid] == target){
-                return mid; 
-            }
-
-            if(nums[mid] > target){
-                right = mid -1; 
-            } else{
-                left = mid + 1; 
-            }
-        }
-
-
-
-
         return -1; 
     }
 };
