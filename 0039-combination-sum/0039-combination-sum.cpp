a61723ec3077fc0340989c4dabc204b83fc3d25e
유승선 <yssgood@naver.com>
2024-06-10 21:06:35 +0900

    Time: 4 ms (74.31%), Space: 12.5 MB (55.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> answer; 
-    void dfs(vector<int>& candidates, int& target, vector<int>& container, int index){
+    void dfs(vector<int>& candidates, vector<int>& container, int& target, int index){
         if(target <= 0){
-            if(target == 0){
-                answer.push_back(container); 
-            }
-            return; 
+            if(target == 0) answer.push_back(container); 
+            return;  
         }
 
         for(int i = index; i < candidates.size(); i++){
             container.push_back(candidates[i]); 
-            target -= candidates[i];
-            dfs(candidates, target, container, i); 
-            container.pop_back(); 
+            target -= candidates[i]; 
+            dfs(candidates, container, target, i); 
+            container.pop_back();
             target += candidates[i]; 
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> container; 
-
-        dfs(candidates,target,container,0); 
+        
+        dfs(candidates,container,target,0); 
 
         return answer; 
     }
