b15ad1a16528eabec2a487917f0337250652af40
유승선 <yssgood@naver.com>
2024-06-10 15:55:31 +0900

    Time: 27 ms (61.76%), Space: 40.2 MB (20.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix(nums.size() + 1); 
-        vector<int> suffix(nums.size() + 1); 
-        prefix[0] = 1;  
-        for(int i = 1; i < prefix.size(); i++){
-            prefix[i] = prefix[i-1] * nums[i-1]; 
-        }
-        suffix[suffix.size()-1] = 1; 
-        for(int i = nums.size()-1; i >= 0; i--){
-            suffix[i] = nums[i] * suffix[i+1]; 
-        }
+        vector<int> prefix(nums.size()); 
+        vector<int> suffix(nums.size()); 
         vector<int> answer(nums.size(),0); 
+        prefix[0] = 1 * nums[0]; 
+        for(int i = 1; i < nums.size(); i++){
+            prefix[i] = prefix[i-1] * nums[i]; 
+        }
+
+        suffix[suffix.size()-1] = 1 * nums[nums.size()-1]; 
+        for(int i = nums.size()-2; i >= 0; i--){
+            suffix[i] = suffix[i+1] * nums[i]; 
+        }
+
         for(int i = 0; i < nums.size(); i++){
             if(i == 0){
                 answer[i] = suffix[i+1]; 
-            } 
-            if(i == nums.size()-1){
-                answer[i] = prefix[i]; 
+            } else if(i == nums.size()-1){
+                answer[i] = prefix[i-1]; 
             } else{
-                answer[i] = prefix[i] * suffix[i+1]; 
+                answer[i] = prefix[i-1] * suffix[i+1]; 
             }
         }
 
