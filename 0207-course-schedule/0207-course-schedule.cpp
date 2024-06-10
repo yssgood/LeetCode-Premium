8b7e2ce2f7539234304e2e979e2bad34f5567f9a
유승선 <yssgood@naver.com>
2024-06-10 20:36:51 +0900

    Time: 15 ms (70.19%), Space: 16.8 MB (86.63%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,32 +2,30 @@ class Solution {
 public:
     int visited[2001]; 
     bool dfs(vector<vector<int>>& adj, int node){
-        if(visited[node] == 1) return false; 
-        if(visited[node] == 2) return true; 
-        visited[node] = 1; 
+        if(visited[node] == 1) return false; //I'm still traversing and still visited this node 
+        if(visited[node] == 2) return true; //finished traversing 
+
+
+        visited[node] = 1; //still traversing 
+
         for(int next : adj[node]){
-            if(!dfs(adj, next)) return false; 
+            if(!dfs(adj , next)) return false; //if I find cycle, just return 
         }
-        visited[node] = 2; 
-        return true; 
+
+        visited[node] = 2;  
+
+        return true; //end of traversal  
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses); 
-        for(vector<int>& v : prerequisites){
-            int to = v[0], from = v[1]; 
+        for(vector<int>& p : prerequisites){
+            int to = p[0], from = p[1];  
             adj[from].push_back(to); 
         }
 
         for(int i = 0; i < numCourses; i++){
-            if(visited[i] == 0){
-                if(!dfs(adj,i)) return false; 
-            }
+            if(visited[i] == 0 && !dfs(adj,i)) return false; 
         }
         return true; 
     }
 };
-
-
-//3 <- 1 <- 4 
-// 2 <- 4 
-// 
