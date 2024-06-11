1e7053163ed291720132ca96a0ba8661354f7a66
유승선 <yssgood@naver.com>
2024-06-12 08:37:12 +0900

    Time: 21 ms (23.07%), Space: 17.2 MB (46.14%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, int node,vector<bool>& visited){
+    void dfs(vector<vector<int>>& adj, vector<bool>& visited, int node){
         visited[node] = true; 
         for(int next : adj[node]){
             if(!visited[next]){
-                dfs(adj, next, visited); 
+                dfs(adj,visited,next); 
             }
         }
     }
     int countComponents(int n, vector<vector<int>>& edges) {
+        //vector<vector<int>> adj(n) 
+        //for() -> adj[from]->to, adj[to]->from 
+        //vector<bool> visite(n)
+        //for(int i = 0 -> n-1) dfs(); answer++ 
+        //if(visited[i] == false) -> dfs(); answer++ 
+
         vector<vector<int>> adj(n); 
         for(vector<int>& v : edges){
-            int from = v[0], to =v[1]; 
-            adj[from].push_back(to);
+            int from = v[0], to = v[1]; 
+            adj[from].push_back(to); 
             adj[to].push_back(from); 
         }
-
         int answer = 0; 
         vector<bool> visited(n,false); 
         for(int i = 0; i < n; i++){
             if(!visited[i]){
                 answer++; 
-                dfs(adj,i,visited); 
+                dfs(adj,visited,i); 
             }
         }
+
         return answer; 
     }
 };
