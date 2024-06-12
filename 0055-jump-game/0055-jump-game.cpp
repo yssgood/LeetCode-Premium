4b116e1f6451f4d15f885d912908a785884a749c
유승선 <yssgood@naver.com>
2024-06-12 12:47:24 +0900

    Time: 47 ms (46.95%), Space: 50.9 MB (50.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> visited(nums.size(), false); 
-        queue<int> q; 
-        q.push(0);  
-
-        while(!q.empty()){
-            int size = q.size(); 
-            for(int i = 0; i < size; i++){
-                int curr = q.front(); 
-                q.pop(); 
-
-                if(curr >= nums.size()-1) return true; 
-
-                for(int i = 1; i <= nums[curr]; i++){
-                    int nCurr = curr + i; 
-
-                    if(nCurr >= nums.size()-1) return true; 
-
-                    if(!visited[nCurr]){
-                        visited[nCurr] = true; 
-                        q.push({nCurr}); 
-                    }
-                }
+        int minJump = 0; 
+        for(int i = nums.size()-2; i >= 0; i--){
+            minJump++;
+            if(nums[i] >= minJump){
+                minJump = 0; 
             }
         }
-        return false; 
+
+        if(minJump != 0){
+            return false; 
+        }
+
+        return true; 
     }
 };
