fcb3e73c3e24b65aed7cb3cb5b3509be3b06046e
유승선 <yssgood@naver.com>
2024-06-16 23:26:27 +0900

    Time: 43 ms (98.91%), Space: 35 MB (87.59%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.cpp
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int visited[10001]; 
     bool dfs(vector<vector<int>>& adj, int source, int destination){
-        if(visited[source] == 2) return true; 
         if(visited[source] == 1) return false; 
+        if(visited[source] == 2) return true; 
         visited[source] = 1; 
         for(int next : adj[source]){
             if(!dfs(adj,next,destination)) return false; 
         }
         visited[source] = 2; 
-        if(adj[source].empty() && source != destination) return false; 
+        if(adj[source].size() == 0 && source != destination) return false; 
         return true; 
     }
     bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
@@ -19,7 +19,6 @@ public:
             adj[from].push_back(to); 
         }
 
-        bool res = dfs(adj,source,destination); 
-        return res; 
+        return dfs(adj,source,destination); 
     }
 };
