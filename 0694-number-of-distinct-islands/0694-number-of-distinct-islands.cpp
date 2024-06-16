53b574e5aad64136ad375e9b0bc2b81304ecdd3b
유승선 <yssgood@naver.com>
2024-06-16 23:57:36 +0900

    Time: 20 ms (87.71%), Space: 34.2 MB (62.29%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
@@ -1,32 +1,39 @@
 class Solution {
-
-
-    void dfs(vector<vector<int>>& grid, int i, int j, string& way){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 1) return; 
+public:
+    void dfs(vector<vector<int>>& grid, string& tmp, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 1){
+            //tmp.pop_back(); 
+            return; 
+        }
 
         grid[i][j] = 0; 
-
-        dfs(grid,i+1,j,way+="D"); 
-        dfs(grid,i-1,j,way+="U"); 
-        dfs(grid,i,j+1,way+="R");
-        dfs(grid,i,j-1,way+="L"); 
-
-        
-        way+="0"; 
+        dfs(grid,tmp+='d',i+1,j); 
+        dfs(grid,tmp+='u',i-1,j);
+        dfs(grid,tmp+='l',i,j-1);
+        dfs(grid,tmp+='r',i,j+1); 
     }
-public:
     int numDistinctIslands(vector<vector<int>>& grid) {
-        set<string> container; 
+        set<string> stringSet; 
         for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
+            for(int j = 0; j < grid[i].size(); j++){
                 if(grid[i][j] == 1){
-                    string empty = ""; 
-                    dfs(grid,i,j,empty);
-                    //cout << empty << endl; 
-                    container.insert(empty);
+                    string tmp = ""; 
+                    dfs(grid,tmp,i,j); 
+                    //sort(tmp.begin(), tmp.end()); 
+                    stringSet.insert(tmp); 
                 }
             }
-        }  
-        return container.size(); 
+        }
+        // for(auto& it : stringSet){
+        //     cout << it << ' '; 
+        // }
+        return stringSet.size(); 
     }
 };
+
+/*
+[[0,1,1],
+[1,0,1],
+[1,1,0]]
+
+*/ 
