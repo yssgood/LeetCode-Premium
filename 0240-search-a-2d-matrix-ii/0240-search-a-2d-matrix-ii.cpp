c4a3fd25b3527f68d6f00d2e13ff453466903c11
유승선 <yssgood@naver.com>
2024-06-22 23:36:16 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size()-1; 
+        int col = 0; 
         
-        for(int i = 0; i < matrix.size(); i++){
+        while(row >= 0 && col < matrix[0].size()-1){
             
-            int first = matrix[i][0]; 
-            int last = matrix[i][matrix[i].size()-1]; 
-
-            //vector<int> curr = matrix[i]; 
-            
-            if(first <= target && last >= target){
-                int start = 0; 
-                int end = matrix[i].size()-1; 
-
-                while(start <= end){
-                    int mid = (end + start) / 2;  
-
-                    if(matrix[i][mid] == target){
-                        return true; 
-                    } else if(matrix[i][mid] < target){
-                        start = mid + 1; 
-                    } else{
-                        end = mid - 1; 
-                    }
-                }
+            if(matrix[row][col] > target){
+                row--; 
+            } else if(matrix[row][col] < target){
+                col++; 
+            } else{
+                return true;
             }
         }
-
         return false; 
     }
 };
