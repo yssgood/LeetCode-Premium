c433669d9a03e6604f0fcefe60d4e1c90ef6ad40
유승선 <yssgood@naver.com>
2024-06-22 23:58:22 +0900

    Time: 70 ms (16.85%), Space: 61.2 MB (91.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int start = 0, end = height.size()-1; 
-        int curr_max = 0;  
+        int answer = 0; 
         while(start < end){
             int width = end - start; 
-            curr_max = max(curr_max, min(height[start], height[end]) * width); 
+            answer = max(answer, min(height[start], height[end]) * width); 
             if(height[start] < height[end]){
                 start++; 
             } else{
                 end--; 
             }
         }
-        return curr_max; 
+        return answer; 
     }
 };
