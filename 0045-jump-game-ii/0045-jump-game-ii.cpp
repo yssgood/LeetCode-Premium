e4b141c37817d0139082d9db947f5ac9def369e0
유승선 <yssgood@naver.com>
2024-06-23 00:24:47 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int curr_max_jump = 0; 
-        int end_index = 0; 
+        if(nums.size() == 1) return 0; 
+        int currMax = 0; 
         int answer = 0; 
-        for(int i = 0; i < nums.size()-1; i++){
-            curr_max_jump = max(curr_max_jump, i + nums[i]); 
+        for(int i = 0; i < nums.size(); i++){
+            int currJump = nums[i]; 
 
-            if(i == end_index){
-                end_index = curr_max_jump; 
+            if(i == currMax){
+                answer++; 
+                currMax = currMax + currJump; 
+                //need to jump again 
+            } else if(i + currJump > currMax){
+                currMax = i + currJump; 
                 answer++; 
             }
         }
-        return answer; 
+        return answer -1; 
     }
 };
