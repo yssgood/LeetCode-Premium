1d4f6db9f0c550c71b835f49ffa837bcba7a8e27
유승선 <yssgood@naver.com>
2024-06-23 20:36:42 +0900

    Time: 36 ms (34.25%), Space: 23.7 MB (29.31%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -3,7 +3,6 @@ public:
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     void wallsAndGates(vector<vector<int>>& rooms) {
         queue<vector<int>> q; 
-        
         for(int i = 0; i < rooms.size(); i++){
             for(int j = 0; j < rooms[i].size(); j++){
                 if(rooms[i][j] == 0){
@@ -15,20 +14,20 @@ public:
         while(!q.empty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
-                vector<int> node = q.front();
-                q.pop(); 
+                vector<int> curr = q.front();
+                q.pop();  
 
-                int x = node[0], y = node[1], dist = node[2]; 
+                int x = curr[0], y = curr[1], dist = curr[2]; 
 
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first;
                     int nY = y + p.second; 
 
-                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size()) continue; 
+                    if(nX < 0 || nY < 0 || nX >= rooms.size() || nY >= rooms[0].size() || rooms[nX][nY] == -1) continue; 
 
                     if(rooms[nX][nY] > dist + 1){
                         rooms[nX][nY] = dist + 1; 
-                        q.push({nX,nY,dist + 1}); 
+                        q.push({nX,nY,dist+1}); 
                     }
                 }
             }
