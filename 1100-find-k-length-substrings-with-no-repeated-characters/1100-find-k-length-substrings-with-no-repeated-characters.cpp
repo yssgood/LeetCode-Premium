f83192cf08220e794007b7c8a40dd7bdb9c922b3
유승선 <yssgood@naver.com>
2024-06-23 23:23:18 +0900

    Time: 3 ms (83.7%), Space: 9 MB (61.48%) - LeetHub

--- 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
+++ 1100-find-k-length-substrings-with-no-repeated-characters/1100-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int numKLenSubstrNoRepeats(string s, int k) {
-        map<char,int> hashMap; 
-        int answer = 0, start = 0, end = 0; 
-        
+        int start = 0, end = 0; 
+        int answer = 0;  
+        unordered_map<char,int> hashMap; 
         while(end < s.length()){
-            if(hashMap.count(s[end])){
-                while(start <= hashMap[s[end]]){
-                    hashMap.erase(s[start]); 
-                    start++; 
-                }
-                hashMap[s[end]] = end; 
-            } else{
-                hashMap[s[end]] = end; 
-            }
-
-            if(end - start + 1 == k){
-                answer++;
-                hashMap.erase(s[start]);  
+            hashMap[s[end]]++; 
+            while(hashMap[s[end]] > 1){
+                hashMap[s[start]]--;
                 start++; 
             }
 
+            if(end - start + 1 == k){
+                answer++; 
+                hashMap[s[start]]--;
+                start++; 
+            }
             end++; 
         }
-
         return answer; 
     }
 };
