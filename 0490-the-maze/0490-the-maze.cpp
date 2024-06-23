4164d8e17d4b3539e10b85ee50588d483fc1166e
유승선 <yssgood@naver.com>
2024-06-23 19:54:01 +0900

    Time: 23 ms (86.32%), Space: 22.3 MB (74.25%) - LeetHub

--- 0490-the-maze/0490-the-maze.cpp
+++ 0490-the-maze/0490-the-maze.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
-    bool visited[101][101]; 
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    bool visited[101][101]; 
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
         queue<pair<int,int>> q; 
         q.push({start[0],start[1]}); 
-        visited[start[0]][start[1]] = true; 
 
         while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                pair<int,int> node = q.front();
-                q.pop(); 
-                
-                int x = node.first, y = node.second;
+            int size = q.size();  
 
-                if(x == destination[0] && y == destination[1]){
+            for(int i = 0; i < size; i++){
+                pair<int,int> curr = q.front(); 
+                q.pop(); 
+
+                int x = curr.first, y = curr.second; 
+
+                if(x == destination[0]  && y == destination[1]){
                     return true; 
                 }
 
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first; 
                     int nY = y + p.second; 
-                    while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] == 0){
-                        nX += p.first;
+
+                    if(nX < 0 || nY < 0 || nX >= maze.size() || nY >= maze[0].size()) continue; 
+
+                    while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] != 1){
+                        nX += p.first; 
                         nY += p.second; 
                     }
-                    nX -= p.first; 
+
+                    nX -= p.first;
                     nY -= p.second; 
-                    
+
+                    if(nX == destination[0] && nY == destination[1]){
+                        return true; 
+                    } 
+
                     if(!visited[nX][nY]){
+                        visited[nX][nY] = true;
                         q.push({nX,nY}); 
-                        visited[nX][nY] = true; 
                     }
                 }
+
+                
             }
         }
         return false; 
+
     }
 };
