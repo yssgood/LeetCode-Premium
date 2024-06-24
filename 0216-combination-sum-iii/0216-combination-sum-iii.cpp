3b4d7a61c295b874b529b8d532ddfb06d8e32832
유승선 <yssgood@naver.com>
2024-06-25 00:08:48 +0900

    Time: 3 ms (49.89%), Space: 7.9 MB (68.13%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> answer; 
-    vector<int> container; 
-    void dfs(int num, int& sum, int k, int target){
+    vector<vector<int>> answer;
+    void dfs(int k , int& n, vector<int>& container,int curr){
         if(container.size() >= k){
-            if(sum == target){
-                answer.push_back(container); 
-            }
+            if(n == 0) answer.push_back(container); 
             return; 
         }
 
-        for(int i = num; i <= 9; i++){
-            container.push_back(i);
-            sum += i;
-            dfs(i + 1, sum, k, target);
-            container.pop_back();
-            sum -= i; 
+        for(int i = curr; i <= 9; i++){
+            container.push_back(i); 
+            n -= i; 
+            dfs(k,n,container,i + 1);
+            container.pop_back(); 
+            n += i; 
         }
-    }
+    } 
     vector<vector<int>> combinationSum3(int k, int n) {
-        int sum = 0; 
-        dfs(1,sum,k,n);
-        return answer;     
+        vector<int> container; 
+        int curr = 1; 
+        dfs(k,n,container,curr); 
+        return answer; 
     }
 };
