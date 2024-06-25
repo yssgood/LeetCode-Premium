6c2750795c463f0072623c8d854310ba6f65cd1a
유승선 <yssgood@naver.com>
2024-06-25 23:34:30 +0900

    Time: 23 ms (82.37%), Space: 30.6 MB (57.95%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int start = 0, end = 0; 
-        int curr = 0; 
+        int currSum = 0; 
         int answer = INT_MAX; 
         while(end < nums.size()){
-            curr += nums[end]; 
-            while(curr >= target){
-                answer = min(answer, end - start + 1); 
-                curr -= nums[start]; 
-                start++;  
+            //요구 사항 
+            currSum += nums[end]; 
+
+
+
+            //범위 확인 
+            while(currSum >= target){
+                answer = min(answer, end - start + 1);
+                currSum -= nums[start];
+                start++; 
             }
 
-            // if(curr == target){
-            //     //cout << end << ' ' << start << endl; 
-            //     answer = min(answer, end - start + 1); 
-            // }
+
+
+            //범위 증가 + 최소/최장 계산 (변경 가능) 
             end++; 
         }
+
         return answer == INT_MAX ? 0 : answer; 
     }
 };
