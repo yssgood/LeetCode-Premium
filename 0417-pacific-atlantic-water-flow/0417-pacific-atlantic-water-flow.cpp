0afbffd1ac6706e438ef4b7244c26a8e3177896c
유승선 <yssgood@naver.com>
2024-06-25 22:50:40 +0900

    Time: 33 ms (48.29%), Space: 21.2 MB (80.97%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    bool p_visited[201][201]; 
-    bool a_visited[201][201]; 
-    vector<pair<int,int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}}; 
-    void dfs(vector<vector<int>>& heights, int i, int j, char ocean){
-        
-        if(ocean == 'P'){
-            p_visited[i][j] = true; 
-        } else if(ocean == 'A'){
-            a_visited[i][j] = true; 
-        }
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int i, int j){
+        visited[i][j] = true; 
 
         for(pair<int,int>& p : dir){
-            int nX = i + p.first;
+            int nX = i + p.first; 
             int nY = j + p.second; 
-
-            if(nX >= 0 && nY >= 0 && nX < heights.size() && nY < heights[0].size() && heights[nX][nY] >= heights[i][j]){
-                if(ocean == 'P' && !p_visited[nX][nY]){
-                    dfs(heights, nX, nY, ocean); 
-                } else if(ocean == 'A' && !a_visited[nX][nY]){
-                    dfs(heights, nX, nY, ocean); 
-                }
+            if(nX >= 0 && nY >= 0 && nX < heights.size() && nY < heights[0].size()&& heights[nX][nY] >= heights[i][j] && !visited[nX][nY]){
+                dfs(heights,visited,nX,nY); 
             }
         }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>> answer; 
+        vector<vector<bool>> p_visited(heights.size(), vector<bool>(heights[0].size(),false)); 
+        vector<vector<bool>> a_visited(heights.size(), vector<bool>(heights[0].size(),false)); 
         for(int i = 0; i < heights.size(); i++){
             for(int j = 0; j < heights[0].size(); j++){
                 if(i == 0 || j == 0){
-                    if(!p_visited[i][j]) dfs(heights, i, j, 'P'); 
+                    dfs(heights,p_visited,i,j); 
                 }
 
                 if(i == heights.size()-1 || j == heights[0].size()-1){
-                    if(!a_visited[i][j]) dfs(heights, i, j, 'A'); 
+                    dfs(heights,a_visited,i,j); 
                 }
             }
         }
@@ -48,5 +38,6 @@ public:
 
 
         return answer; 
+
     }
 };
