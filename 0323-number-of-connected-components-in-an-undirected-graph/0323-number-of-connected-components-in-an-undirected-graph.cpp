3f88291cec5769b7d0f44380b431282e160aa54a
유승선 <yssgood@naver.com>
2024-06-25 22:53:31 +0900

    Time: 11 ms (89.03%), Space: 17.4 MB (36.74%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    bool visited[2001]; 
-    void dfs(vector<vector<int>>& adj, int node){
-        visited[node] = true;
+    void dfs(vector<vector<int>>& adj, vector<bool>& visited, int node){
+        visited[node] = true; 
         for(int next : adj[node]){
             if(!visited[next]){
-                dfs(adj,next); 
+                dfs(adj, visited, next); 
             }
         }
     }
@@ -13,15 +12,16 @@ public:
         vector<vector<int>> adj(n); 
         for(vector<int>& v : edges){
             int from = v[0], to = v[1]; 
-            adj[from].push_back(to); 
+            adj[from].push_back(to);
             adj[to].push_back(from); 
         }
 
         int answer = 0; 
+        vector<bool> visited(n,false); 
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                answer++;
-                dfs(adj,i); 
+                answer++; 
+                dfs(adj,visited,i); 
             }
         }
         return answer; 
