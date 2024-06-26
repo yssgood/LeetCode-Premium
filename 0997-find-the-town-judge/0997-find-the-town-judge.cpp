b0234da69384b31453824c048bc7edfc37bf0dbe
유승선 <yssgood@naver.com>
2024-06-26 21:30:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(trust.empty()) return 1; 
+
+        vector<pair<int,int>> check(n+1); 
+        for(vector<int>& v : trust){
+            int from = v[0], to = v[1]; 
+            check[from].second++; 
+            check[to].first++; //first represents person who trust me, second represents person I trust to  
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(check[i].first == n-1 && check[i].second == 0){
+                return i; 
+            }
+        }
+
+        return -1; 
+    }
+};
