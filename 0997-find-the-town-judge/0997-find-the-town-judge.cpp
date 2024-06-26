0b81a3b91a0eefe39035580b94d8b1da9173e93b
유승선 <yssgood@naver.com>
2024-06-26 21:45:01 +0900

    Time: 117 ms (74.88%), Space: 65 MB (60.41%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if(trust.empty()) return n == 1 ? 1 : -1; 
-
-        vector<pair<int,int>> check(n+1); 
-        for(vector<int>& v : trust){
-            int from = v[0], to = v[1]; 
-            check[from].second++; 
-            check[to].first++; //first represents person who trust me, second represents person I trust to  
+        if(trust.empty()) return n > 1 ? -1 : 1; 
+        
+        map<int,int> hashMap; 
+        int candidate = 1; 
+        for(int i = 0; i < trust.size(); i++){
+            //candidate = trust[i][1]; 
+            //if(trust[i][0] == candidate) candidate = trust[i][1]; 
+            hashMap[trust[i][0]]++; 
         }
 
         for(int i = 1; i <= n; i++){
-            if(check[i].first == n-1 && check[i].second == 0){
-                return i; 
-            }
+            if(hashMap[i] == 0) candidate = i; 
         }
 
-        return -1; 
+        int count = 0; 
+        for(int i = 0; i < trust.size(); i++){
+            if(trust[i][0] == candidate) return -1; 
+            count += trust[i][1] == candidate ? 1 : 0; 
+        }
+
+        cout << candidate; 
+        return count == n - 1 ? candidate : -1; 
     }
 };
