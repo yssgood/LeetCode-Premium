ea529c1a1978d8babcb6d9b01ab75c467183cb4a
유승선 <yssgood@naver.com>
2024-06-26 21:25:59 +0900

    Time: 12 ms (48.48%), Space: 11.8 MB (92.44%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>& grid, int i, int j){
-        if(i < 0  || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 1) return; 
-
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 0) return; 
+        
         grid[i][j] = 1; 
-
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
+        dfs(grid,i,j+1); 
         dfs(grid,i,j-1); 
+
     }
     int closedIsland(vector<vector<int>>& grid) {
-        //1. grid 밖에 있는 0을 찾는다. 
-        //-> i == 0 || j == 0 || i == grid.size()-1 || j == grid[0].size() -1 -> if (grid[i][y] == 0) dfs() 
-
-        //2. grid 를 다시 탐색한다 
-        //if grid[x][y] = 0 -> dfs() 
-        //answer++ 
-
-        //사전 작업 (1) 
+        int answer = 0; 
+        
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(i == 0 || j == 0 || i == grid.size()-1 || j == grid[0].size()-1){
-                    if(grid[i][j] == 0){
-                        dfs(grid,i,j); 
-                    }
-                } 
-            }
-        }
-
-        int answer = 0; 
-
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 0){
-                    answer++;
                     dfs(grid,i,j); 
                 }
             }
         }
 
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(grid[i][j] == 0){
+                    answer++; 
+                    dfs(grid,i,j); 
+                }
+            }
+        }
         return answer; 
     }
 };
