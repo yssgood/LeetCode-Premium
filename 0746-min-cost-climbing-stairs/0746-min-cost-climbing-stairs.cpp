310e8078dd987b4848631c09f601f7ef5758728a
유승선 <yssgood@naver.com>
2024-06-29 22:42:04 +0900

    Time: 3 ms (79.91%), Space: 16.3 MB (40%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,24 @@
+/*
+Patterns: 
+Minimum (Maximum) Path to Reach a Target
+
+Statement
+Given a target find minimum (maximum) cost / path / sum to reach the target.
+
+Approach
+Choose minimum (maximum) path among all possible paths before the current state, then add value for the current state.
+*/
+
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        if(cost.size() < 3) return min(cost[0],cost[1]); 
-        vector<int> dp(cost.size()+1); 
-        dp[0] = cost[0];
+        vector<int> dp(cost.size()); 
+        dp[0] = cost[0]; 
         dp[1] = cost[1]; 
-
         for(int i = 2; i < cost.size(); i++){
-            dp[i] = min(dp[i-1],dp[i-2]) + cost[i]; 
+            //dp[i] = min(dp[i-1], dp[i-2]) + (i == cost.size()-1 ? 0 : cost[i]); ]
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]; 
         }
-
-        return min(dp[cost.size()-1], dp[cost.size()-2]);  
+        return min(dp[cost.size()-1], dp[cost.size()-2]); 
     }
 };
