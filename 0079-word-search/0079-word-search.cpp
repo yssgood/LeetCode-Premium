bcbc553f93431887a4c60e29e0c1ee0852209f29
유승선 <yssgood@naver.com>
2024-08-04 23:36:50 +0900

    Time: 1019 ms (22.46%), Space: 11.9 MB (23.32%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, int i, int j, string& word, int index){
-        if(index >= word.length()) return true; 
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int index){
+        if(index >= word.length()){
+            return true; 
+        }
+        if(i  < 0  || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]){
+            return false; 
+        }
 
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]) return false; 
-
-        bool flag = true; 
-        char curr = board[i][j]; 
+        char tmp = board[i][j]; 
         board[i][j] = '-'; 
-        flag = (dfs(board,i+1,j,word,index+1) || dfs(board,i-1,j,word,index+1) || dfs(board,i,j+1,word,index+1) || dfs(board,i,j-1,word,index+1)); 
 
-        board[i][j] = curr; 
+        bool flag = (dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1));
 
+        board[i][j] = tmp;  
         return flag; 
     }
     bool exist(vector<vector<char>>& board, string word) {
-        //for() for() -> dfs() 
-        //x y -> !visited -> word[0] 
-        //vector<pair<int,int>> dir -> dfs() 
-        //if curr_index >= word.length() 
-
+        
         for(int i = 0; i < board.size(); i++){
             for(int j = 0; j < board[i].size(); j++){
-                if(board[i][j] == word[0]){
-                    if(dfs(board,i,j,word,0)){
-                        return true; 
-                    }
+                if(board[i][j] == word[0] && dfs(board, word, i, j, 0)){
+                    return true; 
                 }
             }
         }
