ace89b3ee5f101224d5fed8a92c839bcd7c4c9f7
유승선 <yssgood@naver.com>
2024-08-04 23:28:04 +0900

    Time: 2 ms (47.73%), Space: 8.8 MB (51.37%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -23,7 +23,9 @@ public:
         }
 
         for(int i = 0; i < matrix.size(); i++){
-            reverse(matrix[i].begin(), matrix[i].end()); 
+            for(int j = 0; j < matrix.size() / 2; j++){
+                swap(matrix[i][j], matrix[i][matrix.size() - j - 1]); 
+            }
         }
     }
 };
