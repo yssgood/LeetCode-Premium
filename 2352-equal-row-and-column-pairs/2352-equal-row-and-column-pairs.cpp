0f617087f6783eb77078d3d6cafd05f6640c2816
유승선 <yssgood@naver.com>
2024-08-15 16:21:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        map<string,int> rowMap, colMap; 
+        int answer = 0; 
+        map<string,int> rowMap; 
         for(int i = 0; i < grid.size(); i++){
-            string Rowkey = ""; 
-            //string Colkey = ""; 
+            string tmp = ""; 
             for(int j = 0; j < grid[0].size(); j++){
-                //Rowkey += grid[i][j] + '0'; 
-                //Colkey += grid[j][i] + '0'; 
-                Rowkey += to_string(grid[i][j]); 
-                //Colkey += to_string(grid[j][i]); 
+                tmp += to_string(grid[i][j]); 
             }
-            rowMap[Rowkey]++; 
-            //colMap[Colkey]++; 
+            rowMap[tmp]++; 
         }
 
-        int answer = 0;
-
         for(int i = 0; i < grid.size(); i++){
-            string Colkey = ""; 
+            string tmp = ""; 
             for(int j = 0; j < grid[0].size(); j++){
-                Colkey += to_string(grid[j][i]); 
+                tmp += to_string(grid[j][i]); 
             }
-            if(rowMap.count(Colkey)){
-                answer += rowMap[Colkey]; 
-            }
+            
+            if(rowMap.count(tmp)) answer += rowMap[tmp]; 
         }
 
-       
         return answer; 
+
+        
     }
 };
-
-
-/*
-[[13,13],
-[13,13]]
-*/
