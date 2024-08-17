c8ec69951e568ec90d33196df4606729586d81e3
유승선 <yssgood@naver.com>
2024-08-17 12:15:56 +0900

    Time: 7 ms (42.9%), Space: 16.4 MB (41.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,42 +3,44 @@ public:
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> q; 
-        int allFresh = 0; 
-
+        int freshOrange = 0; 
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 1) allFresh++; 
-                if(grid[i][j] == 2) q.push({i,j}); 
+                if(grid[i][j] == 2){
+                    q.push({i,j}); 
+                }
+                if(grid[i][j] == 1){
+                    freshOrange++; 
+                }
             }
         }
 
-        if(q.empty()) return 0; 
+        if(!freshOrange) return 0; 
 
-        int distance = -1; 
+        int time = -1; 
+        
         while(!q.empty()){
             int size = q.size(); 
+            time++; 
             for(int i = 0; i < size; i++){
-                pair<int,int> curr = q.front();
+                pair<int,int> curr = q.front(); 
                 q.pop(); 
-
-
                 int x = curr.first, y = curr.second; 
 
                 for(pair<int,int>& p : dir){
-                    int nX = x + p.first;
+                    int nX = x + p.first; 
                     int nY = y + p.second; 
 
                     if(nX < 0 || nY < 0 || nX >= grid.size() || nY >= grid[0].size() || grid[nX][nY] != 1) continue; 
 
-                    grid[nX][nY] = 2;
-                    allFresh--; 
+                    grid[nX][nY] = 2; 
                     q.push({nX,nY}); 
-
+                    freshOrange--; 
                 }
             }
-            distance++; 
         }
 
-        return allFresh > 0 ? -1 : distance; 
+
+        return freshOrange == 0 ? time : -1; 
     }
 };
