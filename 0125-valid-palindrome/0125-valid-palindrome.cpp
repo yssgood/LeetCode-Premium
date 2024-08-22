98824db909ce3e0c4233f788181ffc7d63f061f8
유승선 <yssgood@naver.com>
2024-08-22 23:39:52 +0900

    Time: 0 ms (100%), Space: 8.6 MB (94.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,13 +3,15 @@ public:
     bool isPalindrome(string s) {
         int start = 0, end = s.length()-1; 
         while(start < end){
-            
-            while(start < end && !isalnum(s[start])) start++; 
+            while(start < end && !isalnum(s[start])) start++;  
             while(start < end && !isalnum(s[end])) end--; 
 
-            if(tolower(s[start]) != tolower(s[end])) return false; 
+            char a = tolower(s[start]);
+            char b = tolower(s[end]); 
+
+            if(a != b) return false; 
             start++;
-            end--; 
+            end--;  
         }
         return true; 
     }
