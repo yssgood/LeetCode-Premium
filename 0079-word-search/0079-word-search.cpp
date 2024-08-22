0c2a56820affd90d0f31209759552f0db92a11cc
유승선 <yssgood@naver.com>
2024-08-22 23:03:27 +0900

    Time: 196 ms (89.28%), Space: 9.9 MB (84.31%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
-    bool dfs(vector<vector<char>>& board, string word, int i, int j, int index){
-        if(index >= word.length()){
-            return true; 
-        }
-        if(i  < 0  || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]){
-            return false; 
-        }
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j,  int index){
+        if(index >= word.length()) return true; 
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]) return false; 
 
-        char tmp = board[i][j]; 
-        board[i][j] = '-'; 
+        char original = board[i][j]; 
+        board[i][j] = '!'; 
 
-        bool flag = (dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1));
+        bool flag = (dfs(board, word, i + 1, j, index + 1) || dfs(board,word, i - 1, j, index +1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j -1, index + 1)); 
 
-        board[i][j] = tmp;  
+        board[i][j] = original; 
         return flag; 
     }
     bool exist(vector<vector<char>>& board, string word) {
         
         for(int i = 0; i < board.size(); i++){
             for(int j = 0; j < board[i].size(); j++){
-                if(board[i][j] == word[0] && dfs(board, word, i, j, 0)){
-                    return true; 
-                }
+                if(board[i][j] == word[0] && dfs(board,word,i,j,0)) return true; 
             }
         }
         return false; 
