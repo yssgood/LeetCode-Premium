eb4d0d3eae1d6e4d27139606a5948445814d63ee
유승선 <yssgood@naver.com>
2024-08-22 22:54:58 +0900

    Time: 7 ms (83.24%), Space: 17.3 MB (7.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,16 +3,15 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         map<int,int> rowMap, colMap; 
         for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
+            for(int j = 0; j < matrix[0].size(); j++){
                 if(matrix[i][j] == 0){
                     rowMap[i]++; 
                     colMap[j]++; 
                 }
             }
         }
-
         for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
+            for(int j = 0; j < matrix[0].size(); j++){
                 if(rowMap.count(i) || colMap.count(j)){
                     matrix[i][j] = 0; 
                 }
