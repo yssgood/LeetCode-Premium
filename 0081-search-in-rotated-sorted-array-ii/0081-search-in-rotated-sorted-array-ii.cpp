56783714f8dde6785967def4aee877164a7e5757
유승선 <yssgood@naver.com>
2024-08-23 22:45:09 +0900

    Time: 8 ms (13.94%), Space: 16.9 MB (21.96%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -3,20 +3,41 @@ public:
     bool search(vector<int>& nums, int target) {
         int left = 0, right = nums.size()-1; 
 
-        while(left <= right){
+        while(left < right){
             while(left < right && nums[left] == nums[left+1]) left++; 
             while(left < right && nums[right] == nums[right-1]) right--; 
+
+            cout << left << ' ' << right << endl; 
+
+            int mid = (right + left) / 2; 
+
+            if(nums[mid] > nums[right]){
+                left = mid + 1; 
+            } else{
+                right = mid; 
+            }
+        }
+
+        if(target > nums[nums.size()-1]){
+            right = left; 
+            left = 0; 
+        } else{
+            right = nums.size()-1; 
+        }
+
+        while(left <= right){
             int mid = (right + left) / 2; 
 
             if(nums[mid] == target) return true; 
 
-            if(nums[mid] > target){
-                right = mid - 1; 
-            } else{
+            if(nums[mid] < target){
                 left = mid + 1; 
+            } else{
+                right = mid -1; 
             }
-
         }
+
+        cout << left; 
         return false; 
     }
 };
