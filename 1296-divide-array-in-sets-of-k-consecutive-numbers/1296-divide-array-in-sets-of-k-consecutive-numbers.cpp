4192efbfde58a3f95198130fd6825fbe8090ffae
유승선 <yssgood@naver.com>
2024-08-24 13:37:49 +0900

    Time: 262 ms (8.58%), Space: 76.6 MB (5.02%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -3,20 +3,26 @@ public:
     bool isPossibleDivide(vector<int>& nums, int k) {
         map<int,int> hashMap; 
         for(int n : nums) hashMap[n]++; 
-        //sort(nums.begin(), nums.end()); 
+        int groups = 0; 
+        sort(nums.begin(),nums.end()); 
         for(int i = 0; i < nums.size(); i++){
-            int n = nums[i]; 
-            if(hashMap[n] > 0){
-                for(int next = n; next < n + k; next++){
-                    if(!hashMap.count(next) || hashMap[next] < 1){
-                        //cout << next << ' ' << hashMap[next] << endl; 
-                        return false; 
-                    }
-                hashMap[next]--; 
-                }
+            if(hashMap[nums[i]] <= 0) continue; 
+            int currNum = nums[i]; 
+            int streak = 1; 
+            hashMap[nums[i]]--; 
+            while(hashMap[currNum + 1] > 0 && streak < k){
+                hashMap[currNum + 1]--; 
+                streak++;
+                currNum++; 
+            }
+
+            //cout << nums[i] << ' ' << streak << endl; 
+
+            if(streak == k){
+                groups++; 
             }
         }
 
-        return true; 
+        return (groups * k) == nums.size() ? true : false;  
     }
 };
