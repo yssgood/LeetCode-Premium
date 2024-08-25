680e381ad31d11b590eaf9891c07f15896d6b5f0
유승선 <yssgood@naver.com>
2024-08-25 13:35:39 +0900

    Time: 160 ms (21.83%), Space: 58.6 MB (12.29%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
-        map<char,int> hashMap; 
-        int left = 0, right = 0; 
-        int answer = 0; 
-        while(right < s.length()){
-            hashMap[s[right]]++; 
-            if(hashMap.size() <= 2) answer = max(answer, right - left + 1); 
+        int start = 0, end = 0; 
+        map<char,int> hashMap;
+        int answer = 0;  
+        while(end < s.length()){
+            hashMap[s[end]]++; 
+
             while(hashMap.size() > 2){
-                
-                hashMap[s[left]]--;
-                if(hashMap[s[left]] <= 0) hashMap.erase(s[left]); 
-                left++; 
+                hashMap[s[start]]--; 
+                if(hashMap[s[start]] <= 0) hashMap.erase(s[start]); 
+                start++; 
             }
 
-
-
-            right++; 
+            answer = max(answer, end -start + 1); 
+            end++; 
         }
         return answer; 
     }
