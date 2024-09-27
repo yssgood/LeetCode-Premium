9ca48be598e43dc738e26f423503edb8f3f52e56
유승선 <yssgood@naver.com>
2024-09-28 00:27:47 +0900

    Time: 1010 ms (30.62%), Space: 12 MB (31.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j,  int index){
+    bool dfs(int i, int j, int index,string word, vector<vector<char>>& board){
         if(index >= word.length()) return true; 
         if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]) return false; 
+        char save = board[i][j]; 
+        board[i][j] = '-'; 
+        bool flag = (dfs(i + 1, j, index + 1, word, board) || dfs(i - 1, j, index +1, word, board)|| dfs(i, j +1, index +1, word, board) ||dfs(i, j-1,index+1,word,board)); 
 
-        char original = board[i][j]; 
-        board[i][j] = '!'; 
+        board[i][j] = save; 
 
-        bool flag = (dfs(board, word, i + 1, j, index + 1) || dfs(board,word, i - 1, j, index +1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j -1, index + 1)); 
-
-        board[i][j] = original; 
         return flag; 
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
         for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){
-                if(board[i][j] == word[0] && dfs(board,word,i,j,0)) return true; 
+            for(int j = 0; j < board[0].size(); j++){
+                if(board[i][j] == word[0] && dfs(i,j,0,word,board)){
+                    return true; 
+                }
             }
         }
+
         return false; 
     }
 };
