802cbc1ecdb82ced403eaa3dcb544cc45abfff02
유승선 <yssgood@naver.com>
2024-09-27 23:59:07 +0900

    Time: 4 ms (39.73%), Space: 9 MB (10.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
+            for(int j = 0; j < matrix[i].size(); j++){
                 if(matrix[i][j] > 0){
                     int save = matrix[i][j]; 
                     matrix[i][j] = matrix[j][i] * -1; 
                     matrix[j][i] = save * -1; 
-                   // matrix[i][j] *= -1; 
-                    //matrix[j][i] *= -1; 
-                    //swap(matrix[i][j], matrix[j][i]); 
                 }
             }
         }
 
         for(int i = 0; i < matrix.size(); i++){
             for(int j = 0; j < matrix[i].size(); j++){
-                //if(matrix[i][j] < 0){
                 matrix[i][j] *= -1; 
-                //}
             }
         }
 
