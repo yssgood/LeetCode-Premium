2658cc9a649d4ab58167f48859ada3f881ed711d
유승선 <yssgood@naver.com>
2024-09-29 23:18:36 +0900

    Time: 34 ms (41.47%), Space: 19.9 MB (99.37%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,48 +1,50 @@
 class Solution {
 public:
+    bool pVisited[201][201];
+    bool aVisited[201][201]; 
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
-    bool pacific[201][201]; 
-    bool atlantic[201][201]; 
-    void dfs(int i, int j, vector<vector<int>>& heights, char ocean){
-        if(i < 0 || j < 0 || i >= heights.size() || j >= heights[0].size()) return; 
-        if(ocean == 'p' && pacific[i][j] || ocean == 'a' && atlantic[i][j]) return; 
-
-        if(ocean == 'p') pacific[i][j] = true; 
-        if(ocean == 'a') atlantic[i][j] = true; 
-
+    void dfs(int i, int j, char ocean, vector<vector<int>>& heights){
+        int curr = heights[i][j]; 
+        //cout << i << ' ' << j << ' ' << ocean << endl; 
+        if(ocean == 'a') aVisited[i][j] = true; 
+        if(ocean == 'p') pVisited[i][j] = true; 
         for(pair<int,int>& p : dir){
             int nX = i + p.first; 
             int nY = j + p.second; 
-
-            if(nX < 0 || nX >= heights.size() || nY < 0 || nY >= heights[0].size()) continue; 
-            if(ocean == 'p' && pacific[nX][nY] || ocean == 'a' && atlantic[nX][nY]) continue; 
-
-            if(heights[nX][nY] >= heights[i][j]){
-                dfs(nX, nY, heights, ocean); 
+            
+            if(nX >= 0 && nY >= 0 && nX < heights.size() && nY < heights[0].size() && heights[nX][nY] >= curr){
+                if(ocean == 'a' && !aVisited[nX][nY]){
+                    dfs(nX,nY,'a',heights); 
+                }
+                if(ocean == 'p' && !pVisited[nX][nY]){
+                    dfs(nX,nY,'p',heights); 
+                }
             }
         }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         vector<vector<int>> answer; 
-        for(int i = 0; i < heights.size(); i++){
-            for(int j = 0; j < heights[i].size(); j++){
-                if(i == 0 || j == 0){
-                    dfs(i,j,heights,'p'); 
-                }
 
+        for(int i = 0; i < heights.size(); i++){
+            for(int j = 0; j < heights[0].size(); j++){
+                if(i == 0 || j == 0){
+                    dfs(i,j,'p',heights); 
+                }
                 if(i == heights.size()-1 || j == heights[0].size()-1){
-                    dfs(i,j,heights,'a'); 
+                    dfs(i,j,'a',heights); 
                 }
             }
         }
 
         for(int i = 0; i < heights.size(); i++){
-            for(int j = 0; j < heights[i].size(); j++){
-                if(pacific[i][j] && atlantic[i][j]){
+            for(int j = 0; j < heights[0].size(); j++){
+                if(aVisited[i][j] && pVisited[i][j]){
                     answer.push_back({i,j}); 
                 }
             }
         }
+
+
         return answer; 
     }
 };
