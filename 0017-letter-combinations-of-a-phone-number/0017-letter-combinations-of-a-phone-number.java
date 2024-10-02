e7c7db04477361da382ed304680dd678e28570aa
유승선 <yssgood@naver.com>
2024-10-03 01:04:24 +0900

    Time: 1 ms (61.33%), Space: 42.1 MB (42.39%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    void dfs(String digits, int index, List<String> answer,StringBuilder sb,Map<Character,List<Character>> hashMap){
+        if(sb.length() >= digits.length()){
+            answer.add(new String(sb.toString())); 
+            return; 
+        }
+
+        for(int i = 0; i < hashMap.get(digits.charAt(index)).size(); i++){
+            sb.append(hashMap.get(digits.charAt(index)).get(i)); 
+            dfs(digits,index+1,answer,sb,hashMap); 
+            sb.deleteCharAt(index); 
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){
+            List<String> ret = new ArrayList<>();
+            return ret; 
+        }
+        List<String> answer = new ArrayList<>(); 
+        Map<Character, List<Character>> hashMap = new HashMap<>() {{
+            put('2', Arrays.asList('a', 'b', 'c'));
+            put('3', Arrays.asList('d', 'e', 'f'));
+            put('4', Arrays.asList('g', 'h', 'i'));
+            put('5', Arrays.asList('j', 'k', 'l'));
+            put('6', Arrays.asList('m', 'n', 'o'));
+            put('7', Arrays.asList('p', 'q', 'r', 's'));
+            put('8', Arrays.asList('t', 'u', 'v'));
+            put('9', Arrays.asList('w', 'x', 'y', 'z'));
+        }};
+        StringBuilder sb = new StringBuilder(); 
+        dfs(digits,0,answer,sb,hashMap); 
+
+        return answer; 
+    }
+}
