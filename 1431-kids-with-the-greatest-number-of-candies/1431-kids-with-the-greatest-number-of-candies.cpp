d2f8557a5fee016e24a428e4e3e35eb668bc71a5
유승선 <yssgood@naver.com>
2024-10-09 16:52:35 +0900

    Time: 3 ms (49.61%), Space: 11 MB (98.82%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        int max_ = *max_element(candies.begin(),candies.end()); 
-        vector<bool> answer(candies.size(), false); 
+        vector<bool> answer(candies.size(),false); 
+        int max_ = 0;
+        for(int n : candies) max_ = max(max_,n); 
         for(int i = 0; i < candies.size(); i++){
             if(candies[i] + extraCandies >= max_){
                 answer[i] = true; 
             }
         }
-
         return answer; 
     }
 };
