aa250881b9eba916bb7a5b85ad12d04581677fbf
유승선 <yssgood@naver.com>
2024-10-13 19:49:05 +0900

    Time: 5 ms (35.96%), Space: 10.6 MB (93.26%) - LeetHub

--- 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.cpp
+++ 1228-missing-number-in-arithmetic-progression/1228-missing-number-in-arithmetic-progression.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int missingNumber(vector<int>& arr) {
-        int min_value = INT_MAX;
-        sort(arr.begin(), arr.end()); 
-        for(int i = 1; i < arr.size(); i++){
-            min_value = min(min_value, arr[i] - arr[i-1]); 
-        }
-        for(int i = 1; i < arr.size(); i++){
-            if(arr[i] - arr[i-1] != min_value){
-                return arr[i-1] + min_value; 
+        //first and last elements are not removed 
+        //[5, 7, 9, 11, 15] 
+        //[15,13,12]
+
+        int n = arr.size(); 
+        int diff = (arr.back() - arr.front()) / n; 
+
+        int left = 0, right = arr.size()-1; 
+        int front = arr[0]; 
+
+        while(left < right){
+            int mid = (right + left) / 2; 
+            if(arr[mid] == front + (mid * diff)){
+                left = mid + 1; 
+            } else{
+                right = mid; 
             }
         }
-        return arr[1] - arr[0]; 
+
+        return front + left * diff; 
     }
 };
