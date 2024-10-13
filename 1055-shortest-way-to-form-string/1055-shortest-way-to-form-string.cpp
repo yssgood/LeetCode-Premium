976d7ec96eabdaded13097baf22b9141a29f18bb
유승선 <yssgood@naver.com>
2024-10-13 18:21:39 +0900

    Time: 0 ms (100%), Space: 8.6 MB (38.13%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int shortestWay(string source, string target) {
-        int sourceP = 0, targetP = 0; 
+        unordered_map<char,int> hashMap;
+        for(char c : source) hashMap[c]++; 
+        int start = 0;
+        int end = 0; 
         int answer = 0; 
+        while(end < target.length()){
+            if(!hashMap.count(target[end])) return -1; 
 
-        while(targetP < target.length()){
-            bool flag = false; 
-            //cout << target[targetP] << ' '; 
-            while(sourceP < source.length()){
-                if(source[sourceP] == target[targetP]){
-                    flag = true; 
-                    sourceP++;
-                    targetP++; 
+            while(start < source.length() && end < target.length()){
+                if(source[start] == target[end]){
+                    start++;
+                    end++; 
                 } else{
-                    sourceP++; 
+                    start++; 
                 }
             }
 
-            if(sourceP >= source.length()){
-                if(!flag) return -1; 
-                sourceP = 0; 
+            answer++;
+            if(start >= source.length()){
+                start = 0; 
             }
-
-            //targetP++; 
-            answer++; 
+            
         }
-
-        return answer; 
+        return answer;
     }
 };
