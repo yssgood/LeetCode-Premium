9bc0510ddee5d45fb400a82cfc5f8b182521b098
유승선 <yssgood@naver.com>
2024-10-15 22:03:28 +0900

    Time: 228 ms (88.12%), Space: 104.7 MB (86.45%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    struct Node{
-        int to;
-        bool out, in; 
-    };
-    void dfs(vector<vector<Node>>& adj, vector<int>& visited, int node, int& answer){
+    bool visited[50001]; 
+    void dfs(vector<vector<pair<int,bool>>>& adj, int node, int& answer){
         visited[node] = true; 
-        for(Node& next : adj[node]){
-            if(!visited[next.to]){
-                if(next.out == true) answer++;  
-                dfs(adj,visited,next.to,answer); 
+        for(pair<int,bool>& next : adj[node]){
+            if(!visited[next.first]){
+                answer += next.second == true ? 1 : 0; 
+                dfs(adj,next.first,answer);  
             }
         }
     }
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<Node>> adj(n); 
-        vector<int> visited(n,false); 
+        vector<vector<pair<int,bool>>> adj(n); 
         for(vector<int>& v : connections){
-            int from = v[0], to =v[1]; 
-
-            adj[from].push_back({to, true, false});
-            adj[to].push_back({from, false, true}); 
+            int from = v[0], to = v[1]; 
+            adj[from].push_back({to,true}); 
+            adj[to].push_back({from,false}); 
         }
 
         int answer = 0; 
-
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(adj,visited,i,answer); 
-            }
-        }
-
+        dfs(adj,0,answer);
         return answer; 
     }
 };
