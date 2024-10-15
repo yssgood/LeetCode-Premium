137ec5ab1cb2aecaeb90f9d730f68b92613f6588
유승선 <yssgood@naver.com>
2024-10-15 22:27:51 +0900

    Time: 124 ms (30.25%), Space: 45.8 MB (27.35%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    vector<pair<int,int>> dir = {{0,1},{0,-1}, {1,0}, {-1,0}}; 
-    bool condition(int nX, int nY, vector<vector<char>>& maze){
-        return (nX == 0 || nX == maze.size()-1 || nY == 0 || nY == maze[0].size()-1); 
-    }
+    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         queue<vector<int>> q; 
         q.push({entrance[0], entrance[1], 0}); 
-        maze[entrance[0]][entrance[1]] = '+'; 
-
         while(!q.empty()){
-            int size = q.size(); 
+            int size = q.size();
             for(int i = 0; i < size; i++){
-                vector<int> curr = q.front(); 
+                vector<int> v = q.front(); 
                 q.pop(); 
 
-                int x = curr[0], y = curr[1], dist = curr[2]; 
+                int x = v[0], y = v[1], dist = v[2]; 
+                maze[x][y] = '+'; 
+                if(dist > 0 && (x == maze.size()-1 || x == 0) || dist > 0 && (y == maze[0].size()-1 || y == 0)){
+                    return dist; 
+                }
 
                 for(pair<int,int>& p : dir){
                     int nX = x + p.first; 
                     int nY = y + p.second; 
-
                     if(nX < 0 || nY < 0 || nX >= maze.size() || nY >= maze[0].size() || maze[nX][nY] == '+') continue; 
 
-                    if(condition(nX,nY,maze)){
-                        return dist + 1; 
-                    }
-                    
                     maze[nX][nY] = '+'; 
                     q.push({nX,nY,dist+1}); 
-                    
                 }
             }
         }
-
         return -1; 
     }
 };
