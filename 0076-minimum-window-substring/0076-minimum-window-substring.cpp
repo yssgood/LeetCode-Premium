4e882aef1568336e3ae5ef71fae55e02e823bf28
유승선 <yssgood@naver.com>
2024-10-16 21:50:12 +0900

    Time: 11 ms (77.35%), Space: 10.8 MB (63.37%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // if (s.empty() || t.empty() || s.length() < t.length()) {
-        //     return "";
-        // }
-        int check = t.length(); 
+        int start = 0, end = 0; 
+        string answer = ""; 
         unordered_map<char,int> hashMap; 
-        //vector<int> hashMap(128, 0); 
         for(char& c : t) hashMap[c]++; 
 
-        int start = 0, end = 0; 
-        string answer = s + "AA"; 
-        int startIndex = 0, minLen = INT_MAX; 
+        int count = 0;
+        int tmpStart = 0; 
+        int minLength = INT_MAX; 
         while(end < s.length()){
-            //요구 사항 확인 
-            //check -= hashMap.count(s[end]) ? 1 : 0; 
             if(hashMap[s[end]]-- > 0){
-                check--; 
                 //hashMap[s[end]]--;
+                count++; 
             }
 
-            //범위 조정 
-            while(check == 0){
-                //cout << end << ' ' << start << ' ' << answer.length() << endl; 
-                if(end - start +1 < minLen){
-                    //answer = s.substr(start, end - start); 
-                    startIndex = start;  
-                    minLen = end - start + 1; 
+            while(count >= t.length()){
+ 
+                if(end - start + 1 < minLength){
+                    tmpStart = start; 
+                    minLength = end - start + 1; 
                 }
 
-                //check += hashMap.count(s[start]) ? 1 : 0; 
-                // if(hashMap.count(s[start])){
-                //     hashMap[s[start]]++; 
-                //     if(hashMap[s[start]] > 0) check++; 
-                // }
                 if(hashMap[s[start++]]++ == 0){
-                    check++; 
+                    count--; 
                 }
-                //start++; 
+
+                // if(hashMap.count(s[start])){
+                //     count--; 
+                //     hashMap[s[start]]++; 
+                // }
+                // start++; 
             }
 
-
-
-            //범위 증가 
             end++; 
         }
-        
-        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen); 
+
+        return minLength == INT_MAX ? "" : s.substr(tmpStart, minLength); 
     }
 };
