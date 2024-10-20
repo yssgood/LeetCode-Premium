167edc700d2c4d3445904aad88647fa94a120af8
유승선 <yssgood@naver.com>
2024-10-20 22:31:55 +0900

    Time: 34 ms (100%), Space: 100.8 MB (92.92%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        vector<int> answer(spells.size(),0); 
-        sort(potions.begin(), potions.end()); 
-        int pSize = potions.size(); 
-        for(int i = 0; i < spells.size(); i++){
-            if((long long)spells[i] * (long long)potions.back() < success) continue; 
-            int left = 0, right = potions.size()-1; 
+    int binarySearch(int spell, vector<int>& potions, long long success){
+        int left = 0, right = potions.size()-1; 
 
-            while(left < right){
-                int mid = (right + left) / 2; 
+        while(left <= right){
+            int mid = (right + left) / 2; 
+            long long curr = (long long)potions[mid] * (long long)spell; 
 
-                long long curr = (long long)spells[i] * (long long)potions[mid]; 
-
-                if(curr >= success){
-                    right = mid; 
-                } else{
-                    left = mid + 1; 
-                }
+            if(curr >= success){
+                right = mid - 1; 
+            } else{
+                left = mid + 1; 
             }
-
-            answer[i] = pSize - left; 
-
+        }
+        return left; 
+    }
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        sort(potions.begin(), potions.end()); 
+        vector<int> answer(spells.size(),0); 
+        int n = potions.size(); 
+        for(int i = 0; i < spells.size(); i++){
+            int lowerBound = binarySearch(spells[i],potions,success); 
+            answer[i] = n - lowerBound; 
         }
         return answer; 
     }
