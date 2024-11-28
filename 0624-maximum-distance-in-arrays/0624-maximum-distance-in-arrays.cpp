2dfec04528bfa911f7c7d63372fc5ab30d0f579e
유승선 <yssgood@naver.com>
2024-11-28 22:25:25 +0900

    Time: 0 ms (100%), Space: 108.2 MB (19.83%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int maxDistance(vector<vector<int>>& arrays) {
-        int answer = 0; 
-        int curr_min = arrays[0][0]; 
-        int curr_max = arrays[0].back();
+    int maxDistance(vector<vector<int>>& arrays) {  
+        int max_ = 0;  
+        int curr_min = arrays[0][0];
+        int curr_max = arrays[0].back(); 
         for(int i = 1; i < arrays.size(); i++){
-            int next_min = arrays[i][0];
-            int next_max = arrays[i].back(); 
-            answer = max(answer, max(abs(curr_max-next_min), abs(curr_min - next_max)));
-            curr_min = min(curr_min, next_min); 
-            curr_max = max(curr_max, next_max); 
+            int first = abs(curr_max - arrays[i][0]); 
+            int second = abs(curr_min - arrays[i].back());
+            max_ = max(max_, max(first, second)); 
+            curr_min = min(curr_min, arrays[i][0]); 
+            curr_max = max(curr_max, arrays[i].back());          
         }
-        return answer; 
+
+        return max_; 
     }
 };
