b4700a7bd6992f3fec8bc758bbcf8a1b3b6a7648
유승선 <yssgood@naver.com>
2024-11-30 12:30:38 +0900

    Time: 2 ms (39.04%), Space: 48.8 MB (40.14%) - LeetHub

--- 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
+++ 0186-reverse-words-in-a-string-ii/0186-reverse-words-in-a-string-ii.java
@@ -1,28 +1,24 @@
 class Solution {
     public void reverse(char[] s, int start, int end){
         while(start < end){
-            char tmp = s[start]; 
-            s[start] = s[end];
-            s[end] = tmp;
+            char t = s[start]; 
+            s[start] = s[end]; 
+            s[end] = t; 
             start++;
             end--; 
         }
     }
     public void reverseWords(char[] s) {
         reverse(s,0,s.length-1); 
-
-        int start = 0, end = 0; 
+        int start = 0; 
+        int end = 0; 
         while(end < s.length){
-            
-            while(end < s.length && Character.isLetterOrDigit(s[end])){
-                end++; 
+            if(s[end] == ' '){
+                reverse(s,start,end-1); 
+                start = end + 1; 
             }
-            //System.out.println(start + " " + end); 
-            reverse(s,start,end-1); 
-            start = end + 1; 
-
             end++; 
         }
-
+        reverse(s,start,end-1); 
     }
 }
