e8872ed926e14835f15c2d178ab60283065e9c72
유승선 <yssgood@naver.com>
2024-12-20 00:00:55 +0900

    Time: 1 ms (31.29%), Space: 44.5 MB (86.66%) - LeetHub

--- 3632-button-with-longest-push-time/3632-button-with-longest-push-time.java
+++ 3632-button-with-longest-push-time/3632-button-with-longest-push-time.java
@@ -1,19 +1,14 @@
 class Solution {
     public int buttonWithLongestTime(int[][] events) {
-        int curr = events[0][1]; 
+        int maxTime = events[0][1]; 
+        int currIndex = events[0][0]; 
         for(int i = 1; i < events.length; i++){
-            int next = events[i][1]; 
-            events[i][1] = next - curr; 
-            curr = next; 
-        }
-
-        Arrays.sort(events, (a,b)->{
-            if(a[1] == b[1]){
-                return a[0] - b[0]; 
+            int time = events[i][1] - events[i-1][1]; 
+            if(time > maxTime || time == maxTime && events[i][0] < currIndex){
+                currIndex = events[i][0]; 
+                maxTime = time; 
             }
-            return b[1] - a[1]; 
-        });
-
-        return events[0][0]; 
+        }
+        return currIndex; 
     }
 }
