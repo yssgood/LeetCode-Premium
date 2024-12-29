bdc7949ca2f5b99b78ffffb398b5746c3849c077
유승선 <yssgood@naver.com>
2024-12-29 15:17:45 +0900

    Time: 1 ms (83.65%), Space: 42.1 MB (29.57%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int p1 = 0, p2 = 0; 
+        StringBuilder sb = new StringBuilder();  
+        while(p1 < word1.length() || p2 < word2.length()){
+            if(p1 < word1.length()){
+                sb.append(word1.charAt(p1++)); 
+            }
+            if(p2 < word2.length()){
+                sb.append(word2.charAt(p2++)); 
+            }
+        }
+        return sb.toString(); 
+    }
+}
