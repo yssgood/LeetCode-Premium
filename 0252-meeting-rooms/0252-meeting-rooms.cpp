32b2eed1ba332155082a67a1eca7df1e1f387691
유승선 <yssgood@naver.com>
2025-01-24 23:12:55 +0900

    Time: 0 ms (100%), Space: 16 MB (49.74%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         if(intervals.empty()) return true; 
-        sort(intervals.begin(), intervals.end()); 
-        int currTime = intervals[0][1]; 
+        sort(intervals.begin(), intervals.end(),[](vector<int>& a, vector<int>& b){
+            return a[0] < b[0]; 
+        });
+
+        int currEnd = intervals[0][1]; 
 
         for(int i = 1; i < intervals.size(); i++){
-            int startTime = intervals[i][0]; 
-            int endTime = intervals[i][1]; 
+            int nextStart = intervals[i][0]; 
+            int nextEnd = intervals[i][1]; 
 
-            if(currTime  > startTime) return false; 
-            currTime = endTime; 
+            if(nextStart < currEnd) return false; 
+
+            currEnd = nextEnd; 
         }
+
         return true; 
     }
 };
