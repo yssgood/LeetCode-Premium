f2ba9c8fe9087f8f0d27fa853d7621a753126e9d
유승선 <yssgood@naver.com>
2025-01-24 23:05:43 +0900

    Time: 0 ms (100%), Space: 14 MB (98.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,20 +3,17 @@ public:
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size()-1; 
         while(left < right){
-            int mid = (right + left) / 2; 
-
+            int mid = (right + left) / 2;  
 
             if(nums[mid] > nums[right]){
                 left = mid + 1; 
             } else{
-                right = mid;
+                right = mid; 
             }
         }
 
-        //cout << left; 
-
-        if(target > nums[nums.size()-1]){
-            right = left; 
+        if(nums.back() < target){
+            right = left - 1; 
             left = 0; 
         } else{
             right = nums.size()-1; 
@@ -25,18 +22,14 @@ public:
         while(left <= right){
             int mid = (right + left) / 2; 
 
-            if(nums[mid] == target){
-                return mid; 
-            }
+            if(nums[mid] == target) return mid; 
 
-            if(nums[mid] < target){
-                left = mid + 1; 
+            if(nums[mid] > target){
+                right = mid - 1; 
             } else{
-                right = mid -1; 
+                left = mid + 1; 
             }
         }
-        return -1;
-
-        return 1; 
+        return -1; 
     }
 };
