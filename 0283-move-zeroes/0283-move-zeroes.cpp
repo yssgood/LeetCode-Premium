8e3845110cf080874d6cad386379672c3ebd78e6
유승선 <yssgood@naver.com>
2025-01-30 18:13:19 +0900

    Time: 0 ms (100%), Space: 23.9 MB (50.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int pos = 0;  
+        int zeroIndex = 0; 
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                nums[pos++] = nums[i];
-                nums[i] = 0; 
-            }
+            if(nums[i] != 0) swap(nums[zeroIndex++],nums[i]); 
         }
     }
 };
