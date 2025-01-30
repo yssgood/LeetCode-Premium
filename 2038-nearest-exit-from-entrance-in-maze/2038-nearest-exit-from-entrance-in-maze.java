4608d3e5dd1a93cddf5bf6ac7ddbfc2beefe1f7b
유승선 <yssgood@naver.com>
2025-01-30 18:00:38 +0900

    Time: 7 ms (71.63%), Space: 45.6 MB (78.51%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -1,40 +1,30 @@
 class Solution {
-    public static int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     public int nearestExit(char[][] maze, int[] entrance) {
         Queue<int[]> q = new LinkedList<>(); 
-        boolean visited[][] = new boolean[maze.length][maze[0].length]; 
-
-        q.add(new int[]{entrance[0], entrance[1],0}); 
-
-        visited[entrance[0]][entrance[1]] = true; 
+        q.add(new int[]{entrance[0],entrance[1],0}); 
+        maze[entrance[0]][entrance[1]] = '+'; 
 
         while(!q.isEmpty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
+                int[] front = q.poll(); 
 
-                int[] first = q.poll(); 
-
-                int x = first[0], y = first[1], dist = first[2]; 
-
-                //if((x != entrance[0] && y != entrance[1]) && (x == maze.length-1 || x == 0 || y == 0 || y == maze[0].length-1)) return dist; 
+                int x = front[0], y = front[1], dist = front[2]; 
                 
-                if(dist > 0 && x == maze.length-1 || dist > 0 && x == 0 || dist > 0 && y == maze[0].length-1 || dist > 0 && y == 0) return dist; 
+                for(int[] d : dir){
+                    int nX = x + d[0];
+                    int nY = y + d[1]; 
 
-                for(int[] p : dir){
+                    if(nX < 0 || nY < 0 || nX >= maze.length || nY >= maze[0].length || maze[nX][nY] == '+') continue; 
 
-                    int nX = x + p[0]; 
-                    int nY = y + p[1]; 
-                    
-                    if(nX < 0 || nX >= maze.length || nY < 0 || nY >= maze[0].length || visited[nX][nY] || maze[nX][nY] == '+') continue; 
+                    if(nX == 0 || nX == maze.length-1 || nY == 0 || nY == maze[0].length-1) return dist + 1; 
 
-                    visited[nX][nY] = true; 
+                    maze[nX][nY] = '+'; 
                     q.add(new int[]{nX,nY,dist+1}); 
                 }
-
-
             }
         }
-
 
         return -1; 
     }
