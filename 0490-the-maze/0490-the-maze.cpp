03e03a843a20ff286e64a6f9530359da775e0d00
유승선 <yssgood@naver.com>
2025-02-01 13:32:11 +0900

    Time: 3 ms (76.63%), Space: 24.7 MB (40.96%) - LeetHub

--- 0490-the-maze/0490-the-maze.cpp
+++ 0490-the-maze/0490-the-maze.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
-    bool visited[101][101]; 
+    vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
-        
-        queue<pair<int,int>> q; 
+        queue<vector<int>> q; 
+        vector<vector<bool>> visited(maze.size(),vector<bool>(maze[0].size(),false)); 
         q.push({start[0],start[1]}); 
         visited[start[0]][start[1]] = true; 
 
         while(!q.empty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
-                pair<int,int> curr = q.front();
+                vector<int> curr = q.front(); 
                 q.pop(); 
 
-                int x = curr.first, y = curr.second; 
+                int x = curr[0], y = curr[1]; 
 
-                for(pair<int,int>& p : dir){
-                    int nX = x + p.first; 
-                    int nY = y + p.second; 
+                for(vector<int>& d : dir){
+                    int nX = x + d[0]; 
+                    int nY = y + d[1]; 
 
-                    if(nX < 0 || nY < 0 || nX >= maze.size() || nY >= maze[0].size() || maze[nX][nY] == 1) continue; 
-
-                    while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] == 0){
-                        nX += p.first; 
-                        nY += p.second; 
+                    while(nX >= 0 && nY >= 0 && nX < maze.size() && nY < maze[0].size() && maze[nX][nY] != 1){
+                        nX += d[0]; 
+                        nY += d[1]; 
                     }
-                    
-                    //cout << nX << ' ' << nY << endl; 
 
-                    nX -= p.first;
-                    nY -= p.second; 
+                    nX -= d[0];
+                    nY -= d[1]; 
 
-                    
+                    if(nX == destination[0] && nY == destination[1]) return true; 
 
                     if(!visited[nX][nY]){
-                        visited[nX][nY] = true; 
                         q.push({nX,nY}); 
-                        if(nX == destination[0] && nY == destination[1]) return true; 
+                        visited[nX][nY] = true; 
                     }
                 }
             }
