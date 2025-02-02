c4ddd06d5798a652a86e87ed3f6d08f0c74d7ba4
유승선 <yssgood@naver.com>
2025-02-02 21:05:11 +0900

    Time: 0 ms (100%), Space: 17.2 MB (57.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = 0, sell = 0; 
         int answer = 0; 
-        int pointer = 0; 
-        int low = prices[0], high = prices[0]; 
-        while(pointer < prices.size()-1){
-
-            while(pointer < prices.size()-1 && prices[pointer] >= prices[pointer+1]) pointer++; 
-            low = prices[pointer]; 
-            while(pointer < prices.size()-1 && prices[pointer] <= prices[pointer+1]) pointer++; 
-            high = prices[pointer];
-            answer += high - low; 
-            pointer++; 
+        int bestPrice = prices[0]; 
+        for(int i = 1; i < prices.size(); i++){
+            int curr = prices[i]; 
+            if(curr - bestPrice > 0){
+                answer += (curr - bestPrice); 
+                bestPrice = INT_MAX; 
+            }
+            bestPrice = min(bestPrice, curr); 
         }
-
         return answer; 
     }
 };
