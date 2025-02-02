3d6315d1b141fa759b1b2f4b8e02c0a731184f3f
유승선 <yssgood@naver.com>
2025-02-02 21:13:05 +0900

    Time: 0 ms (100%), Space: 52.3 MB (30.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int minJump = 0; 
-        for(int i = nums.size()-2; i >= 0; i--){
-            minJump++;
-            if(nums[i] >= minJump){
-                minJump = 0; 
-            }
+        if(nums.size() <= 1) return true; 
+        int jump = 0; 
+        for(int i = 0; i < nums.size()-1; i++){
+            if(jump < i) break; 
+            jump = max(jump, i + nums[i]); 
+            if(jump >= nums.size()-1) return true; 
         }
-
-        if(minJump != 0){
-            return false; 
-        }
-
-        return true; 
+        return false; 
     }
 };
