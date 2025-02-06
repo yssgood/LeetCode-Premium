c0d61005193d0216a56210dde17483565511be6b
유승선 <yssgood@naver.com>
2025-02-06 21:21:19 +0900

    Time: 1 ms (83.62%), Space: 41.9 MB (58.75%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,14 +1,34 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder(); 
         int p1 = 0, p2 = 0; 
-        StringBuilder sb = new StringBuilder();  
-        while(p1 < word1.length() || p2 < word2.length()){
+        while(p1 < word1.length() && p2 < word2.length()){
+            char a = '!';  
+            char b = '!'; 
+
             if(p1 < word1.length()){
-                sb.append(word1.charAt(p1++)); 
+                a = word1.charAt(p1); 
             }
             if(p2 < word2.length()){
-                sb.append(word2.charAt(p2++)); 
+                b = word2.charAt(p2); 
             }
+
+            if(a != '!'){
+                sb.append(a); 
+            }
+            if(b != '!'){
+                sb.append(b); 
+            }
+
+            p1++;
+            p2++; 
+        }
+
+        if(p1 < word1.length()){
+            sb.append(word1.substring(p1)); 
+        } 
+        if(p2 < word2.length()){
+            sb.append(word2.substring(p2)); 
         }
         return sb.toString(); 
     }
