d1f3b9b85fe86100d09463a90a8bc170c0e9dcf8
유승선 <yssgood@naver.com>
2025-02-09 15:47:38 +0900

    Time: 0 ms (100%), Space: 42.1 MB (37.74%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,35 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        int min_len = Math.min(word1.length(), word2.length()); 
         StringBuilder sb = new StringBuilder(); 
-        int p1 = 0, p2 = 0; 
-        while(p1 < word1.length() && p2 < word2.length()){
-            char a = '!';  
-            char b = '!'; 
-
-            if(p1 < word1.length()){
-                a = word1.charAt(p1); 
-            }
-            if(p2 < word2.length()){
-                b = word2.charAt(p2); 
-            }
-
-            if(a != '!'){
-                sb.append(a); 
-            }
-            if(b != '!'){
-                sb.append(b); 
-            }
-
-            p1++;
-            p2++; 
+        for(int i = 0 ; i < min_len; i++){
+            sb.append(word1.charAt(i)); 
+            sb.append(word2.charAt(i)); 
         }
 
-        if(p1 < word1.length()){
-            sb.append(word1.substring(p1)); 
-        } 
-        if(p2 < word2.length()){
-            sb.append(word2.substring(p2)); 
+        if(word1.length() > word2.length()){
+            sb.append(word1.substring(min_len));
+        } else if(word1.length () < word2.length()){
+            sb.append(word2.substring(min_len)); 
         }
+
         return sb.toString(); 
     }
 }
