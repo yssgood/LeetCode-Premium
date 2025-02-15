58d0db620c95e4a6ebc6881553293a8ec49f4b2f
유승선 <yssgood@naver.com>
2025-02-15 20:36:18 +0900

    Time: 0 ms (100%), Space: 12.9 MB (93.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    void dfs(int left, int right,string add, vector<string>& answer, int n){
-        if(left == n && right == n){
-            answer.push_back(add);
+    void dfs(int& left, int& right, string& curr, vector<string>& answer, int n){
+        if((left + right) >= (n * 2)){
+            answer.push_back(curr); 
             return; 
         }
-        
-        if(left < n){
-            add += '(';
-            dfs(left+1,right,add,answer,n);
-            add.pop_back(); 
+
+        if(left + 1 <= n){
+            curr += '('; 
+            left++; 
+            dfs(left,right,curr,answer,n); 
+            left--; 
+            curr.pop_back(); 
         }
-        if(right < left){
-            add += ')';
-            dfs(left,right+1,add,answer,n); 
-            add.pop_back(); 
+
+        if(left > right){
+            curr += ')'; 
+            right++;
+            dfs(left,right,curr,answer,n); 
+            right--; 
+            curr.pop_back(); 
         }
+
     }
-public:
     vector<string> generateParenthesis(int n) {
-        vector<string> answer;
-        string add = ""; 
-        int left = 0, right = 0; 
-        dfs(left,right,add,answer,n); 
+        vector<string> answer; 
+        string curr = ""; 
+        int left = 0, right = 0;
+        dfs(left,right,curr,answer,n); 
         return answer; 
     }
 };
