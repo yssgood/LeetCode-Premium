d7c363bc6caccb4f739fa6a8f13a86f3432e8fe7
유승선 <yssgood@naver.com>
2025-03-04 21:51:05 +0900

    Time: 67 ms (57%), Space: 14.5 MB (46.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if(nums.size() <= 1) return 1; 
         vector<int> dp(nums.size(),1); 
-        int answer = 1; 
+        int max_ = 1; 
         for(int i = 1; i < nums.size(); i++){
-            //cout << nums[i] << ' '; 
             for(int j = 0; j < i; j++){
                 if(nums[j] < nums[i]){
-                    //cout << nums[i] << ' ' << nums[j] << endl; 
                     dp[i] = max(dp[i], dp[j] + 1); 
-                    answer = max(answer, dp[i]); 
+                    max_ = max(max_, dp[i]); 
                 }
             }
         }
-
-        // for(int i = 0; i < dp.size(); i++){
-        //     cout << dp[i] << ' '; 
-        // }
-
-        return answer; 
+        return max_; 
     }
 };
