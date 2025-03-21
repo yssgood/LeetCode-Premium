fcd946e6c7f9db29fe8b8cae507ea8c0a3666cb6
유승선 <yssgood@naver.com>
2025-03-21 20:35:18 +0900

    Time: 0 ms (100%), Space: 8.6 MB (33.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,30 +1,16 @@
-/*
-Distinct Ways
-
-Statement
-Given a target find a number of distinct ways to reach the target.
-
-Approach
-Sum all possible ways to reach the current state.
-
-routes[i] = routes[i-1] + routes[i-2], ... , + routes[i-k]
-
-*/
-
-
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1) return 1; 
-        if(n == 2) return 2; 
-
-        vector<int> dp(n+1);
-        dp[1] = 1; 
+        if(n <= 1) return 1; 
+        
+        vector<int> dp(n + 1, 0); 
+        dp[1] = 1;
         dp[2] = 2; 
 
         for(int i = 3; i <= n; i++){
             dp[i] = dp[i-1] + dp[i-2]; 
         }
+
         return dp[n]; 
     }
 };
