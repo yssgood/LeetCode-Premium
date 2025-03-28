2ea2662c3c0dad9310969c032aacb9c490c439aa
유승선 <yssgood@naver.com>
2025-03-28 20:10:50 +0900

    Time: 21 ms (12.24%), Space: 44.2 MB (61.74%) - LeetHub

--- 1483-rank-teams-by-votes/1483-rank-teams-by-votes.java
+++ 1483-rank-teams-by-votes/1483-rank-teams-by-votes.java
@@ -1,33 +1,41 @@
 class Solution {
     public String rankTeams(String[] votes) {
         Map<Character,int[]> hashMap = new HashMap<>(); 
-        
+        for(int i = 0; i < votes[0].length(); i++){
+            hashMap.put(votes[0].charAt(i), new int[votes[0].length()]);
+        }
+
         for(String s : votes){
             for(int i = 0; i < s.length(); i++){
+                // int[] vec = hashMap.get(s.charAt(i)); 
+                // vec[i]++; 
+                // hashMap.put(s.charAt(i), vec); 
                 hashMap.computeIfAbsent(s.charAt(i), k -> new int[votes[0].length()])[i]++; 
             }
         }
 
-        char[] toChar = votes[0].toCharArray(); 
+        String first = votes[0]; 
+        char[] tmp = first.toCharArray(); 
         List<Character> lst = new ArrayList<>(); 
-        for(char c : toChar) lst.add(c); 
+        for(char c : tmp) lst.add(c); 
+        
+        Collections.sort(lst,(a,b) -> {
+            int[] arrA = hashMap.get(a); 
+            int[] arrB = hashMap.get(b); 
 
-        Collections.sort(lst, (a,b) -> {
-            int[] rankA = hashMap.get(a); 
-            int[] rankB = hashMap.get(b); 
-            for(int i = 0; i < votes[0].length(); i++){
-                if(rankA[i] != rankB[i]){
-                    return rankB[i] - rankA[i]; 
+            for(int i = 0; i < arrA.length; i++){
+                if(arrA[i] != arrB[i]){
+                    return arrB[i] - arrA[i]; 
                 }
             }
 
             return a - b; 
-        }); 
+        });
+
 
         StringBuilder sb = new StringBuilder(); 
-        for(char c : lst){
-            sb.append(c); 
-        }
+        for(char c : lst) sb.append(c); 
+
 
         return sb.toString(); 
     }
