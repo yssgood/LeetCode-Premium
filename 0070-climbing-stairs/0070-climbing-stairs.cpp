320f0f67378e355106a111b404fe38d904dbd247
유승선 <yssgood@naver.com>
2025-05-04 19:40:40 +0900

    Time: 0 ms (100%), Space: 8.5 MB (33.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 1) return 1; 
-        
-        vector<int> dp(n + 1, 0); 
-        dp[1] = 1;
+        if(n < 2) return 1; 
+
+        vector<int> dp(n+1); 
+        dp[1] = 1; 
         dp[2] = 2; 
 
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2]; 
+            dp[i] = dp[i-2] + dp[i-1]; 
         }
-
         return dp[n]; 
     }
 };
