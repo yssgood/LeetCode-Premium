d111ab3500b82b7a3c9b0b3d0dde6361a2f68a3d
유승선 <yssgood@naver.com>
2025-05-05 11:57:38 +0900

    Time: 0 ms (100%), Space: 11.8 MB (52.56%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,14 +3,17 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if(obstacleGrid[0][0] == 1) return 0; 
         int m = obstacleGrid.size(), n = obstacleGrid[0].size(); 
+
         vector<vector<int>> dp(m,vector<int>(n,0)); 
         dp[0][0] = 1; 
+
         for(int j = 1; j < n; j++){
             if(obstacleGrid[0][j] == 1){
                 break; 
             }
             dp[0][j] = 1; 
         }
+
         for(int i = 1; i < m; i++){
             if(obstacleGrid[i][0] == 1){
                 break; 
@@ -18,10 +21,11 @@ public:
             dp[i][0] = 1; 
         }
 
+
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(obstacleGrid[i][j] != 1){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
                 }
             }
         }
