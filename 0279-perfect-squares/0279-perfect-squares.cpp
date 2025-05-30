6ae674344968d55e784429e54be775cb88da3acd
유승선 <yssgood@naver.com>
2025-05-30 22:59:45 +0900

    Time: 47 ms (58.51%), Space: 13.1 MB (67.41%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -3,17 +3,15 @@ public:
     int numSquares(int n) {
         if(n <= 1) return 1; 
 
-        vector<int> dp(n+1, INT_MAX); 
+        vector<int> dp(n + 1, INT_MAX); 
         dp[1] = 1; 
-
         for(int i = 2; i <= n; i++){
             for(int j = 1; j * j <= i; j++){
                 if(j * j == i){
-                    dp[i] = 1;
+                    dp[i] = 1; 
                     break; 
-                } else{
-                    dp[i] = min(dp[i], dp[i - j*j] + 1);
                 }
+                dp[i] = min(dp[i], dp[i - j * j] + 1); 
             }
         }
 
