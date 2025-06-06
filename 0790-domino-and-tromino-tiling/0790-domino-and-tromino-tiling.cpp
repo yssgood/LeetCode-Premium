b5931025379277be00bce39f7c843d3a059fc48c
유승선 <yssgood@naver.com>
2025-06-06 17:40:43 +0900

    Time: 2 ms (20.35%), Space: 9.4 MB (11.83%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,38 +1,19 @@
-/*
-Distinct Ways
-
-Statement
-Given a target find a number of distinct ways to reach the target.
-
-
-Approach
-Sum all possible ways to reach the current state.
-
-routes[i] = routes[i-1] + routes[i-2], ... , + routes[i-k]
-
-*/ 
-
-
 class Solution {
 public:
     int numTilings(int n) {
-        int MOD = 1'000'000'007;
-        if(n <= 2){
-            return n; 
-        }
-
-        long full[1001]; 
-        long part[1001]; 
+        if(n <= 2) return n; 
+        long MOD = pow(10,9) + 7; 
+        vector<long> half(n+1,0); 
+        vector<long> full(n+1,0);
 
         full[1] = 1; 
         full[2] = 2; 
-        part[2] = 1; 
+        half[2] = 1; 
 
-        for(int i = 3; i < n + 1; ++i){
-            full[i] = (full[i-1] + full[i-2] + 2 * part[i - 1]) % MOD; 
-            part[i] = (part[i-1] + full[i - 2]) % MOD; 
+        for(int i=  3; i <= n; i++){
+            full[i] = (full[i-1] + full[i-2] + 2 * half[i-1]) % MOD;
+            half[i] = (half[i-1] + full[i-2]) % MOD; 
         }
-
         return full[n]; 
     }
 };
