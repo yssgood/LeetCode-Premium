edebda82d63d71763117e6ddb1e9d8dce1733a09
유승선 <yssgood@naver.com>
2025-06-06 15:58:09 +0900

    Time: 3 ms (20.14%), Space: 9.1 MB (33.68%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length(); 
-        if(n == 0 || s[0] == '0') return 0; 
-
-        vector<int> dp(n + 1, 0); 
-
+        vector<int> dp(n+1,0); 
         dp[0] = 1; 
-        dp[1] = 1; 
-
-        for(int i = 2; i <= n; i++){
-            int oneDigit = s[i-1] - '0'; 
-            if(oneDigit >= 1 && oneDigit <= 9){
+        for(int i = 1; i <= n; i++){
+            int onesDigit = s[i-1] - '0'; 
+            if(onesDigit >= 1 && onesDigit <= 9){
                 dp[i] += dp[i-1]; 
             }
-            int twoDigit = (s[i-2] - '0') * 10 + (s[i-1] - '0');
-            if(twoDigit >= 10 && twoDigit <= 26){
-                dp[i] += dp[i - 2];
+
+            if(i - 2 >= 0){
+                int twoDigit = stoi(s.substr(i-2,2)); 
+                if(twoDigit >= 10 && twoDigit <= 26){
+                    dp[i] += dp[i-2]; 
+                }
             }
         }
 
