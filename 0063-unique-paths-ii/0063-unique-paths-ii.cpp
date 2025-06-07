300981fe5445ba5ce792070361a3aa17ee4ad781
유승선 <yssgood@naver.com>
2025-06-07 12:37:48 +0900

    Time: 0 ms (100%), Space: 11.9 MB (54.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if(obstacleGrid[0][0] == 1) return 0; 
         int m = obstacleGrid.size(), n = obstacleGrid[0].size(); 
-
-        vector<vector<int>> dp(m,vector<int>(n,0)); 
-        dp[0][0] = 1; 
-
-        for(int j = 1; j < n; j++){
-            if(obstacleGrid[0][j] == 1){
-                break; 
-            }
-            dp[0][j] = 1; 
-        }
-
-        for(int i = 1; i < m; i++){
-            if(obstacleGrid[i][0] == 1){
-                break; 
-            }
-            dp[i][0] = 1; 
-        }
-
-
+        vector<vector<int>> dp(m, vector<int>(n,0)); 
+        for(int i = 0; i < m && obstacleGrid[i][0] != 1; i++) dp[i][0] = 1; 
+        for(int j = 0; j < n && obstacleGrid[0][j] != 1; j++) dp[0][j] = 1; 
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(obstacleGrid[i][j] != 1){
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
+                if(obstacleGrid[i][j] != 1) dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
             }
         }
-
         return dp[m-1][n-1]; 
     }
 };
