98462f3efe7c37f785ebd78d30e8f1442c50af8d
유승선 <yssgood@naver.com>
2025-06-07 10:10:42 +0900

    Time: 2 ms (30.48%), Space: 13 MB (82.05%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        unordered_set<int> travelDays(days.begin(), days.end()); 
-        vector<int> dp(366,0); 
+        set<int> hashSet(days.begin(),days.end()); 
+        vector<int> dp(366,INT_MAX); 
 
-        for(int day = 1; day <= 365; day++){
-            if(!travelDays.count(day)){
-                dp[day] = dp[day-1];  
+        dp[0] = 0; 
+
+        for(int i = 1; i <= 365; i++){
+            if(!hashSet.count(i)){
+                dp[i] = dp[i-1]; 
             } else{
-                int cost1 = dp[day-1] + costs[0]; 
-                int cost2 = dp[max(0,day-7)] + costs[1];  
-                int cost3 = dp[max(0,day-30)] + costs[2]; 
-
-                dp[day] = min({cost1,cost2,cost3}); 
+                dp[i] = min(dp[i],dp[i-1] + costs[0]); 
+                dp[i] = min(dp[i],dp[max(0,i-7)] + costs[1]); 
+                dp[i] = min(dp[i],dp[max(0,i-30)] + costs[2]); 
             }
         }
+
 
         return dp[365]; 
     }
