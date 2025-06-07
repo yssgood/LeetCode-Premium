0e59ebf27bc7914b7e86613d7ded52962d7bf120
유승선 <yssgood@naver.com>
2025-06-07 16:50:22 +0900

    Time: 0 ms (100%), Space: 14.3 MB (96.18%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size(); 
-        for(int i = 1; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
-                int left = (j - 1 < 0) ? INT_MAX : matrix[i-1][j-1]; 
-                int top = matrix[i-1][j]; 
-                int right = (j + 1 >= matrix[i].size()) ? INT_MAX : matrix[i-1][j+1]; 
-                matrix[i][j] = min(left,min(top,right)) + matrix[i][j]; 
+        int n = matrix.size(); 
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int left = (j - 1 < 0) ? 100000 : matrix[i-1][j-1]; 
+                int middle = matrix[i-1][j]; 
+                int right = (j + 1 >= n) ? 100000 : matrix[i-1][j+1]; 
+                matrix[i][j] = min(left,min(middle,right)) + matrix[i][j]; 
             }
         }
-
-        return *min_element(matrix[m-1].begin(), matrix[m-1].end());
+        return *min_element(matrix[n-1].begin(), matrix[n-1].end()); 
     }
 };
