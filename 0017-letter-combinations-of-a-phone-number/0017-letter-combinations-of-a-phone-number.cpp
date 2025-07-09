4f7a3462eb4ba28046d23fb18c27167cd91a2baf
유승선 <yssgood@naver.com>
2025-07-09 21:45:27 +0900

    Time: 0 ms (100%), Space: 9 MB (87.79%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,31 @@
 class Solution {
+private:
+    map<char,vector<char>> hashMap = {{'2', {'a','b','c'}},
+                                    {'3', {'d','e','f'}},
+                                    {'4', {'g','h','i'}},
+                                    {'5', {'j','k','l'}},
+                                    {'6', {'m','n','o'}},
+                                    {'7', {'p','q','r','s'}},
+                                    {'8', {'t','u','v'}},
+                                    {'9', {'w','x','y','z'}}};
 public:
-    vector<string> answer; 
-    void dfs(string& digits, int index, map<char,vector<char>>& hashMap, string& tmp){
+    void dfs(string digits, string& tmp, vector<string>& answer, int index){
         if(tmp.length() >= digits.length()){
             answer.push_back(tmp);
             return; 
         }
 
-
         for(int i = 0; i < hashMap[digits[index]].size(); i++){
-            tmp += hashMap[digits[index]][i];
-            dfs(digits,index+1,hashMap,tmp);
-            tmp.pop_back(); 
+            tmp += hashMap[digits[index]][i]; 
+            dfs(digits,tmp,answer,index+1); 
+            tmp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {}; 
-        map<char,vector<char>> hashMap = {{'2', {'a','b','c'}},
-                                            {'3', {'d','e','f'}},
-                                            {'4', {'g','h','i'}},
-                                            {'5', {'j','k','l'}},
-                                            {'6', {'m','n','o'}},
-                                            {'7', {'p','q','r','s'}},
-                                            {'8', {'t','u','v'}},
-                                            {'9', {'w','x','y','z'}}};
-
+        vector<string> answer; 
         string tmp = ""; 
-        dfs(digits,0,hashMap,tmp); 
+        dfs(digits,tmp,answer,0); 
         return answer; 
     }
 };
