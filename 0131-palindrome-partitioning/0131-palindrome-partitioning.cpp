e1d632d2868a4b38eab41408a4d14f0d97bb293d
유승선 <yssgood@naver.com>
2025-07-09 22:01:42 +0900

    Time: 29 ms (92.27%), Space: 53.4 MB (93.64%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    vector<vector<string>> answer; 
-    bool isPalindrome(string& s){
-        int start = 0, end = s.length()-1; 
-        while(start < end){
-            if(s[start] != s[end]) return false; 
+    bool isPalindrome(string tmp){
+        int start = 0, end = tmp.length()-1;
+        while(start < end){  // Changed <= to < (small optimization)
+            if(tmp[start] != tmp[end]) return false;
             start++;
             end--; 
         }
         return true; 
     }
-    void dfs(string& s, vector<string>& container, int index){
+    
+    void dfs(string& s, vector<vector<string>>& answer, vector<string>& container, int index){
         if(index >= s.length()){
-            answer.push_back(container);
+            answer.push_back(container); 
             return; 
         }
+
         for(int i = index; i < s.length(); i++){
-            string tmp = s.substr(index, i - index + 1);
-            //cout << tmp << endl; 
-            if(isPalindrome(tmp)){
-                container.push_back(tmp);
-                dfs(s,container,i+1); 
+            // Extract substring from index to i
+            string substring = s.substr(index, i - index + 1);
+            
+            if(isPalindrome(substring)){
+                container.push_back(substring); 
+                dfs(s, answer, container, i + 1);  // Move to next position after i
                 container.pop_back(); 
             }
         }
     }
+    
     vector<vector<string>> partition(string s) {
-        vector<string> container;
-        dfs(s,container,0);
+        vector<vector<string>> answer; 
+        vector<string> container; 
+        dfs(s, answer, container, 0); 
         return answer; 
     }
 };
