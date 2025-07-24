f80a635b49950f0fd016e28dc49ba2ce9ab9f559
유승선 <yssgood@naver.com>
2025-07-24 23:42:43 +0900

    Time: 1 ms (83.05%), Space: 18.2 MB (65.68%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,42 +2,42 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> answer; 
-        vector<int> inward(numCourses,0); 
         queue<int> q; 
+        vector<int> inBound(numCourses); 
         vector<vector<int>> adj(numCourses); 
 
         for(vector<int>& v : prerequisites){
-            int from = v[1], to = v[0]; 
-            inward[to]++; 
+            int to = v[0], from = v[1]; 
+            inBound[to]++; 
             adj[from].push_back(to); 
         }
 
         for(int i = 0; i < numCourses; i++){
-            if(inward[i] == 0) q.push(i);  
+            if(inBound[i] == 0){
+                q.push(i); 
+                answer.push_back(i); 
+            }
         }
 
-        if(q.empty()) return {}; 
-
-        int visitedCourse = 0; 
+        int time = 0; 
 
         while(!q.empty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
-                int currNode = q.front(); 
+                int node = q.front(); 
                 q.pop(); 
-                answer.push_back(currNode); 
-                visitedCourse++; 
-                for(int next : adj[currNode]){
-                    inward[next]--; 
-                    if(inward[next] <= 0){
-                        q.push(next); 
+                time++; 
+                for(int nextNode : adj[node]){
+                    inBound[nextNode]--; 
+                    if(inBound[nextNode] == 0){
+                        q.push(nextNode); 
+                        answer.push_back(nextNode); 
                     }
                 }
             }
         }
 
-        if(visitedCourse != numCourses) return {}; 
-
+        if (time != numCourses) return {}; 
         return answer; 
     }
 };
