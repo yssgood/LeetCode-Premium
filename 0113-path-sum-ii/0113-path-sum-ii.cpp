a15b668e336fa46590b756c4e5286ec46a71329e
유승선 <yssgood@naver.com>
2025-08-16 11:08:37 +0900

    Time: 0 ms (100%), Space: 21 MB (46.71%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -14,26 +14,16 @@ public:
     void pathSumHelper(TreeNode* root, int targetSum, vector<vector<int>>& answer, vector<int>& container){       
         if(!root) return; 
         if(!root->left && !root->right){
-            //cout << root->val << ' ' << targetSum << endl; 
             if(targetSum == root->val){
-                //cout << root->val << ' ' << targetSum << endl; 
-                //for(int n : container) cout << n << ' '; 
-                //cout << endl; 
                 container.push_back(root->val); 
                 answer.push_back(container); 
-                container.pop_back();
-                return; 
-            } else{
-                //container.pop_back();
-                return; 
-            }
+                container.pop_back(); //why do we need this? 
+            } 
+            return; 
         }
 
         container.push_back(root->val); 
-        //for(int n : container) cout << n << ' ';
-        //cout << endl; 
         pathSumHelper(root->left, targetSum - root->val, answer, container);  
-        //container.pop_back(); 
         pathSumHelper(root->right, targetSum - root->val, answer, container); 
         container.pop_back(); 
     }
