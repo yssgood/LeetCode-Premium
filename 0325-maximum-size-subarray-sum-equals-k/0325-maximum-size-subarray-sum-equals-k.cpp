c153d48179982772e4b6764052361d7c5df2febf
유승선 <yssgood@naver.com>
2025-08-18 21:58:49 +0900

    Time: 167 ms (35.09%), Space: 130.8 MB (67.55%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.cpp
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int maxSubArrayLen(vector<int>& nums, int k) {
-        long long max_ = 0; 
-        map<long long,long long> hashMap; 
+        int maxLen = 0; 
+        unordered_map<long long, int> hashMap; 
+        hashMap[0] = -1;  // Handle subarrays starting from index 0
         
-        hashMap[nums[0]] = 0; 
+        // Build prefix sums AND store first occurrences in one pass
         for(int i = 1; i < nums.size(); i++){
             nums[i] = nums[i-1] + nums[i]; 
-            if(!hashMap.count(nums[i])) hashMap[nums[i]] = i; 
         }
         
-        for(int i = 0; i < nums.size(); i++){
-            long long target = (long long)nums[i] - k; 
-            if(nums[i] == k){
-                max_ = max(max_, (long long)i - 0 + 1); 
-            } else if(hashMap.count(target)){
-                max_ = max(max_, (long long)i - (hashMap[target] + 1) + 1); 
+        // Store first occurrences (but don't overwrite hashMap[0]!)
+        if(nums[0] != 0) {  // Only store if it won't overwrite our base case
+            hashMap[nums[0]] = 0;
+        }
+        for(int i = 1; i < nums.size(); i++){
+            if(!hashMap.count(nums[i])) {
+                hashMap[nums[i]] = i; 
             }
         }
-        //1, 0, 5, 3, 6
-        //1, 0, 5, 
-        //-2 -1 2 1 
-        //-2 -3 -1 0
-        return max_; 
+        
+        // Find maximum length
+        for(int i = 0; i < nums.size(); i++){
+            long long target = (long long)nums[i] - k; 
+            if(hashMap.count(target)){
+                maxLen = max(maxLen, i - hashMap[target]); 
+            }
+        }
+        return maxLen; 
     }
 };
