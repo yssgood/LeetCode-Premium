695ad65cadfa00159e56f68b162d471651ceb78e
유승선 <yssgood@naver.com>
2025-08-22 20:21:19 +0900

    Time: 0 ms (100%), Space: 12.9 MB (47.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,10 +11,24 @@
  */
 class Solution {
 public:
+    //idea : we move p and q if they meet conditions 
+    //if p AND q are both nullptr, then we have reached to the destination 
+    //if p == nullptr while q is not, then this is false condition (same thing applies to other)
+    //if their values are different, then it is false condition = 
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p && !q) return true; 
-        if(!p || !q) return false; 
-        if(p->val != q->val) return false; 
+        if(!p && !q){
+            return true; 
+        }
+
+        if(p == nullptr && q != nullptr || p != nullptr && q == nullptr){
+            return false; 
+        }
+
+        //if(p == nullptr || q == nullptr) if q is nullptr, think what happens 
+
+        if(p->val != q->val){
+            return false; 
+        }
 
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right); 
     }
