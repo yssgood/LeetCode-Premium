449b3931a5c6368f9b022eb30f2c8a59276aaf76
유승선 <yssgood@naver.com>
2025-08-23 21:20:28 +0900

    Time: 18 ms (85.68%), Space: 97.3 MB (10.39%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,40 @@
+class Bucket {
+public:
+    bool used = false;
+    int minval = numeric_limits<int>::max();  // same as INT_MAX
+    int maxval = numeric_limits<int>::min();  // same as INT_MIN
+};
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if (nums.empty() || nums.size() < 2) return 0;
+
+        int mini = *min_element(nums.begin(), nums.end()),
+            maxi = *max_element(nums.begin(), nums.end());
+
+        int bucketSize =
+            max(1, (maxi - mini) /
+                       ((int)nums.size() - 1));  // bucket size or capacity
+        int bucketNum = (maxi - mini) / bucketSize + 1;  // number of buckets
+        vector<Bucket> buckets(bucketNum);
+
+        for (auto&& num : nums) {
+            int bucketIdx =
+                (num - mini) / bucketSize;  // locating correct bucket
+            buckets[bucketIdx].used = true;
+            buckets[bucketIdx].minval = min(num, buckets[bucketIdx].minval);
+            buckets[bucketIdx].maxval = max(num, buckets[bucketIdx].maxval);
+        }
+
+        int prevBucketMax = mini, maxGap = 0;
+        for (auto&& bucket : buckets) {
+            if (!bucket.used) continue;
+
+            maxGap = max(maxGap, bucket.minval - prevBucketMax);
+            prevBucketMax = bucket.maxval;
+        }
+
+        return maxGap;
+    }
+};
