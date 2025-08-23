1017bf4152972478aabad3956b7e4ce33cfd3158
유승선 <yssgood@naver.com>
2025-08-24 08:07:58 +0900

    Time: 213 ms (58.24%), Space: 146.9 MB (42.14%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    int partition(vector<int>& arr, int low, int high){
-        int randomIndex = low + rand() % (high - low + 1);
-        swap(arr[low], arr[randomIndex]);  // Move random element to front
-        int pivot = arr[low]; 
-        int i = low + 1; 
-        int j = high; 
-
-        while (true) {
-            // Move i right: find element that should be on right side (> pivot)
-            while (i <= high && arr[i] <= pivot) {
-                i++;
-            }
-        
-            // Move j left: find element that should be on left side (<= pivot)
-            while (j >= low && arr[j] > pivot) {
-                j--;
-            }
-        
-            // If pointers crossed, partitioning is done
-            if (i >= j) break;
-            
-            // Swap the misplaced elements
-            swap(arr[i], arr[j]);
+    void mergeSort(vector<int>& nums, int left, int right){
+        if(left >= right){
+            return; 
         }
 
-        // Place pivot in its correct final position
-        swap(arr[low], arr[j]);
-        return j;    
+        int mid = (right + left) / 2; 
+        mergeSort(nums, left, mid); 
+        mergeSort(nums, mid + 1, right); 
+        merge(nums,left, mid,right); 
     }
-    void quickSort(vector<int>& arr, int low, int high){
-        if(low >= high) return; 
-        
-        int pivotIndex = partition(arr, low, high); 
-        quickSort(arr, low, pivotIndex - 1); 
-        quickSort(arr, pivotIndex + 1, high); 
+    void merge(vector<int>& nums, int left, int mid, int right){
+        vector<int> tmp(right - left + 1); 
+        int i = left; 
+        int j = mid + 1; 
+        int k = 0; 
+        while(i <= mid && j <= right){
+            if(nums[i] <= nums[j]){
+                tmp[k] = nums[i]; 
+                k++; 
+                i++; 
+            } else{
+                tmp[k] = nums[j]; 
+                k++; 
+                j++; 
+            }
+        }
+
+        while(i <= mid) tmp[k++] = nums[i++]; 
+        while(j <= mid) tmp[k++] = nums[j++]; 
+
+        for(int i = 0; i < k; i++){
+            nums[left + i] = tmp[i]; 
+        }
     }
     vector<int> sortArray(vector<int>& nums) {
-        quickSort(nums, 0, nums.size()-1);
+        mergeSort(nums, 0, nums.size()-1);
         return nums; 
     }
 };
