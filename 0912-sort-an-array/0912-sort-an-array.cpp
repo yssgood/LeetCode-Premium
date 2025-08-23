53f12234dc6796b8ab9eea52947aa39df7f19271
유승선 <yssgood@naver.com>
2025-08-24 08:52:09 +0900

    Time: 1828 ms (6.25%), Space: 74.1 MB (61.93%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    void mergeSort(vector<int>& nums, int left, int right){
-        if(left >= right){
-            return; 
-        }
-
-        int mid = (right + left) / 2; 
-        mergeSort(nums, left, mid); 
-        mergeSort(nums, mid + 1, right); 
-        merge(nums,left, mid,right); 
+    void quickSort(vector<int>& nums, int left, int right){
+        if(left >= right) return; //when no more to divide 
+        int pivotIndex = partition(nums,left,right); 
+        quickSort(nums,left,pivotIndex-1); 
+        quickSort(nums,pivotIndex+1,right); 
     }
-    void merge(vector<int>& nums, int left, int mid, int right){
-        vector<int> tmp(right - left + 1); 
-        int i = left; 
-        int j = mid + 1; 
-        int k = 0; 
-        while(i <= mid && j <= right){
-            if(nums[i] <= nums[j]){
-                tmp[k] = nums[i]; 
-                k++; 
-                i++; 
-            } else{
-                tmp[k] = nums[j]; 
-                k++; 
-                j++; 
-            }
-        }
+    int partition(vector<int>& nums, int left, int right){
+        //int pivot = nums[left]; 
+        int randomIndex = left + rand() % (right - left + 1);
+        swap(nums[left], nums[randomIndex]);  // Move random element to front
+        int pivot = nums[left]; 
+        int i = left + 1; 
+        int j = right; 
 
-        while(i <= mid) tmp[k++] = nums[i++]; 
-        while(j <= mid) tmp[k++] = nums[j++]; 
+        while(true){
+            while(i <= right && nums[i] <= pivot) i++; //allow duplicates to the left side 
+            while(j >= left && nums[j] > pivot) j--; //strictly larger elements on the right side
 
-        for(int i = 0; i < k; i++){
-            nums[left + i] = tmp[i]; 
+            if(i >= j) break; 
+            
+            swap(nums[i],nums[j]); 
         }
+        //at the end, all of the elements in the left side are smaller than the pivot 
+        //all of the elements in the right side are biger than the pivot 
+        //j is the index where my pivot should be 
+
+        swap(nums[left],nums[j]); 
+        //swap(pivot,nums[j]) will only try to swap the values, we need to change the location!
+        return j; 
     }
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        quickSort(nums, 0, nums.size()-1); 
         return nums; 
     }
 };
