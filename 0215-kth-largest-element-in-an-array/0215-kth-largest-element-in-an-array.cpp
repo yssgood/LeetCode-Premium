18f1c9f42b2a82171611a4c9702a7e4544e2c2a1
유승선 <yssgood@naver.com>
2025-08-24 00:32:54 +0900

    Time: 2749 ms (5%), Space: 59.1 MB (99.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        return quickSelect(nums, k);
+    int partition(vector<int>& arr, int low, int high) {
+        // Using leftmost element as pivot (can cause TLE!)
+        int pivot = arr[low];
+        int i = low + 1;
+        int j = high;
+        
+        while (true) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+            
+            if (i >= j) break;
+            
+            swap(arr[i], arr[j]);
+        }
+        
+        swap(arr[low], arr[j]);
+        return j;
     }
     
-    int quickSelect(vector<int>& nums, int k) {
-        int pivot = nums[rand() % nums.size()];
-        
-        vector<int> left;
-        vector<int> mid;
-        vector<int> right;
-        
-        for (int num: nums) {
-            if (num > pivot) {
-                left.push_back(num);
-            } else if (num < pivot) {
-                right.push_back(num);
-            } else {
-                mid.push_back(num);
-            }
+    int quickSelect(vector<int>& arr, int low, int high, int k) {
+        if (low == high) {
+            return arr[low];  // Base case: single element
         }
         
-        if (k <= left.size()) {
-            return quickSelect(left, k);
+        int pivotIndex = partition(arr, low, high);
+        
+        if (pivotIndex == k) {
+            return arr[pivotIndex];  // Found the k-th element!
         }
-        
-        if (left.size() + mid.size() < k) {
-            return quickSelect(right, k - left.size() - mid.size());
+        else if (k < pivotIndex) {
+            // Target is in left partition
+            return quickSelect(arr, low, pivotIndex - 1, k);
         }
+        else {
+            // Target is in right partition  
+            return quickSelect(arr, pivotIndex + 1, high, k);
+        }
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        // No random seed needed since we're using deterministic pivot
         
-        return pivot;
+        // Convert "k-th largest" to "index of k-th largest"
+        // For k-th largest in 0-indexed array: targetIndex = n - k
+        int n = nums.size();
+        int targetIndex = n - k;
         
+        return quickSelect(nums, 0, n - 1, targetIndex);
     }
 };
