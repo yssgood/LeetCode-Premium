a0c16141db10a7cd4a4907eeb5c61e603407bf0f
유승선 <yssgood@naver.com>
2025-09-07 12:04:50 +0900

    Time: 105 ms (9.26%), Space: 48.8 MB (8.1%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,45 +1,86 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n + 1);
+        //create graph strcuture -> ourVec 
         
-        for (vector<int>& v : times) {
-            int from = v[0], to = v[1], weight = v[2];
-            adj[from].push_back({to, weight});
+        vector<vector<pair<int,int>>> ourVec(n+1);
+        
+        for (int i = 0; i < times.size(); i++){
+            vector<int> inner = times[i];
+            int from = inner[0];
+            int to = inner[1];
+            int weight = inner[2];
+            ourVec[from].push_back({to,weight});{}
         }
         
-        vector<int> dist(n + 1, INT_MAX);
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
         
-        dist[k] = 0;
-        pq.push({0, k});  // {distance, node}
+        //create distance vector 
+        vector<int> distance(n+1,INT_MAX); 
         
-        while (!pq.empty()) {
-            vector<int> front = pq.top();
-            pq.pop();
-            
-            int d = front[0];
-            int node = front[1];
-            
-            //if (d > dist[node]) continue;
-            
-            for (auto& p : adj[node]) {
-                int neighbor = p.first;
-                int weight = p.second;
+        //declare priority_queue 
+        //{type,vector<type>,greater<>} -> pq (min-heap) 
+        //{type,vector<type>} -> pq (max-heap) 
+        priority_queue<vector<int>,vector<vector<int>>,greater<>> pq; 
+        
+        //set-up 
+        distance[k] = 0;
+        pq.push({0,k}); //{distance, node} 
+        
+        
+        //start your bfs using priority_queue
+        while (!pq.empty()){
+            int size = pq.size();
+            for (int i = 0; i < size; i++){
+                vector<int> curr = pq.top();
+                pq.pop();
                 
-                if (dist[node] + weight < dist[neighbor]) {
-                    dist[neighbor] = dist[node] + weight;
-                    pq.push({dist[neighbor], neighbor});
+                int d = curr[0]; 
+                int node = curr[1]; 
+                
+                for(pair<int,int> next :ourVec[node]){
+                    int neighbor = next.first; 
+                    int weight = next.second; 
+                    
+                    if(distance[node] + weight < distance[neighbor]){
+                        distance[neighbor] = distance[node] + weight; 
+                        pq.push({distance[neighbor], neighbor}); 
+                    }
+                    
                 }
+                 
             }
         }
         
-        int maxTime = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == INT_MAX) return -1;
-            maxTime = max(maxTime, dist[i]);
+        
+        int maxTime = 0; 
+        for(int i = 1; i <= n; i++){
+            if(distance[i] == INT_MAX) return -1; 
+            maxTime = max(maxTime, distance[i]); 
         }
         
-        return maxTime;
+        return maxTime; 
     }
 };
+
+
+
+/* 
+
+[0,1] [0,2] 
+
+ourVec 
+[0] -> {1,2} 
+[1] -> {0}
+[2] -> {0} 
+
+[2,1,1] [2,3,1] 
+
+ourVec 
+[1]
+[2] -> {(1,1),(3,1)} 
+[3] 
+
+[1,0,1,2,INT_MAX]
+maximum time from this distance vector -> entire time to travel all the nodes 
+
+*/ 
