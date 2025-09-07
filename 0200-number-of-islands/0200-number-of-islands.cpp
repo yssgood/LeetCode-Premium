4aa7fd7c995d133ad0ee8a67060ddc08a46ea5b1
유승선 <yssgood@naver.com>
2025-09-07 10:42:58 +0900

    Time: 35 ms (18.73%), Space: 23.3 MB (32.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<char>>& grid){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1') return; 
-        grid[i][j] = '0'; 
-        dfs(i + 1, j, grid);
-        dfs(i - 1, j, grid);
-        dfs(i, j + 1, grid);
-        dfs(i, j - 1, grid);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int count = 0; 
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == '1'){
-                    dfs(i,j,grid); 
-                    count++; 
+
+        int count = 0;
+        vector<pair<int,int>> possiblemoves = {{0,1},{0,-1},{1,0},{-1,0}};
+        
+        for (int i = 0; i < grid.size(); i++){
+            for (int j = 0; j < grid[i].size(); j++){
+                if (grid[i][j] == '1'){
+                    count++;
+                    queue<pair<int,int>> ourQ;
+                    ourQ.push({i,j});
+                    grid[i][j] = '0'; 
+                    while (!ourQ.empty()){
+                        pair<int,int> curr = ourQ.front();
+                        ourQ.pop(); 
+                        
+                        int x = curr.first; 
+                        int y = curr.second; 
+                        
+                        for (pair<int,int> possiblemove: possiblemoves){
+                            int nX = x + possiblemove.first; 
+                            int nY = y + possiblemove.second; 
+                            
+                            if (nX < grid.size() && nX >= 0 && nY < grid[x].size() && nY >= 0){
+                                if (grid[nX][nY] == '1'){
+                                    ourQ.push({nX,nY});
+                                    grid[nX][nY] = '0';
+                                }
+                            }
+                            
+                        }
+                    }
+                    
                 }
             }
         }
-        return count; 
+        return count;
     }
 };
