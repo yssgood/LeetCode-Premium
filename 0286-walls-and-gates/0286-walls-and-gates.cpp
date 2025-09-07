47d7b6ea7304b54cb00e89da8dbdfc0b3e8be977
유승선 <yssgood@naver.com>
2025-09-07 11:31:42 +0900

    Time: 27 ms (26.86%), Space: 25.9 MB (24.12%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.cpp
+++ 0286-walls-and-gates/0286-walls-and-gates.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
     void wallsAndGates(vector<vector<int>>& rooms) {
-        queue<vector<int>> q; 
-        for(int i = 0; i < rooms.size(); i++){
-            for(int j = 0; j < rooms[i].size(); j++){
-                if(rooms[i][j] == 0) q.push({i,j,0}); 
-            }
-        }
-
-        while(!q.empty()){
-            int size = q.size(); 
-            for(int i = 0; i < size; i++){
-                vector<int> curr = q.front(); 
-                q.pop(); 
-
-                int x = curr[0], y = curr[1], dist = curr[2]; 
-
-                for(vector<int>& d : dir){
-                    int nX = x + d[0]; 
-                    int nY = y + d[1]; 
-
-                    if(nX < 0 || nY < 0 || nX >= rooms.size()|| nY >= rooms[0].size() || rooms[nX][nY] == -1) continue; 
-
-                    if(rooms[nX][nY] > dist + 1){
-                        rooms[nX][nY] = dist + 1; 
-                        q.push({nX,nY,dist+1}); 
-                    }
+        vector<pair<int,int>> possibleMoves = {{1,0},{-1,0},{0,-1},{0,1}};
+        queue<vector<int>> ourQ;
+        for (int i = 0; i < rooms.size(); i++){
+            for (int j = 0; j < rooms[i].size(); j++){
+                if (rooms[i][j] == 0){
+                    ourQ.push({i,j,0});
                 }
             }
         }
+        
+        while(!ourQ.empty()){
+            int size = ourQ.size(); 
+            for(int i = 0; i < size; i++){
+                vector<int> curr = ourQ.front();
+                ourQ.pop();
+                for (pair<int,int> possibleMove: possibleMoves){
+                    int nX = possibleMove.first + curr[0];
+                    int nY = possibleMove.second + curr[1];
+                    if (nX < 0 || nX >= rooms.size() || nY < 0 || nY >= rooms[nX].size()){
+                        continue; 
+                    }
+                    if (rooms[nX][nY] == -1){
+                        continue;
+                    }
+                    if (curr[2] + 1 < rooms[nX][nY]){
+                        rooms[nX][nY] = curr[2] + 1;
+                        ourQ.push({nX,nY,rooms[nX][nY]});
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
     }
 };
+
+
+//add values to your queue 
+//bfs from the gates 
