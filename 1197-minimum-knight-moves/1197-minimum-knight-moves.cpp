f56351bc33fca872fc0157dba712452aba289759
유승선 <yssgood@naver.com>
2025-09-07 09:42:36 +0900

    Time: 1106 ms (21.36%), Space: 171.7 MB (23.86%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<pair<int,int>> dir = {{-2,1},{-2,-1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2}};
     int minKnightMoves(int x, int y) {
+        if(x == 0 && y == 0) return 0; 
         queue<vector<int>> q; 
         x = abs(x); //amount of moves we make to 5 5 and -5 -5 are same
         y = abs(y); 
@@ -16,9 +17,9 @@ public:
 
                 int xx = v[0], yy = v[1], dist = v[2]; 
 
-                if(xx == x && yy == y){
-                    return dist; 
-                }
+                // if(xx == x && yy == y){
+                //     return dist; 
+                // }
 
 
                 for(pair<int,int>& p : dir){
@@ -26,6 +27,9 @@ public:
                     int nY = yy + p.second; 
 
                     if(!set.count({nX,nY}) && nX >= -2 && nY >= -2){
+                        if(nX == x && nY == y){
+                            return dist + 1;  
+                        }
                         q.push({nX,nY,dist+1});  
                         set.insert({nX,nY}); 
                     }
