eb2cb1c12498367c52aac76826443cf5b3f3a4fe
유승선 <yssgood@naver.com>
2025-09-12 21:58:16 +0900

    Time: 11 ms (18.65%), Space: 18.1 MB (25.75%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        unordered_map<int,int> hashMap; 
-        int max_ = *max_element(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end()); 
+        map<int,int> hashMap; 
+        int max_ = *max_element(nums.begin(), nums.end()); 
         for(int n : nums) hashMap[n] += n; 
-        vector<int> dp(max_ + 1,0); 
-        for(int i = 1; i <= max_; i++){
-            dp[i] = max(dp[i-1], dp[max(0,i-2)] + hashMap[i]); 
+        vector<int> dp(max_ + 1, 0); 
+        dp[1] = hashMap[1]; 
+        for(int i = 2; i <= max_; i++){
+            dp[i] = max(dp[i-1], dp[i-2] + hashMap[i]); 
         }
-
         return dp[max_]; 
     }
 };
