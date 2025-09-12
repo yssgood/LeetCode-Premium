6e298174a3720116a77bb43a0398812c68867625
유승선 <yssgood@naver.com>
2025-09-12 21:43:51 +0900

    Time: 0 ms (100%), Space: 10.8 MB (5.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,12 +4,19 @@ public:
         int n = nums.size(); 
         if(n <= 1) return nums[0]; 
 
-        vector<int>dp(n+1,INT_MAX); 
-        dp[0] = 0; 
-        dp[1] = nums[0]; 
-        for(int i = 2; i <= n; i++){
-            dp[i] = max(dp[i-2] + nums[i-1], dp[i-1]); 
+        // vector<int>dp(n+1,INT_MAX); 
+        // dp[0] = 0; 
+        // dp[1] = nums[0]; 
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = max(dp[i-2] + nums[i-1], dp[i-1]); 
+        // }
+
+        vector<int>dp(n,INT_MAX); 
+        dp[0] = nums[0]; 
+        dp[1] = max(nums[0],nums[1]); 
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]); 
         }
-        return dp[n]; 
+        return dp[n-1]; 
     }
 };
