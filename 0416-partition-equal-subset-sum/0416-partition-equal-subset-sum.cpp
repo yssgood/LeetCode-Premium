ef9edfa50bfc84911d0c1a144dee5267d4f17621
유승선 <yssgood@naver.com>
2025-09-13 21:34:41 +0900

    Time: 159 ms (29.42%), Space: 17.3 MB (67.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -9,32 +9,29 @@ public:
         int target = sum / 2;
         int n = nums.size();
         
-        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+        // dp[i][j] = can we make sum j using first i items?
+        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
         
-        // Base case: can always make sum 0
-        for(int i = 0; i < n; i++) {
+        // Base case: can always make sum 0 (pick nothing)
+        for(int i = 0; i <= n; i++){
             dp[i][0] = true;
         }
         
-        // Base case: first item
-        for(int i = 1; i <= target; i++){
-            if(i == nums[0]) dp[0][i] = true; 
-        }
-        
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j <= target; j++) {
-                if(j < nums[i]) {
-                    dp[i][j] = dp[i-1][j];
-                } 
-                else if(j == nums[i]) {
-                    dp[i][j] = true;
-                }
-                else {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]];
+        for(int i = 1; i <= n; i++){
+            int num = nums[i-1];  // Row i uses item at index i-1
+            for(int j = 1; j <= target; j++){
+                // Don't take current item
+                dp[i][j] = dp[i-1][j];
+                
+                // Take current item (if possible)
+                if(j >= num){
+                    //dp[i][j] = dp[i][j] || dp[i-1][j-num];
+                    if(dp[i][j] == true) continue; 
+                    dp[i][j] = dp[i-1][j-num]; 
                 }
             }
         }
         
-        return dp[n-1][target];
+        return dp[n][target];
     }
 };
