312925e4ce3d9dba39bf3493be4135ba30927011
유승선 <yssgood@naver.com>
2025-09-13 19:24:38 +0900

    Time: 1 ms (13.8%), Space: 17.7 MB (31.09%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size(); 
-        vector<int> memo(n,-1); 
-        return min(solve(n-1,cost,memo), solve(n-2,cost,memo));  
-    }
-
-    int solve(int index, vector<int>& cost, vector<int>& memo){
-        if(index == 0) return cost[0]; 
-        if(index == 1) return cost[1];
-
-        if(memo[index] != -1) return memo[index]; 
-
-        int firstStep = solve(index-1,cost,memo); 
-        int secondStep = solve(index-2,cost,memo); 
-
-        memo[index] = min(firstStep, secondStep) + cost[index]; 
-        return memo[index];  
+        vector<int> dp(n,0); 
+        dp[0] = cost[0]; 
+        dp[1] = cost[1]; 
+        for(int i = 2; i < n; i++){
+            dp[i] = min(dp[i-1],dp[i-2]) + cost[i]; 
+        }
+        return min(dp[n-1],dp[n-2]); 
     }
 };
