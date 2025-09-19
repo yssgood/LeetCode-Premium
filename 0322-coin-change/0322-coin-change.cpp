3d930e06d06e6e7717a2b24d2c78f6d331178426
유승선 <yssgood@naver.com>
2025-09-19 20:55:22 +0900

    Time: 11 ms (98.11%), Space: 18 MB (73.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,33 +2,64 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size(); 
-        vector<vector<int>> ourVec(n+1, vector<int>(amount+1,INT_MAX-1));
-        for (int i = 0; i <= n; i++){
-            ourVec[i][0] = 0;
-        }
+        vector<int> memo(amount+1, INT_MAX-1); 
         
-        for (int i = 1; i <= n; i++){
+        memo[0] = 0; //min number coin to make 0 
+        
+        for (int i = 0; i < n; i++){
+            int currcoin = coins[i];
             for (int j = 1; j <= amount; j++){
-                int currcoin = coins[i-1];
-                int unchoose = ourVec[i-1][j];
-                if (j >= currcoin && ourVec[i][j-currcoin] != INT_MAX-1){
-                    int choose = 1 + ourVec[i][j-currcoin];
-                    //before you choose, add condition to prevent negative index
+                int unchoose = memo[j];
+                if (j >= currcoin){
+                    int choose = 1 + memo[j-coins[i]];
                     if (choose < unchoose){
-                        ourVec[i][j] = choose;
-                    } else{
-                        ourVec[i][j] = unchoose;
+                        memo[j] = choose;
                     }
-                    //ourVec[i][j] = min(choose, unchoose); 
-                } else{
-                    ourVec[i][j] = unchoose;   
-                }
+                    else {
+                        memo[j] = unchoose;
+                    } 
+                } 
             }
         }
         
-        if (ourVec[n][amount] == INT_MAX-1){
+        if (memo[amount] == INT_MAX-1){
             return -1;
         }
-        return ourVec[n][amount];
+        return memo[amount];
+
+        
+        
+        
+    
+        // vector<vector<int>> ourVec(n+1, vector<int>(amount+1,INT_MAX-1));
+        // for (int i = 0; i <= n; i++){
+        //     ourVec[i][0] = 0;
+        // }
+        
+        //2D -> 1D 
+        
+        // for (int i = 1; i <= n; i++){
+        //     for (int j = 1; j <= amount; j++){
+        //         int currcoin = coins[i-1];
+        //         int unchoose = ourVec[i-1][j];
+        //         if (j >= currcoin){
+        //             int choose = 1 + ourVec[i][j-currcoin];
+        //             //before you choose, add condition to prevent negative index
+        //             if (choose < unchoose){
+        //                 ourVec[i][j] = choose;
+        //             } else{
+        //                 ourVec[i][j] = unchoose;
+        //             }
+        //             //ourVec[i][j] = min(choose, unchoose); 
+        //         } else{
+        //             ourVec[i][j] = unchoose;   
+        //         }
+        //     }
+        // }
+        
+        // if (ourVec[n][amount] == INT_MAX-1){
+        //     return -1;
+        // }
+        // return ourVec[n][amount];
     }
 };
