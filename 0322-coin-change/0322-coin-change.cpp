a62485c5cfc70ddbced83a9054c1ea2eaaae2de9
유승선 <yssgood@naver.com>
2025-09-19 20:42:32 +0900

    Time: 44 ms (41.35%), Space: 53.1 MB (12.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,16 +2,33 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size(); 
-        vector<int> dp(amount+1,INT_MAX-1); 
-        dp[0] = 0; 
-        for(int i = 0; i < n; i++){
-            int currCoin = coins[i]; 
-            for(int j = 1; j <= amount; j++){
-                if(j >= currCoin){
-                    dp[j] = min(dp[j], dp[j - currCoin] + 1); 
+        vector<vector<int>> ourVec(n+1, vector<int>(amount+1,INT_MAX-1));
+        for (int i = 0; i <= n; i++){
+            ourVec[i][0] = 0;
+        }
+        
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= amount; j++){
+                int currcoin = coins[i-1];
+                int unchoose = ourVec[i-1][j];
+                if (j >= currcoin && ourVec[i][j-currcoin] != INT_MAX-1){
+                    int choose = 1 + ourVec[i][j-currcoin];
+                    //before you choose, add condition to prevent negative index
+                    if (choose < unchoose){
+                        ourVec[i][j] = choose;
+                    } else{
+                        ourVec[i][j] = unchoose;
+                    }
+                    //ourVec[i][j] = min(choose, unchoose); 
+                } else{
+                    ourVec[i][j] = unchoose;   
                 }
             }
         }
-        return dp[amount] == INT_MAX - 1 ? -1 : dp[amount]; 
+        
+        if (ourVec[n][amount] == INT_MAX-1){
+            return -1;
+        }
+        return ourVec[n][amount];
     }
 };
