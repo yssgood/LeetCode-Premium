def92fbb267a3285047ceba734ba35640e737b81
유승선 <yssgood@naver.com>
2025-09-20 21:03:02 +0900

    Time: 89 ms (6.43%), Space: 66 MB (14.76%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-
         vector<int> answer; 
-        
-        for(int i = 0; i < nums.size(); ++i){
-            nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]); 
-        }
-        
-        for (int i = 0; i < nums.size(); ++i){
-            if (nums[i] > 0){
-                answer.push_back(i+1);
+        map<int,int> hashMap; 
+        for(int n : nums) hashMap[n]++; 
+        for(int i = 1; i <= nums.size(); i++){
+            if(!hashMap.count(i)){
+                answer.push_back(i); 
             }
         }
-
         return answer; 
     }
 };
-
-
-
-
-
-        //4 3 2 7 8 2 3 1
-        //1 2 2 3 3 4 7 8
