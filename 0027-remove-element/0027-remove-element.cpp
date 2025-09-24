17c12bfb2ea1092b6961e5d33a25e21fbce7644e
유승선 <yssgood@naver.com>
2025-09-24 21:40:03 +0900

    Time: 0 ms (100%), Space: 11.7 MB (46.79%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int index = 0; 
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] != val){
-                nums[index++] = nums[i]; 
+                swap(nums[i], nums[index++]); 
             }
         }
         return index; 
