a2b50327300fb9e81d4d9856f6f9d8f440338e01
유승선 <yssgood@naver.com>
2025-09-24 23:05:44 +0900

    Time: 0 ms (100%), Space: 29.5 MB (83.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        //7 6 5 4 3 2 1 
-        //5 6 7 1 2 3 4 
-        k = k % (nums.size()); 
+        k = k % nums.size();  // Critical fix!
         reverse(nums.begin(), nums.end()); 
-        reverse(nums.begin(),nums.begin() + k); 
-        reverse(nums.begin()+k,nums.end()); 
+        reverse(nums.begin(), nums.begin()+k); 
+        reverse(nums.begin()+k, nums.end()); 
     }
 };
