4dbf8e6513d6f3bc7eccaf21ca26c112c6cce775
유승선 <yssgood@naver.com>
2025-09-27 20:49:17 +0900

    Time: 12 ms (23.1%), Space: 19.4 MB (79.33%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,7 +4,7 @@ public:
         if(nums.size() <= 2) return nums.size(); 
         int index = 2; 
         for(int i = 2; i < nums.size(); i++){
-            if(nums[i] != nums[index-2]){
+            if(nums[index-2] != nums[i]){
                 nums[index++] = nums[i]; 
             }
         }
