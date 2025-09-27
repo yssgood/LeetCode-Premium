c7141c81f5b761cdc3523f9a5d9c516ad46e7aef
유승선 <yssgood@naver.com>
2025-09-27 20:34:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index = 0; 
-        for(int i = m; i < m + n; i++){
-            nums1[i] = nums2[index++]; 
+        int index = m + n - 1; 
+        int p1 = m-1; 
+        int p2 = n-1;
+        if(p1 < 0){
+            nums1[0] = nums2[0]; 
+            return; 
         }
-        sort(nums1.begin(), nums1.end()); 
+        while(p2 >= 0){
+            //cout << p1 << ' ' << p2 << endl; 
+            if(nums1[p1] > nums2[p2]){
+                nums1[index--] = nums1[p1--]; 
+            } else{
+                nums1[index--] = nums2[p2--]; 
+            }
+        }
     }
 };
+
+
+//1 2 3 3 5 6         2 5 6 
+//  p1               p2
