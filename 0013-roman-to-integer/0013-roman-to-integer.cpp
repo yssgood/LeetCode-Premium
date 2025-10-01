d9ea36d38f2020da9afb871a94b0086534884080
유승선 <yssgood@naver.com>
2025-10-01 21:16:48 +0900

    Time: 15 ms (9.69%), Space: 13.4 MB (18.52%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> hashMap = {{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D', 500}, {'M', 1000}};
+
+        int answer = 0; 
+        for(int i = 0; i < s.length(); i++){
+            if(i + 1 < s.length() && hashMap[s[i]] < hashMap[s[i+1]]){
+                answer -= hashMap[s[i]]; 
+            } else{
+                answer += hashMap[s[i]]; 
+            }
+        }
+        
+        return answer; 
+    }
+};
