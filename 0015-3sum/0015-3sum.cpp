c676dcc58873ad01be186d9aac9a6071fcdae3fe
유승선 <yssgood@naver.com>
2025-10-02 22:36:19 +0900

    Time: 43 ms (86.59%), Space: 29.1 MB (47.83%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,29 +3,24 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> answer; 
         sort(nums.begin(), nums.end()); 
+        //-4 -1 -1 0 1 2 
         for(int i = 0; i < nums.size()-2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue; 
-            int curr = nums[i]; 
             int start = i + 1, end = nums.size()-1; 
             while(start < end){
-                int second = nums[start]; 
-                int third = nums[end];  
-
-                int sum = curr + second + third; 
+                int sum = nums[i] + nums[start] + nums[end]; 
 
                 if(sum > 0){
                     end--; 
                 } else if(sum < 0){
                     start++; 
                 } else{
-                    while(start < end && nums[start] == nums[start+1]) start++; 
-                    while(start < end && nums[end] == nums[end-1]) end--; 
-                    answer.push_back({curr,second,third}); 
-                    start++;
-                    end--; 
+                    answer.push_back({nums[i], nums[start], nums[end]}); 
+                    while(start + 1 < end && nums[start] == nums[start+1]) start++; 
+                    start++; 
                 }
             }
-        }
+        } 
         return answer; 
     }
 };
