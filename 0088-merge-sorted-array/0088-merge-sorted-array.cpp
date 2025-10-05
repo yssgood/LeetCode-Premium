b5e816786e195b9533f415ab2659daa2df8ef99c
유승선 <yssgood@naver.com>
2025-10-05 11:53:45 +0900

    Time: 0 ms (100%), Space: 12.2 MB (95.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index = m + n - 1; 
+        int last = m + n - 1; 
         int p1 = m-1; 
-        int p2 = n-1;
-        if(p1 < 0){
-            nums1[0] = nums2[0]; 
-            return; 
-        }
-        while(p2 >= 0){
-            //cout << p1 << ' ' << p2 << endl; 
+        int p2 = n-1; 
+        while(p1 >= 0 && p2 >= 0){
             if(nums1[p1] > nums2[p2]){
-                nums1[index--] = nums1[p1--]; 
+                //cout << "HH" << endl; 
+                nums1[last--] = nums1[p1--]; 
             } else{
-                nums1[index--] = nums2[p2--]; 
+                //cout << p1 << ' ' << p2 << endl; 
+                nums1[last--] = nums2[p2--]; 
             }
+        }
+
+        while(p2 >= 0){
+            nums1[last--] = nums2[p2--]; 
         }
     }
 };
-
-
-//1 2 3 3 5 6         2 5 6 
-//  p1               p2
