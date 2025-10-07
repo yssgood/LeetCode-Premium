e08cc0f32d83a39cfe5f679c7912fd4b939454ef
유승선 <yssgood@naver.com>
2025-10-07 22:18:59 +0900

    Time: 0 ms (100%), Space: 9.6 MB (63.16%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<pair<int,int>> dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> container; 
         int m = board.size(), n = board[0].size(); 
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 int neighbors = 0; 
@@ -11,21 +11,24 @@ public:
                     int nX = i + p.first; 
                     int nY = j + p.second; 
 
-                    if(nX >= 0 && nX < m && nY >= 0 && nY < n && board[nX][nY] == 1) neighbors++; 
+                    if(nX >= 0 && nX < m && nY >= 0 && nY < n && (board[nX][nY] == 1 || board[nX][nY] == 3)) neighbors++; 
                 }
+                
                 if(board[i][j] == 1){
-                    if(neighbors < 2) container.push_back({i,j,0});
-                    if(neighbors > 3) container.push_back({i,j,0}); 
+                    if(neighbors < 2 || neighbors > 3) board[i][j] = 3; // alive → dead (encode as 3)
                 }
                 if(board[i][j] == 0 && neighbors == 3){
-                    container.push_back({i,j,1}); 
+                    board[i][j] = 2; // dead → alive (encode as 2)
                 }
             }
         }
 
-        for(vector<int>& v : container){
-            int x = v[0], y = v[1], cell =v[2]; 
-            board[x][y] = cell; 
+        // decode states
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 2) board[i][j] = 1; // was dead, now alive
+                else if(board[i][j] == 3) board[i][j] = 0; // was alive, now dead
+            }
         }
     }
 };
