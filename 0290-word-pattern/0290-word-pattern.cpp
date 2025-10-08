0cb9c1d951a84399747055aae1dd7d1a0e502d21
유승선 <yssgood@naver.com>
2025-10-08 19:21:08 +0900

    Time: 0 ms (100%), Space: 8.9 MB (40.07%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        vector<string> vec; 
-        string tmp; 
+        vector<string> container; 
         stringstream ss(s); 
+        string tmp; 
         while(ss >> tmp){
-            vec.push_back(tmp); 
+            container.push_back(tmp); 
         }
-        map<string,char> reverseMap; 
-        map<char,string> hashMap; 
-        if(pattern.length()!= vec.size()) return false; 
-        for(int i = 0; i < pattern.length(); i++){
-            char curr = pattern[i]; 
-            string target = vec[i]; 
-            if(hashMap.count(curr) && hashMap[curr] != target) return false; 
-            if(reverseMap.count(target) && reverseMap[target] != curr) return false; 
 
-            hashMap[curr] = target;
-            reverseMap[target] = curr; 
+        map<char,string> patternMap; 
+        map<string,char> stringMap; 
+
+        if(container.size() != pattern.length()) return false; 
+
+        for(int i = 0; i < pattern.length(); i++){
+            char p = pattern[i]; 
+            string compare = container[i]; 
+
+            if(patternMap.count(p) && patternMap[p] != compare || stringMap.count(compare) && stringMap[compare] != p) return false; 
+
+            patternMap[p] = compare; 
+            stringMap[compare] = p; 
         }
         return true; 
     }
