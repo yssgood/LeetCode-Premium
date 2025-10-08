ff122ae0ff455b0aacf33d912d9d8378269570f9
유승선 <yssgood@naver.com>
2025-10-08 21:43:05 +0900

    Time: 0 ms (100%), Space: 9.2 MB (40.81%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if(nums.size() == 0) return {}; 
         vector<string> answer; 
-        int curr = nums[0]; 
-        int start = 0; 
-        for(int i = 1; i < nums.size(); i++){
-            int next = curr + 1; 
-            if(nums[i] != next){
-                string first = to_string(nums[start]); 
-                string second = to_string(curr); 
-                if(i - start > 1){
-                    answer.push_back(first + "->" + second); 
-                }  else{
-                    answer.push_back(second); 
-                }
-                start = i; 
+        for(int i = 0; i < nums.size(); i++){
+            int start = nums[i]; 
+            while(i < nums.size()-1 && nums[i] + 1 == nums[i+1]){
+                i++; 
             }
-            curr = nums[i]; 
-        }
-        int n = nums.size()-1; 
-        string first = to_string(nums[start]); 
-        string second = to_string(curr); 
-        if(n - start >= 1){
-            answer.push_back(first + "->" + second); 
-        } else{
-            answer.push_back(second); 
+
+            if(start != nums[i]){
+                answer.push_back(to_string(start) + "->" + to_string(nums[i])); 
+            } else{
+                answer.push_back(to_string(start)); 
+            }
         }
         return answer; 
     }
