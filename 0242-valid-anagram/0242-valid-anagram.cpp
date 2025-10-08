e982f825b0b8b8bbd0b70a2a0f4e1c436e28de82
유승선 <yssgood@naver.com>
2025-10-08 19:25:33 +0900

    Time: 6 ms (24.92%), Space: 9.7 MB (74.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> hashMap; 
-        for(char& c : s) hashMap[c]++; 
-        for(char& c : t) hashMap[c]--; 
-
-        for(auto& it : hashMap){
-            //cout << it.first << ' ' << it.second << endl; 
-            if(it.second < 0 || it.second > 0) return false; 
+        map<char,int> sMap; 
+        for(char c : s) sMap[c]++;
+        for(char c : t) sMap[c]--; 
+        for(auto& it : sMap){
+            if(it.second != 0) return false; 
         }
-        return true; 
+        return true;
     }
 };
