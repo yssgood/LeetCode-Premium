e5bc327d49016ff7b31d58a4b83d4a2b621a1e20
유승선 <yssgood@naver.com>
2025-10-09 23:01:02 +0900

    Time: 0 ms (100%), Space: 17.1 MB (61.68%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,49 +1,22 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        int answer = 0; 
-        stack<int> numStack; 
+        stack<int> st;
+        
         for(string& t : tokens){
-            if(t == "+"){
-                int first = numStack.top(); 
-                numStack.pop(); 
-                int second = numStack.top(); 
-                numStack.pop(); 
-                numStack.push(second + first); 
-            } else if(t == "-"){
-                int first = numStack.top(); 
-                numStack.pop(); 
-                int second = numStack.top(); 
-                numStack.pop(); 
-                //cout << first << ' ' << second << ' ' << (second + first) << endl; 
-
-                numStack.push(second - first); 
-            } else if(t == "*"){
-                int first = numStack.top(); 
-                numStack.pop(); 
-                int second = numStack.top(); 
-                numStack.pop(); 
-                numStack.push(second * first); 
-            } else if(t == "/"){
-                int first = numStack.top(); 
-                numStack.pop(); 
-                int second = numStack.top(); 
-                numStack.pop(); 
-                //cout << first << ' ' << second << ' ' << (second / first) << endl; 
-                numStack.push(second / first); 
-            } else{
-                int num = 0; 
-                if(t[0] == '-'){
-                   num = stoi(t.substr(1)) * -1;  
-                } else{
-                    num = stoi(t); 
-                }
-                numStack.push(num); 
+            if(t == "+" || t == "-" || t == "*" || t == "/"){
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+                
+                if(t == "+") st.push(a + b);
+                else if(t == "-") st.push(a - b);
+                else if(t == "*") st.push(a * b);
+                else st.push(a / b);
+            } else {
+                st.push(stoi(t));
             }
         }
-
-        cout << numStack.top(); 
-
-        return numStack.top(); 
+        
+        return st.top();
     }
 };
