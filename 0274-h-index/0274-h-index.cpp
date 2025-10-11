d4929a7b86ff188b995e9d1c0352731bc4c8b927
유승선 <yssgood@naver.com>
2025-10-11 19:34:55 +0900

    Time: 3 ms (13.96%), Space: 12.3 MB (37.65%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        //0 1 3 5 6
-        sort(citations.begin(), citations.end()); 
+        sort(citations.begin(), citations.end());
         for(int i = 0; i < citations.size(); i++){
-            int h = citations.size() - i; 
-            if(citations[i] >= h){
-                return h; 
-            }
+            int hIndex = citations.size() - i; 
+            if(citations[i] >= hIndex) return hIndex; 
         }
-        return 0; 
+        return 0;  
     }
 };
