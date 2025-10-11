32bfdffa55b5b6e7718d7b450a664a585fe07b61
유승선 <yssgood@naver.com>
2025-10-11 19:47:20 +0900

    Time: 9 ms (34.46%), Space: 13.5 MB (7.46%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> hashMap = {{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D', 500}, {'M',1000}}; 
-        int answer = 0; 
+        map<char,int> hashMap = {{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D', 500}, {'M', 1000}}; 
+        int answer = 0;  
+
         for(int i = 0; i < s.length(); i++){
-            if(i + 1 < s.length() && hashMap[s[i]] < hashMap[s[i+1]]){
-                answer += hashMap[s[i+1]] - hashMap[s[i]]; 
+            int first = hashMap[s[i]]; 
+            int second = i < s.length()-1 ? hashMap[s[i+1]] : INT_MIN; 
+            if(first < second){
+                answer += second - first; 
                 i++; 
             } else{
-                answer += hashMap[s[i]]; 
+                answer += first; 
             }
         }
+
         return answer; 
     }
 };
