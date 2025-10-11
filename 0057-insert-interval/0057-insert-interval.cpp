8cd613f032e302ec73f5ef7ccdef10eba5860952
유승선 <yssgood@naver.com>
2025-10-11 20:16:37 +0900

    Time: 0 ms (100%), Space: 21.6 MB (94.11%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if(intervals.empty()) return {{newInterval[0],newInterval[1]}}; 
         vector<vector<int>> answer; 
-        bool flag = false; 
-        for(int i = 0; i < intervals.size(); i++){
-            vector<int> curr = intervals[i]; 
-            if(curr[1] >= newInterval[0] && !flag){
-                flag = true; 
-                if(newInterval[1] < curr[0]){
-                    answer.push_back(newInterval); 
-                    answer.push_back(curr); 
-                    continue; 
-                }
-                curr[0] = min(curr[0],newInterval[0]); 
-                curr[1] = max(curr[1],newInterval[1]); 
-                while(i + 1 < intervals.size() && newInterval[1] >= intervals[i+1][0]){
-                    curr[1] = max(curr[1],intervals[i+1][1]); 
-                    i++; 
-                }
+        int index = 0;
+        for(int i = index; i < intervals.size(); i++){
+            if(intervals[i][1] < newInterval[0]){
+                answer.push_back(intervals[i]);
+                index++; 
             }
-            answer.push_back(curr); 
         }
-        if(!flag) answer.push_back(newInterval);
-        return answer;  
+
+        for(int i = index; i < intervals.size(); i++){
+            int start = intervals[i][0]; 
+            if(start <= newInterval[1]){
+                newInterval[0] = min(newInterval[0], start);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+                index++; 
+            }
+        }
+
+        answer.push_back(newInterval); 
+
+        for(int i = index; i < intervals.size(); i++){
+            answer.push_back(intervals[i]); 
+        }
+
+        return answer; 
     }
 };
