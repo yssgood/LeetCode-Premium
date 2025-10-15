1e545ea19d694be8762bf8ff9a9ef98d6d961082
유승선 <yssgood@naver.com>
2025-10-15 21:29:31 +0900

    Time: 0 ms (100%), Space: 11.2 MB (72.3%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -1,33 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(!head || left == right) return head;
-        
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* prev = dummy;
-        
-        // Move to node before left
-        for(int i = 1; i < left; i++) {
-            prev = prev->next;
+        if(!head || left == right) return head; 
+        ListNode* dummy = new ListNode(-1); 
+        dummy->next = head; 
+        ListNode* prev = dummy; 
+        for(int i = 1; i < left; i++){
+            prev = prev->next; 
         }
-        
-        // Reverse the sublist
-        ListNode* curr = prev->next;
-        ListNode* next = nullptr;
-        ListNode* reversedHead = nullptr;
-        
-        for(int i = left; i <= right; i++) {
-            next = curr->next;
-            curr->next = reversedHead;
-            reversedHead = curr;
-            curr = next;
+        ListNode* curr = prev->next; 
+        ListNode* nextHeader = nullptr; 
+
+        for(int i = left; i <= right; i++){
+            ListNode* tmp = curr->next; 
+            curr->next = nextHeader; 
+            nextHeader = curr; 
+            curr = tmp; 
         }
-        
-        // Connect back
-        prev->next->next = curr;  // tail of reversed connects to rest
-        prev->next = reversedHead;  // prev connects to new head
-        
-        return dummy->next;
+
+        prev->next->next = curr; 
+        prev->next = nextHeader; 
+
+        return dummy->next; 
     }
 };
