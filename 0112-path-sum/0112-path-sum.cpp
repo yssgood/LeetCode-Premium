0f81ae388f7f3fffe0ed57ccbda5812c28948e59
유승선 <yssgood@naver.com>
2025-10-21 22:00:47 +0900

    Time: 0 ms (100%), Space: 21.4 MB (67.44%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,17 +11,18 @@
  */
 class Solution {
 public:
-    //our idea, decrease targetSum by its root 
-    //if we reach to correct leaf, what will happen to targetSum? 
-    //we need to try this both to left and right
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false; 
 
-        if(root->left == nullptr && root->right == nullptr){
-            if(root->val == targetSum) return true; 
+        if(!root->left && !root->right){
+            if(targetSum - root->val == 0) return true; 
             return false; 
         }
 
-        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val); 
+        if(hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val)){
+            return true; 
+        }
+
+        return false; 
     }
 };
