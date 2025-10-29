a135c3f88429a4cf2a3860290d029df5d29a3b75
유승선 <yssgood@naver.com>
2025-10-29 22:02:43 +0900

    Time: 0 ms (100%), Space: 71.7 MB (52.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int max_sum = nums[0]; 
-        int currSum = nums[0]; 
+        int curr_sum = nums[0]; 
         for(int i = 1; i < nums.size(); i++){
-            int includedSum = currSum + nums[i]; 
-            max_sum = max(max_sum, max(includedSum,nums[i])); 
-            currSum = max(includedSum, nums[i]); 
+            int includeSum = nums[i] + curr_sum; 
+            max_sum = max(max_sum, max(includeSum, nums[i])); 
+            curr_sum = max(includeSum, nums[i]); 
         }
-
         return max_sum; 
     }
 };
