edc5681794bbb6fe067563b5569eae58f740916e
유승선 <yssgood@naver.com>
2025-11-04 20:47:57 +0900

    Time: 6 ms (73.06%), Space: 46.9 MB (5.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,41 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int courses = 0; 
         List<List<Integer>> adj = new ArrayList<>(); 
         for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>()); 
         }
 
-        int[] inWard = new int[numCourses]; 
-
-        for(int[] p : prerequisites){
-            int from = p[1]; 
-            int to = p[0]; 
+        int[] inOrder = new int[numCourses]; 
+        
+        for(int[] course : prerequisites){
+            int to = course[0]; 
+            int from = course[1]; 
 
             adj.get(from).add(to); 
-            inWard[to]++; 
+            inOrder[to]++; 
         }
 
         Queue<Integer> q = new LinkedList<>(); 
         for(int i = 0; i < numCourses; i++){
-            if(inWard[i] == 0) q.add(i); 
+            if(inOrder[i] == 0) q.add(i); 
         }
 
         if(q.isEmpty()) return false; 
 
         while(!q.isEmpty()){
             int size = q.size(); 
-
             for(int i = 0; i < size; i++){
-                courses++; 
-                int curr = q.poll(); 
-
-                for(int v : adj.get(curr)){
-                    inWard[v]--; 
-                    if(inWard[v] == 0){
-                        q.add(v); 
+                int first = q.poll(); 
+                for(int next : adj.get(first)){
+                    inOrder[next]--; 
+                    if(inOrder[next] == 0){
+                        q.add(next); 
                     }
                 }
             }
         }
 
-
-        return courses == numCourses ? true : false; 
+        for(int n : inOrder) if(n > 0) return false; 
+        return true; 
     }
 }
