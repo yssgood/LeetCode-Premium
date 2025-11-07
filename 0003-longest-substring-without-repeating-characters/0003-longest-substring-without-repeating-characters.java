317a813d9f296b9703ae175f3ccbc4aa1cf3bbc1
유승선 <yssgood@naver.com>
2025-11-07 23:51:38 +0900

    Time: 10 ms (14.17%), Space: 46.2 MB (5.31%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,25 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> hashMap = new HashMap<>();
-        int start = 0;
-        int end = 0;
-        int answer = 0;
-
-        while (end < s.length()) {
-            // 현재 문자를 해시맵에 추가 또는 갱신
-            hashMap.put(s.charAt(end), hashMap.getOrDefault(s.charAt(end), 0) + 1);
-
-            // 중복 문자가 있을 경우, 시작 위치를 앞으로 이동
-            while (hashMap.get(s.charAt(end)) > 1) {
-                hashMap.put(s.charAt(start), hashMap.get(s.charAt(start)) - 1);
-                start++;
+        int start = 0, end = 0; 
+        int answer = 0; 
+        Map<Character,Integer> hashMap = new HashMap<>();
+        while(end < s.length()){
+            hashMap.put(s.charAt(end), hashMap.getOrDefault(s.charAt(end),0) + 1); 
+            while(hashMap.get(s.charAt(end)) > 1){
+                hashMap.put(s.charAt(start), hashMap.get(s.charAt(start)) - 1); 
+                if(hashMap.get(s.charAt(start)) <= 0){
+                    hashMap.remove(s.charAt(start)); 
+                }
+                start++; 
             }
 
-            // 최대 길이 갱신
-            answer = Math.max(answer, end - start + 1);
-            end++;
+            answer = Math.max(answer, end - start + 1); 
+            end++; 
         }
-
-        return answer;
+        return answer; 
     }
 }
