14212ca5ca115d4a33ae37cda4de7b71c33aa8af
유승선 <yssgood@naver.com>
2025-11-07 22:33:40 +0900

    Time: 2 ms (86.21%), Space: 45.7 MB (6.43%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,22 @@
 class Solution {
-    void dfs(int[] candidates, List<List<Integer>> answer, List<Integer> container, int target, int index){
-        if(target <= 0){
-            //System.out.println(container + " " +  target);  
-            if(target == 0){
-                answer.add(new ArrayList<>(container)); 
-                //System.out.println(container + " " +  target);  
-            }
-            return; 
-        }
-        for(int i = index; i < candidates.length; i++){
-            target -= candidates[i]; 
-            container.add(candidates[i]); 
-            dfs(candidates, answer, container, target, i); 
-            target += candidates[i];
-            container.removeLast(); 
-        }
-    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> answer = new ArrayList<>(); 
         List<Integer> container = new ArrayList<>(); 
-        dfs(candidates,answer,container,target,0);
+        dfs(candidates, target, answer, container, 0); 
         return answer; 
+    }
+    void dfs(int[] candidates, int target, List<List<Integer>> answer, List<Integer> container, int index){
+        if(target <= 0){
+            if(target == 0) answer.add(new ArrayList<>(container)); 
+            return; 
+        }
+
+        for(int i = index; i < candidates.length; i++){
+            target -= candidates[i]; 
+            container.add(candidates[i]); 
+            dfs(candidates, target, answer, container, i); 
+            target += candidates[i]; 
+            container.removeLast();  
+        }
     }
 }
