89f290fbda1826c64ffc0cab137ab6b7cf6188d9
유승선 <yssgood@naver.com>
2025-11-07 22:39:05 +0900

    Time: 0 ms (100%), Space: 41.9 MB (8.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] matrix = new int[m][n]; 
+        for(int j = 0; j < n; j++){
+            matrix[0][j] = 1; 
+        }
+        for(int i = 0; i < m; i++){
+            matrix[i][0] = 1; 
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]; 
+            }
+        }
+        return matrix[m-1][n-1]; 
+    }
+}
