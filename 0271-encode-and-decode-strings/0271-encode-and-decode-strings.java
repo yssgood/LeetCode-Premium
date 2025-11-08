49abe4df0c525255eb07dd6ee9d9010d7fe3d680
유승선 <yssgood@naver.com>
2025-11-08 18:24:47 +0900

    Time: 16 ms (16.96%), Space: 53.1 MB (5.49%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -2,26 +2,31 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder sb = new StringBuilder(); 
+        String res = ""; 
         for(String s : strs){
-            sb.append(s.length()).append('#').append(s); 
+            if(s.equals("")){
+                res += "??/!!/";
+            } else{
+                res += s + "/!!/"; 
+            }
         }
-        return sb.toString(); 
+        //System.out.print(res); 
+        return res; 
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> decoded = new ArrayList<>();
-        int i = 0; 
-        while(i < s.length()){
-            int delimiterPos = s.indexOf('#',i);
-            int length = Integer.parseInt(s.substring(i,delimiterPos)); 
-            int start = delimiterPos + 1; 
-            String str = s.substring(start, start + length); 
-            decoded.add(str); 
-            i = start + length; 
+        List<String> res = new ArrayList<>(); 
+        String[] split = s.split("/!!/"); 
+        for(String strs : split){
+            if(strs.equals("??")){
+                res.add(""); 
+            } else{
+                res.add(strs); 
+            }
         }
-        return decoded; 
+
+        return res; 
     }
 }
 
