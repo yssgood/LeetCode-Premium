995166a5592e28d10c4baf7d71f88f807e930dd6
유승선 <yssgood@naver.com>
2025-11-10 00:15:46 +0900

    Time: 9 ms (21.55%), Space: 65.2 MB (8.47%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,36 +2,36 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> answer = new ArrayList<>(); 
         List<String> container = new ArrayList<>(); 
-        dfs(s,container,answer,0); 
+        dfs(s,answer,container,0); 
         return answer; 
     }
-    void dfs(String s, List<String> container, List<List<String>> answer, int index){
-        if(index == s.length()){
-            answer.add(new ArrayList<>(container)); 
+    void dfs(String s, List<List<String>> answer, List<String> container, int index){
+        if(index >= s.length()){
+            if(container.size() > 0) answer.add(new ArrayList<>(container)); 
             return; 
         }
 
         for(int i = index; i < s.length(); i++){
-            String candidate = s.substring(index, i + 1); 
-            if(isPalindrome(candidate)){
-                container.add(candidate); 
-                dfs(s,container,answer,i+1); 
+            String subString = s.substring(index,i+1); 
+            if(isPalindrome(subString)){
+                container.add(subString); 
+                dfs(s,answer,container,i+1); 
                 container.removeLast(); 
             }
-            //String candidate = s.substring(i,i+1) //? 
-            //check if they are parition 
-            //add it to my contianer
-            //do the backtracking to the next character 
-            //pop the existing element 
         }
     }
+
     boolean isPalindrome(String s){
-        int start = 0, end = s.length()-1;
+        int start = 0, end = s.length()-1; 
         while(start < end){
-            if(s.charAt(start) != s.charAt(end)) return false;
-            start++; 
-            end--; 
+            if(s.charAt(start) != s.charAt(end)) return false; 
+            start++;
+            end--;  
         }
         return true; 
     }
 }
+
+
+//Time : O(N^2) 
+//Space : O(N); 
