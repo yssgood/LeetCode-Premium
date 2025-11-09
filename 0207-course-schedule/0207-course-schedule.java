857428206182114bdc12eb047b859606bf6d4102
유승선 <yssgood@naver.com>
2025-11-09 19:29:42 +0900

    Time: 7 ms (46.95%), Space: 46.6 MB (14.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,41 +1,38 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>(); 
+        List<List<Integer>> adj =  new ArrayList<>(); 
         for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>()); 
         }
 
         int[] inOrder = new int[numCourses]; 
-        
         for(int[] course : prerequisites){
-            int to = course[0]; 
-            int from = course[1]; 
-
+            int to = course[0], from = course[1]; 
             adj.get(from).add(to); 
             inOrder[to]++; 
         }
 
         Queue<Integer> q = new LinkedList<>(); 
         for(int i = 0; i < numCourses; i++){
-            if(inOrder[i] == 0) q.add(i); 
+            if(inOrder[i] == 0) q.offer(i); 
         }
-
         if(q.isEmpty()) return false; 
 
         while(!q.isEmpty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
-                int first = q.poll(); 
-                for(int next : adj.get(first)){
+                int curr = q.poll(); 
+                for(int next : adj.get(curr)){
                     inOrder[next]--; 
                     if(inOrder[next] == 0){
-                        q.add(next); 
+                        q.offer(next); 
                     }
                 }
             }
         }
-
-        for(int n : inOrder) if(n > 0) return false; 
+        for(int i = 0; i < numCourses; i++){
+            if(inOrder[i] > 0) return false; 
+        }
         return true; 
     }
 }
