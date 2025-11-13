a26e6338f3a681c96bf2e7661958ba08434a7342
유승선 <yssgood@naver.com>
2025-11-13 19:22:13 +0900

    Time: 6 ms (52.51%), Space: 82.4 MB (6.65%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
         List<Integer> answer = new ArrayList<>(); 
-        int max_ = 0; 
+        int max_ = -1; 
         for(int i = heights.length-1; i >= 0; i--){
             if(heights[i] > max_){
                 answer.add(i); 
+                max_ = heights[i]; 
             }
-            max_ = Math.max(max_, heights[i]); 
         }
+
         Collections.sort(answer); 
-        return answer.stream().mapToInt(i -> i).toArray(); 
+        int[] answerArray = new int[answer.size()]; 
+        for(int i = 0; i < answer.size(); i++){
+            answerArray[i] = answer.get(i); 
+        }
+        return answerArray; 
     }
 }
