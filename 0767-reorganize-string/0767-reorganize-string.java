80e55f80442d3141981b62646a46b7c202683465
유승선 <yssgood@naver.com>
2025-11-13 20:05:39 +0900

    Time: 5 ms (38.55%), Space: 43.3 MB (8.23%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,37 +1,41 @@
 class Solution {
+    class Pair {
+        int frequency; 
+        char letter; 
+        public Pair(int frequency, char letter){
+            this.frequency = frequency;  
+            this.letter = letter; 
+        }
+    }
     public String reorganizeString(String s) {
-        // Use Max-Heap to store [frequency, char_as_int]
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        Map<Character, Integer> hashMap = new HashMap<>();
-        for (char c : s.toCharArray()) hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
-        for (char c : hashMap.keySet()) {
-            pq.offer(new int[]{hashMap.get(c), (int)c}); // Cast char to int
+        if(s.length() == 1) return s; 
+        StringBuilder answer = new StringBuilder(); 
+        Map<Character,Integer> hashMap = new HashMap<>(); 
+        for(char c : s.toCharArray()) hashMap.put(c, hashMap.getOrDefault(c,0) + 1); 
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.frequency - a.frequency); 
+        for(char c : hashMap.keySet()){
+            pq.offer(new Pair(hashMap.get(c), c)); 
         }
 
-        StringBuilder sb = new StringBuilder();
-        
-        // Loop while we have at least 2 different characters to pair up
-        while (pq.size() > 1) {
-            int[] first = pq.poll();
-            int[] second = pq.poll();
+        while(pq.size() > 1){
+            Pair first = pq.poll(); 
+            Pair second = pq.poll(); 
 
-            // Append the characters back-to-back
-            sb.append((char)first[1]);
-            sb.append((char)second[1]);
+            answer.append(first.letter).append(second.letter); 
+            first.frequency--; 
+            second.frequency--;  
 
-            // Decrease frequency and add back to PQ if still needed
-            if (--first[0] > 0) pq.offer(first);
-            if (--second[0] > 0) pq.offer(second);
+            if(first.frequency > 0){
+                pq.offer(first); 
+            }
+            if(second.frequency > 0){
+                pq.offer(second); 
+            }
         }
 
-        // If one character type is left
-        if (!pq.isEmpty()) {
-            int[] last = pq.poll();
-            // If it has more than 1 instance left, it will conflict with itself. Impossible.
-            if (last[0] > 1) return "";
-            sb.append((char)last[1]);
-        }
-
-        return sb.toString();
+        if(pq.isEmpty()) return answer.toString(); 
+        if(!pq.isEmpty() && pq.peek().frequency > 1) return "";
+        answer.append(pq.peek().letter); 
+        return answer.toString();  
     }
 }
