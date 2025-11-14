5ea2f9c23ff3c9bd48b56f9c4b848b04a78e816e
유승선 <yssgood@naver.com>
2025-11-15 08:19:48 +0900

    Time: 8 ms (23.63%), Space: 43.2 MB (29.54%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.java
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.java
@@ -1,50 +1,71 @@
 class Solution {
-    int index = 0;
-    
     public int calculate(String s) {
-        return eval(s);
-    }
-    
-    private int eval(String s) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+
+        int n = s.length();
         int num = 0;
-        char operator = '+';
-        
-        while (index < s.length()) {
-            char c = s.charAt(index++);
-            
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
             if (Character.isDigit(c)) {
                 num = num * 10 + (c - '0');
             }
-            
+
             if (c == '(') {
-                num = eval(s);
+                ops.push(c);
             }
-            
-            if ((!Character.isDigit(c) && c != ' ') || index == s.length()) {
-                if (operator == '+') {
-                    stack.push(num);
-                } else if (operator == '-') {
-                    stack.push(-num);
-                } else if (operator == '*') {
-                    stack.push(stack.pop() * num);
-                } else if (operator == '/') {
-                    stack.push(stack.pop() / num);
+
+            // 숫자 뒤, 연산자, 괄호 닫힘, 마지막 문자에서 처리
+            if (!Character.isDigit(c) && c != ' ' || i == n - 1) {
+
+                if (Character.isDigit(c) && i == n - 1) {
+                    nums.push(num);
+                } else {
+                    if (i > 0 && Character.isDigit(s.charAt(i - 1))) {
+                        nums.push(num);
+                    }
                 }
-                
-                if (c == ')') {
-                    break;
-                }
-                
-                operator = c;
                 num = 0;
+
+                if (c == ')') {
+                    while (ops.peek() != '(') {
+                        apply(nums, ops);
+                    }
+                    ops.pop(); // '(' 제거
+                } 
+                else if (isOp(c)) {
+                    while (!ops.isEmpty() && ops.peek() != '('
+                           && prec(ops.peek()) >= prec(c)) {
+                        apply(nums, ops);
+                    }
+                    ops.push(c);
+                }
             }
         }
-        
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
-        return result;
+
+        while (!ops.isEmpty()) apply(nums, ops);
+        return nums.pop();
+    }
+
+    private boolean isOp(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private int prec(char c) {
+        if (c == '+' || c == '-') return 1;
+        return 2;  // * /
+    }
+
+    private void apply(Stack<Integer> nums, Stack<Character> ops) {
+        int b = nums.pop();
+        int a = nums.pop();
+        char op = ops.pop();
+
+        if (op == '+') nums.push(a + b);
+        else if (op == '-') nums.push(a - b);
+        else if (op == '*') nums.push(a * b);
+        else if (op == '/') nums.push(a / b);
     }
 }
