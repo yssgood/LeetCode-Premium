991f4363d7b5dcaafa038913f5a78f69a44953b8
유승선 <yssgood@naver.com>
2025-11-15 09:23:39 +0900

    Time: 2 ms (63.66%), Space: 43.8 MB (14.01%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,37 +1,44 @@
 class Solution {
+    Map<String,List<Integer>> memo = new HashMap<>(); 
     public List<Integer> diffWaysToCompute(String expression) {
-        return helper(expression);
+        return helper(expression); 
     }
-
     public List<Integer> helper(String expression) {
-        List<Integer> res = new ArrayList<>();
+        if(memo.containsKey(expression)) return memo.get(expression); 
+        List<Integer> res = new ArrayList<>(); 
+        boolean isNumber = true; 
 
-        // base case: expression 전체가 숫자인 경우
-        if (expression.chars().allMatch(Character::isDigit)) {
-            res.add(Integer.parseInt(expression));
-            return res;
+        for(char c : expression.toCharArray()){
+            if(!Character.isDigit(c)){
+                isNumber = false;
+                break; 
+            }
         }
 
-        for (int i = 0; i < expression.length(); i++) {
-            char op = expression.charAt(i);
+        if(isNumber){
+            res.add(Integer.parseInt(expression)); 
+            return res; 
+        }
 
-            if (op == '+' || op == '-' || op == '*') {
+        for(int i = 0; i < expression.length(); i++){
+            char op = expression.charAt(i); 
 
-                // 재귀적으로 왼쪽/오른쪽 계산
-                List<Integer> left = helper(expression.substring(0, i));
-                List<Integer> right = helper(expression.substring(i + 1));
-
-                // 조합
-                for (int l : left) {
-                    for (int r : right) {
-                        if (op == '+') res.add(l + r);
-                        else if (op == '-') res.add(l - r);
-                        else if (op == '*') res.add(l * r);
+            if(!Character.isDigit(op)){
+                List<Integer> left = helper(expression.substring(0,i)); 
+                List<Integer> right = helper(expression.substring(i+1)); 
+                
+                for(int l : left){
+                    for(int r : right){
+                        if(op == '+') res.add(l + r); 
+                        if(op == '-') res.add(l - r); 
+                        if(op == '*') res.add(l * r); 
                     }
                 }
             }
         }
 
-        return res;
+        memo.put(expression,res); 
+
+        return res; 
     }
 }
