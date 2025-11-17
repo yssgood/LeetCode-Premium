469c6298c83d64bf50a3f37d46d608a82cdec07e
유승선 <yssgood@naver.com>
2025-11-17 22:25:17 +0900

    Time: 1 ms (66.89%), Space: 42.8 MB (13.2%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,24 +1,28 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>(); 
         Stack<String> stack = new Stack<>(); 
+        Stack<Integer> numStack = new Stack<>(); 
         int num = 0; 
         StringBuilder sb = new StringBuilder(); 
         for(char c : s.toCharArray()){
             if(Character.isDigit(c)){
-                num = num * 10 + (c-'0'); 
+                num = num * 10 + (c - '0');
             } else if(c == '['){
                 numStack.push(num); 
                 stack.push(sb.toString()); 
                 num = 0; 
                 sb = new StringBuilder(); 
             } else if(c == ']'){
-                int nextDigit = numStack.pop(); 
-                StringBuilder tmp = new StringBuilder(stack.pop());
-                for(int i = 0; i < nextDigit; i++){
+                int numTimes = numStack.pop(); 
+                StringBuilder tmp = new StringBuilder(); 
+                for(int i = 0; i < numTimes; i++){
                     tmp.append(sb.toString()); 
                 }
-                sb = tmp; 
+                String prev = stack.pop(); 
+                StringBuilder newString = new StringBuilder(); 
+                newString.append(prev).append(tmp); 
+                sb = newString; 
+                
             } else{
                 sb.append(c); 
             }
