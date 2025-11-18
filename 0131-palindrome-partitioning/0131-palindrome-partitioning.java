df5975763cca2570562e559d40e2d1f64a3383d3
유승선 <yssgood@naver.com>
2025-11-18 16:48:35 +0900

    Time: 7 ms (97.53%), Space: 65.1 MB (21.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,36 +2,34 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> answer = new ArrayList<>(); 
         List<String> container = new ArrayList<>(); 
-        dfs(s,answer,container,0); 
+        dfs(answer,container,0, s);
         return answer; 
     }
-    void dfs(String s, List<List<String>> answer, List<String> container, int index){
+    public void dfs(List<List<String>> answer, List<String> container, int index, String s){
         if(index >= s.length()){
-            if(container.size() > 0) answer.add(new ArrayList<>(container)); 
+            answer.add(new ArrayList<>(container)); 
             return; 
         }
 
         for(int i = index; i < s.length(); i++){
-            String subString = s.substring(index,i+1); 
-            if(isPalindrome(subString)){
-                container.add(subString); 
-                dfs(s,answer,container,i+1); 
+            String tmp = s.substring(index, i + 1); 
+            if(isPalindrome(tmp)){
+                container.add(tmp); 
+                dfs(answer,container,i+1,s);
                 container.removeLast(); 
             }
         }
     }
 
-    boolean isPalindrome(String s){
-        int start = 0, end = s.length()-1; 
+    public boolean isPalindrome(String tmp){
+        int start = 0, end = tmp.length()-1; 
         while(start < end){
-            if(s.charAt(start) != s.charAt(end)) return false; 
+            if(tmp.charAt(start) != tmp.charAt(end)){
+                return false; 
+            }
             start++;
-            end--;  
+            end--; 
         }
         return true; 
     }
 }
-
-
-//Time : O(N^2) 
-//Space : O(N); 
