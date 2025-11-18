52af03afce0cb28f29b83baa2115ce8959bc72a1
유승선 <yssgood@naver.com>
2025-11-18 15:09:00 +0900

    Time: 7 ms (49.6%), Space: 47 MB (21.67%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -30,21 +30,15 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size(); 
             count++; 
-            for(int i = 0; i < size; i++){
-                int curr = q.poll(); 
-                for(int next : adj.get(curr)){
-                    inOrder[next]--; 
-                    if(inOrder[next] == 0){
-                        q.offer(next);
-                    }
+            int curr = q.poll(); 
+            for(int next : adj.get(curr)){
+                inOrder[next]--; 
+                if(inOrder[next] == 0){
+                    q.offer(next);
                 }
             }
         }
 
-        for(int i = 0; i < numCourses; i++){
-            if(inOrder[i] != 0) return false; 
-        }
-
-        return true; 
+        return count == numCourses;
     }
 }
