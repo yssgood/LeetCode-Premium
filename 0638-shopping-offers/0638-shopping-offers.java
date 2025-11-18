6cc437a62afd16f1311b91cebdbefffc113126ff
유승선 <yssgood@naver.com>
2025-11-18 15:44:24 +0900

    Time: 7 ms (65.22%), Space: 46.4 MB (16.07%) - LeetHub

--- 0638-shopping-offers/0638-shopping-offers.java
+++ 0638-shopping-offers/0638-shopping-offers.java
@@ -1,33 +1,35 @@
 class Solution {
-    private Map<List<Integer>, Integer> memo = new HashMap<>();
+    Map<List<Integer>, Integer> memo = new HashMap<>(); 
     public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-        return dfs(price, special, needs); 
+        return helper(price, special, needs); 
     }
 
-    public int dfs(List<Integer> price, List<List<Integer>> special, List<Integer> needs){
-        if(memo.containsKey(needs)) return memo.get(needs);
-        
-        int minPrice = 0; 
-        for(int i = 0; i < needs.size(); i++){
-            minPrice += needs.get(i) * price.get(i);  
+    public int helper(List<Integer> price, List<List<Integer>> special, List<Integer> needs){
+        if(memo.containsKey(needs)) return memo.get(needs); 
+        int min_price = 0;  
+        for(int i = 0; i < price.size(); i++){
+            min_price += price.get(i) * needs.get(i); 
         }
 
-        for(List<Integer> offer : special){
-            List<Integer> newNeeds = new ArrayList<>(needs); 
+        for(int i = 0; i < special.size(); i++){
+            List<Integer> newNeeds = new ArrayList<>(); 
             boolean valid = true; 
-            for(int i = 0; i < needs.size(); i++){
-                newNeeds.set(i, newNeeds.get(i) - offer.get(i)); 
-                if(newNeeds.get(i) < 0){
-                    valid = false;
+            for(int j = 0; j < special.get(i).size()-1; j++){
+                int leftOver = needs.get(j) - special.get(i).get(j); 
+                newNeeds.add(leftOver); 
+                if(leftOver < 0){
+                    valid = false; 
                     break; 
                 }
             }
 
             if(valid){
-                minPrice = Math.min(minPrice, offer.get(offer.size()-1) + dfs(price, special, newNeeds)); 
+                min_price = Math.min(min_price, helper(price, special, newNeeds) + special.get(i).get(special.get(i).size()-1));
             }
         }
-        memo.put(needs, minPrice);
-        return minPrice; 
+
+        memo.put(needs,min_price); 
+
+        return min_price; 
     }
 }
