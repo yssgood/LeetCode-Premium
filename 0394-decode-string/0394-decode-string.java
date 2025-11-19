9ea81ecd325d4097b71b3d18b64c1139ec7b92fb
유승선 <yssgood@naver.com>
2025-11-19 18:58:44 +0900

    Time: 71 ms (8.93%), Space: 42.8 MB (20.57%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,29 +1,34 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> stack = new Stack<>(); 
         Stack<Integer> numStack = new Stack<>(); 
+        Stack<StringBuilder> stringStack = new Stack<>(); 
         int num = 0; 
         StringBuilder sb = new StringBuilder(); 
         for(char c : s.toCharArray()){
+            
             if(Character.isDigit(c)){
-                num = num * 10 + (c - '0');
-            } else if(c == '['){
+                num = num * 10 + (c - '0'); 
+            } 
+            
+            if(c == '['){
                 numStack.push(num); 
-                stack.push(sb.toString()); 
+                stringStack.push(sb); 
                 num = 0; 
-                sb = new StringBuilder(); 
-            } else if(c == ']'){
+                sb = new StringBuilder();
+            } 
+            
+            if(c == ']'){
                 int numTimes = numStack.pop(); 
                 StringBuilder tmp = new StringBuilder(); 
                 for(int i = 0; i < numTimes; i++){
                     tmp.append(sb.toString()); 
                 }
-                String prev = stack.pop(); 
-                StringBuilder newString = new StringBuilder(); 
-                newString.append(prev).append(tmp); 
-                sb = newString; 
-                
-            } else{
+                StringBuilder prev = stringStack.pop(); 
+                prev.append(tmp.toString()); 
+                sb = prev; 
+            } 
+
+            if (Character.isLetter(c)) {
                 sb.append(c); 
             }
         }
