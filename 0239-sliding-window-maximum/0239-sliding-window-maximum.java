52c661346505898c12a72b9409ac6528a05de324
유승선 <yssgood@naver.com>
2025-11-23 17:40:49 +0900

    Time: 45 ms (17.41%), Space: 154.8 MB (9.23%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,38 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        //have monotonic increasing queue 
         LinkedList<Integer> dq = new LinkedList<>(); 
         List<Integer> answer = new ArrayList<>(); 
-        
-        int start = 0, end = 0;
-        
-        while (end < nums.length) {
 
-            // 1) 윈도우 범위 벗어나면 앞에서 제거해야 합니다
-            if (!dq.isEmpty() && dq.peekFirst() < end - k + 1) {
-                dq.pollFirst();
+        for(int i = 0; i < nums.length; i++){
+            if(!dq.isEmpty() && dq.peekFirst() <= i - k){
+                dq.pollFirst(); 
             }
 
-            // 2) 새로운 값보다 작은 값들 뒤에서 제거
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[end]) {
-                dq.pollLast();
+            while(!dq.isEmpty() && nums[i] > nums[dq.peekLast()]){
+                dq.pollLast(); 
             }
 
-            // 3) 현재 인덱스 삽입
-            dq.offerLast(end);
+            dq.add(i); 
 
-            // 4) 윈도우 크기 도달 시 최대값 저장
-            if (end - start + 1 == k) {
-                answer.add(nums[dq.peekFirst()]);
-                start++;
+            if(i - k + 1 >= 0){
+                answer.add(nums[dq.peekFirst()]); 
             }
-
-            end++;
         }
 
-        int[] res = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            res[i] = answer.get(i);
-        }
-        return res;
+        return answer.stream().mapToInt(Integer::intValue).toArray(); 
     }
 }
