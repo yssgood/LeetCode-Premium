1d59f9a2ac0c9a9611e51b3ad9e889e9e337fb7f
유승선 <yssgood@naver.com>
2025-11-23 18:04:39 +0900

    Time: 63 ms (75.63%), Space: 78.2 MB (14.35%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,31 +1,29 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>(); 
-        int answer = 0; 
-        int n = heights.length; 
+        Stack<Integer> indexStack = new Stack<>(); 
+        int max_ = 0;
         for(int i = 0; i < heights.length; i++){
-            while(!stack.isEmpty () && heights[i] < heights[stack.peek()]){
-                int curr = heights[stack.pop()]; 
-                int left = stack.isEmpty() ? -1 : stack.peek(); 
-                int right = i; 
+            int currWeight = heights[i]; 
 
-                int width = (right - left) - 1;  
-                answer = Math.max(answer, width * curr); 
+            while(!indexStack.isEmpty() && currWeight < heights[indexStack.peek()]){
+                int currIndex = indexStack.pop(); 
+                int left = (indexStack.isEmpty()) ? -1 : indexStack.peek(); 
+                int right = i; 
+                int width = (right - left) - 1; 
+                max_ = Math.max(max_, width * heights[currIndex]);
             }
 
-            stack.push(i); 
+            indexStack.push(i); 
         }
 
-        while(!stack.isEmpty()){
-            int curr = heights[stack.pop()]; 
-            int left = stack.isEmpty() ? -1 : stack.peek(); 
-            int right = n; 
+        while(!indexStack.isEmpty()){
+            int curr = heights[indexStack.pop()]; 
+            int left = indexStack.isEmpty() ? -1 : indexStack.peek(); 
+            int right = heights.length; 
 
-            int width = (right - left) - 1;  
-            answer = Math.max(answer, width * curr); 
+            int width = (right - left) - 1; 
+            max_ = Math.max(max_, width * curr);
         }
-
-
-        return answer; 
+        return max_; 
     }
 }
