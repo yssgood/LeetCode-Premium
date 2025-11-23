5fda24add15921b4a34e4136d18167339f6c5000
유승선 <yssgood@naver.com>
2025-11-23 14:59:59 +0900

    Time: 1 ms (61.69%), Space: 44.4 MB (10.17%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,15 +2,16 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>(); 
         List<Integer> container = new ArrayList<>(); 
-        dfs(nums,answer,container,0); 
+        int index = 0;
+        dfs(answer,container,index,nums);
         return answer; 
     }
-    void dfs(int[] nums, List<List<Integer>> answer, List<Integer> container, int index){
+    void dfs(List<List<Integer>> answer,  List<Integer> container, int index, int[] nums){
         answer.add(new ArrayList<>(container)); 
         for(int i = index; i < nums.length; i++){
-            container.add(nums[i]); 
-            dfs(nums,answer,container,i+1);
-            container.removeLast();
+            container.add(nums[i]);
+            dfs(answer,container,i+1,nums); 
+            container.removeLast(); 
         }
     }
 }
