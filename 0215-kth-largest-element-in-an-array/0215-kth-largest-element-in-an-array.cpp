a48da7fcf498af6b01e7546542958c25af63e159
유승선 <yssgood@naver.com>
2025-11-23 19:00:56 +0900

    Time: 2686 ms (5%), Space: 59.3 MB (75.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,12 +1,57 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq; 
-        for(int n : nums) pq.push(n); 
-        while(k > 1){
-            pq.pop(); 
-            k--; 
+    int partition(vector<int>& nums, int left, int right){
+        //int pivotVal = nums[left];
+        int randomIndex = left + rand() % (right - left + 1); 
+        swap(nums[left], nums[randomIndex]); 
+        int pivotVal = nums[left]; 
+        int i = left+1;
+        int j = right;
+        
+        while (true){
+            while (i <= right && nums[i] <= pivotVal){
+                i++;
+            }
+            
+            while (j >= left && nums[j] > pivotVal){
+                j--;
+            }
+            
+            if (j <= i){
+                break;
+            }
+            swap(nums[i],nums[j]);
         }
-        return pq.top(); 
+        swap(nums[left],nums[j]);
+        return j;
+    }
+    
+    
+
+    int quickSelect(vector<int>& nums, int left, int right, int targetIndex){
+        if (left == right){
+            return nums[left];
+        }
+        
+        int ourIndex = partition(nums, left, right);
+        
+        if (ourIndex == targetIndex){
+            return nums[ourIndex];
+        }
+        
+        if (ourIndex > targetIndex){
+            return quickSelect(nums,left,ourIndex-1,targetIndex);
+        }
+        
+        
+        return quickSelect(nums,ourIndex+1,right,targetIndex);
+    }
+    
+    
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        //quickSelect(nums, left, right, targetIndex) 
+        int targetIndex = nums.size()-k;
+        return quickSelect(nums,0,nums.size()-1,targetIndex);
     }
 };
