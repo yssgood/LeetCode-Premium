566e9a018a0803c293bc1bdca7f7e7ae01d8c02a
유승선 <yssgood@naver.com>
2025-11-23 15:33:27 +0900

    Time: 1 ms (93.92%), Space: 47.3 MB (16.99%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,22 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int[] suffix = new int[nums.length]; 
-        int rightMost = 0; 
-        for(int i = nums.length-1; i >= 0; i--){
-            suffix[i] += rightMost + nums[i];
-            rightMost += nums[i]; 
+        int n = nums.length; 
+        int[] prefix = new int[n]; 
+        int[] suffix = new int[n]; 
+        prefix[0] = nums[0]; 
+        suffix[n-1] = nums[n-1]; 
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + nums[i]; 
         }
-        int leftMost = 0; 
-        for(int i = 0; i < nums.length-1; i++){
-            int compare = suffix[i+1]; 
-            if(leftMost == compare) return i; 
-            leftMost += nums[i]; 
+        for(int i = n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1] + nums[i]; 
         }
 
-        //System.out.print(leftMost); 
-        if(leftMost == 0) return nums.length-1; 
+        for(int i = 0; i < n; i++){
+            int left = i - 1 < 0 ? 0 : prefix[i-1]; 
+            int right = i + 1 >= n ? 0 : suffix[i+1]; 
+            if(left == right) return i; 
+        }
 
         return -1; 
     }
