d0104ed7f4cadb40d12a884890b11efee39949bf
유승선 <yssgood@naver.com>
2025-11-23 19:59:10 +0900

    Time: 1 ms (71.07%), Space: 43 MB (12.52%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.java
+++ 0362-design-hit-counter/0362-design-hit-counter.java
@@ -1,20 +1,35 @@
 class HitCounter {
+    class Pair{
+        int time; 
+        int hits; 
+        public Pair(int time, int hits){
+            this.time = time;
+            this.hits = hits; 
+        }
+    }
 
-    private Queue<Integer> q = new LinkedList<>(); 
+    private Queue<Pair> q = new LinkedList<>(); 
+    int size = 0; 
 
     public HitCounter() {
         
     }
     
     public void hit(int timestamp) {
-        q.add(timestamp); 
+        size++; 
+        if(!q.isEmpty() && q.peek().time == timestamp){
+            q.peek().hits++; 
+        } else{
+            q.offer(new Pair(timestamp, 1)); 
+        }
     }
     
     public int getHits(int timestamp) {
-        while(!q.isEmpty() && timestamp - q.peek() >= 300){
+        while(!q.isEmpty() && timestamp - q.peek().time >= 300){
+            size -= q.peek().hits; 
             q.poll(); 
         }
-        return q.size();
+        return size; 
     }
 }
 
