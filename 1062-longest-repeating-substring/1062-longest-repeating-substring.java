0a0d4627054f04163983ecc72587a306c6195899
유승선 <yssgood@naver.com>
2025-11-23 17:19:34 +0900

    Time: 1569 ms (8.21%), Space: 287.1 MB (5.22%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -1,21 +1,18 @@
 class Solution {
     public int longestRepeatingSubstring(String s) {
-        int max_ = 0; 
-        int n = s.length(); 
-        
         Set<String> set = new HashSet<>(); 
+        int n = s.length(); 
+        int answer = 0; 
         for(int i = 0; i < n; i++){
             int count = 0; 
             for(int j = i+1; j <= n; j++){
                 String tmp = s.substring(i,j); 
-                if(set.contains(tmp)){
-                    count++; 
-                }
+                if(set.contains(tmp)) count++; 
                 set.add(tmp); 
             }
-            max_ = Math.max(max_, count); 
+            answer = Math.max(answer,count); 
         }
 
-        return max_; 
+        return answer; 
     }
 }
