fa6d3a6d29ca054b8dcc02b2c06c61e1d02d976a
유승선 <yssgood@naver.com>
2025-11-29 13:20:20 +0900

    Time: 402 ms (5.15%), Space: 113.6 MB (5.7%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,32 +1,34 @@
 class MedianFinder {
-
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>(); 
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); 
-
+    private PriorityQueue<Integer> maxQ; 
+    private PriorityQueue<Integer> minQ; 
     public MedianFinder() {
-        
+        this.maxQ = new PriorityQueue<>(Collections.reverseOrder()); 
+        this.minQ = new PriorityQueue<>(); 
     }
     
     public void addNum(int num) {
-        if(maxHeap.isEmpty() || maxHeap.peek() >= num){
-            maxHeap.offer(num); 
+        //the rule of thumb : maxQ should stay 1 more than the minQ 
+        if(maxQ.isEmpty() || maxQ.peek() >= num){
+            maxQ.offer(num); 
+            System.out.println(maxQ.size()); 
         } else{
-            minHeap.offer(num); 
+            minQ.offer(num); 
         }
 
-        if(maxHeap.size() > minHeap.size() + 1){
-            minHeap.offer(maxHeap.poll());   // ✔ 방향 올바름
-        } else if (minHeap.size() > maxHeap.size()) {
-            maxHeap.offer(minHeap.poll());
+        if(maxQ.size() > minQ.size() + 1){
+            minQ.offer(maxQ.poll()); 
+        } else if(minQ.size() > maxQ.size()) {
+            maxQ.offer(minQ.poll()); 
         }
+
+        
     }
     
     public double findMedian() {
-        if(maxHeap.size() == minHeap.size()){
-            return (double)(maxHeap.peek() + minHeap.peek()) / 2; 
-        } else{
-            return maxHeap.peek(); 
-        }
+        if(maxQ.size() == minQ.size()){
+            return (double)(maxQ.peek() + minQ.peek()) /2; 
+        } 
+        return (double) maxQ.peek(); 
     }
 }
 
