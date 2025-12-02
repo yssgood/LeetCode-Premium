84eb435dc10a372f265eda256ed6896685a18fa3
유승선 <yssgood@naver.com>
2025-12-02 22:05:47 +0900

    Time: 16 ms (24.99%), Space: 31.1 MB (39.05%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n = matrix[0].size(); 
-        vector<vector<int>> numMatrix(m, vector<int>(n,0)); 
-        int max_area = 0; 
+        vector<vector<int>> dp(m,vector<int>(n,0)); 
+        int max_ = 0; 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(matrix[i][j] - '0' > 0) max_area = 1; 
-                numMatrix[i][j] = matrix[i][j] - '0'; 
-            }
-        }
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                int top = numMatrix[i-1][j];
-                int left = numMatrix[i][j-1]; 
-                int cross = numMatrix[i-1][j-1]; 
-                int curr = numMatrix[i][j]; 
-                if(top && left && cross && curr){
-                    numMatrix[i][j] = min({top,left,cross}) + 1;
-                }
-                    max_area = max(max_area, max(numMatrix[i][j], max(left,max(top,cross)))); 
+                if(matrix[i][j] == '1') max_ = 1; 
+                dp[i][j] = matrix[i][j] - '0'; 
             }
         }
 
-        return max_area * max_area; 
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                int top = dp[i-1][j]; 
+                int cross = dp[i-1][j-1];
+                int left = dp[i][j-1];
+                
+                if(matrix[i][j] == '1'){
+                    dp[i][j] = min(top,min(left,cross)) + 1; 
+                    max_ = max(max_, dp[i][j]); 
+                }
+            }
+        }
+
+        return max_ * max_; 
     }
 };
