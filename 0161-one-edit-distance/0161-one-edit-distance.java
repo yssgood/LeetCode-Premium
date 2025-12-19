22ce4cd1b22e5a373765116b3325797e9f03c975
유승선 <yssgood@naver.com>
2025-12-19 22:34:48 +0900

    Time: 1 ms (79.55%), Space: 43.4 MB (46.4%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.java
+++ 0161-one-edit-distance/0161-one-edit-distance.java
@@ -2,22 +2,25 @@ class Solution {
     public boolean isOneEditDistance(String s, String t) {
         if(s.equals(t)) return false; 
         if(s.length() > t.length()){
-            String tmp = new String(s); 
-            s = t;
+            String tmp = s; 
+            s = t; 
             t = tmp; 
         }
-        for(int i = 0; i < s.length(); i++){
-            
-            if(s.charAt(i) != t.charAt(i)){
 
+        for(int i = 0; i < s.length(); i++){
+            char sChar = s.charAt(i); 
+            char tChar = t.charAt(i); 
+
+            if(sChar != tChar){
                 if(s.length() == t.length()){
-                    return s.substring(i + 1).equals(t.substring(i + 1)); 
-                } else{
+                    return s.substring(i+1).equals(t.substring(i+1)); 
+                } 
+                if(s.length() < t.length()){
                     return s.substring(i).equals(t.substring(i+1)); 
                 }
             }
         }
 
-        return s.length() + 1 == t.length();  
+        return s.length()+1 == t.length(); 
     }
 }
