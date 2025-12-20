11871773318d041a7525677aa7321cc6879d7c28
유승선 <yssgood@naver.com>
2025-12-20 19:18:33 +0900

    Time: 7 ms (34.59%), Space: 43 MB (45.28%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,28 +1,26 @@
 class Solution {
     public int shortestWay(String source, String target) {
-        int count = 0; 
-        int start = 0, end = 0; 
         Set<Character> hashSet = new HashSet<>(); 
         for(char c : source.toCharArray()) hashSet.add(c); 
+
+        int count = 0; 
+        int start = 0, end = 0; 
         while(end < target.length()){
-            
-            while(end < target.length() && start < source.length()){
+            if(!hashSet.contains(target.charAt(end))) return -1; 
+            int startIndex = end; 
+            while(start < source.length() && end < target.length()){
                 if(source.charAt(start) == target.charAt(end)){
-                    start++; 
                     end++; 
-                } else{
-                    if(!hashSet.contains(target.charAt(end))) return -1; 
-                    start++; 
                 }
+                start++; 
             }
 
-
-            if(start >= source.length()){
-                start = 0; 
-                end--; 
+            if(end == startIndex){
+                return -1; 
             }
+
+            start = 0; 
             count++; 
-            end++; 
         }
 
         return count; 
