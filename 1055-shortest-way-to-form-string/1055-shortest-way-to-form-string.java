c53b41481ecaba7838fd8b54c6eeaa42e1d37df0
유승선 <yssgood@naver.com>
2025-12-20 19:10:26 +0900

    Time: 10 ms (12.58%), Space: 43 MB (45.28%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,48 +1,30 @@
 class Solution {
     public int shortestWay(String source, String target) {
-        Map<Character, List<Integer>> map = new HashMap<>();
-
-        // store indexes for each char
-        for(int i = 0; i < source.length(); i++) {
-            char c = source.charAt(i);
-            map.computeIfAbsent(c, k -> new ArrayList<>()).add(i);
-        }
-
-        int count = 1;
-        int start = -1; // last used position in source
-
-        for(int e = 0; e < target.length(); e++) {
-            char curr = target.charAt(e);
-
-            if(!map.containsKey(curr)) return -1;
-
-            List<Integer> positions = map.get(curr);
-
-            // binary search for first index > start
-            int idx = upperBound(positions, start);
-
-            if(idx == positions.size()) {
-                // restart source
-                count++;
-                start = -1;
-                idx = upperBound(positions, start);
+        int count = 0; 
+        int start = 0, end = 0; 
+        Set<Character> hashSet = new HashSet<>(); 
+        for(char c : source.toCharArray()) hashSet.add(c); 
+        while(end < target.length()){
+            
+            while(end < target.length() && start < source.length()){
+                if(source.charAt(start) == target.charAt(end)){
+                    start++; 
+                    end++; 
+                } else{
+                    if(!hashSet.contains(target.charAt(end))) return -1; 
+                    start++; 
+                }
             }
 
-            start = positions.get(idx);
+
+            if(start >= source.length()){
+                start = 0; 
+                end--; 
+            }
+            count++; 
+            end++; 
         }
 
-        return count;
-    }
-
-    // returns 첫 번째 value > target
-    private int upperBound(List<Integer> list, int target) {
-        int lo = 0;
-        int hi = list.size();
-        while(lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(list.get(mid) > target) hi = mid;
-            else lo = mid + 1;
-        }
-        return lo;
+        return count; 
     }
 }
