69b27f11686cf4c8ddd100c01b90b5d983e3f465
유승선 <yssgood@naver.com>
2025-12-22 20:24:01 +0900

    Time: 1 ms (86.81%), Space: 43.5 MB (43.47%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,27 +1,17 @@
 class Solution {
-    public boolean valid(String str1, String str2) {
-        for (int i = 0; i <= str1.length() - str2.length(); i += str2.length()) { // 조건 수정
-            String tmp = str1.substring(i, i + str2.length()); // 올바른 범위 사용
-            if (!tmp.equals(str2)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public String gcdOfStrings(String str1, String str2) {
-        if (str2.length() > str1.length()) return gcdOfStrings(str2, str1); 
-        // str1이 항상 str2보다 길거나 같도록 보장
-
-        for (int i = str2.length(); i > 0; i--) {
-            if (str1.length() % i != 0 || str2.length() % i != 0) continue; // 길이 검증 추가
-
-            String candidate = str2.substring(0, i);
-            if (valid(str1, candidate) && valid(str2, candidate)) { // str1과 str2 모두에서 유효성 검사
-                return candidate;
-            }
+        // check if they share common divisor
+        if(!(str1 + str2).equals(str2 + str1)) {
+            return "";
         }
-
-        return "";
+        
+        // find GCD of lengths
+        int gcdLength = gcd(str1.length(), str2.length());
+        
+        return str1.substring(0, gcdLength);
+    }
+    
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
