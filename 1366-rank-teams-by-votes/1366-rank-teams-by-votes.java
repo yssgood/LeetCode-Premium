7a4920ddefdd32c76caf317abddef7922b9e63f3
유승선 <yssgood@naver.com>
2025-12-26 22:07:38 +0900

    Time: 22 ms (14.24%), Space: 46.5 MB (31.53%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.java
@@ -1,42 +1,41 @@
 class Solution {
     public String rankTeams(String[] votes) {
-        Map<Character,int[]> hashMap = new HashMap<>(); 
-        for(int i = 0; i < votes[0].length(); i++){
-            hashMap.put(votes[0].charAt(i), new int[votes[0].length()]);
-        }
-
+        Map<Character, int[]> hashMap = new HashMap<>(); 
+        String answer = votes[0]; 
         for(String s : votes){
             for(int i = 0; i < s.length(); i++){
-                // int[] vec = hashMap.get(s.charAt(i)); 
-                // vec[i]++; 
-                // hashMap.put(s.charAt(i), vec); 
-                hashMap.computeIfAbsent(s.charAt(i), k -> new int[votes[0].length()])[i]++; 
+                hashMap.computeIfAbsent(s.charAt(i), k -> new int[s.length()])[i]++; 
             }
         }
 
-        String first = votes[0]; 
-        char[] tmp = first.toCharArray(); 
+        char[] charArr = answer.toCharArray();
         List<Character> lst = new ArrayList<>(); 
-        for(char c : tmp) lst.add(c); 
-        
-        Collections.sort(lst,(a,b) -> {
-            int[] arrA = hashMap.get(a); 
-            int[] arrB = hashMap.get(b); 
-
-            for(int i = 0; i < arrA.length; i++){
-                if(arrA[i] != arrB[i]){
-                    return arrB[i] - arrA[i]; 
+        for(char c : charArr) lst.add(c); 
+        Collections.sort(lst, (a,b) -> {
+            int[] forA = hashMap.get(a); 
+            int[] forB = hashMap.get(b); 
+            for(int i = 0; i < forA.length; i++){
+                if(forA[i] != forB[i]){
+                    return forB[i] - forA[i]; 
                 }
+                // if(forA[i] > forB[i]){
+                //     return forA[i] - forB[i]; 
+                // }
+                // if(forB[i] < forA[i]){
+                //     return forB[i] - forA[i]; 
+                // }
             }
 
             return a - b; 
         });
 
+        char[] res = new char[answer.length()];  
+        for(int i = 0; i < res.length; i++){
+            res[i] = lst.get(i); 
+        }
 
-        StringBuilder sb = new StringBuilder(); 
-        for(char c : lst) sb.append(c); 
+        answer = new String(res); 
 
-
-        return sb.toString(); 
+        return answer; 
     }
 }
