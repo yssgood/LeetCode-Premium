0304a15070829b8aaa738f7fbc062634ffebbc91
유승선 <yssgood@naver.com>
2025-12-28 10:38:44 +0900

    Time: 3 ms (49%), Space: 44.5 MB (31.88%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,44 +1,37 @@
 class Solution {
-    Map<String,List<Integer>> memo = new HashMap<>(); 
     public List<Integer> diffWaysToCompute(String expression) {
-        return helper(expression); 
-    }
-    public List<Integer> helper(String expression) {
-        if(memo.containsKey(expression)) return memo.get(expression); 
-        List<Integer> res = new ArrayList<>(); 
-        boolean isNumber = true; 
-
-        for(char c : expression.toCharArray()){
-            if(!Character.isDigit(c)){
-                isNumber = false;
-                break; 
-            }
-        }
-
-        if(isNumber){
-            res.add(Integer.parseInt(expression)); 
-            return res; 
-        }
+        List<Integer> answer = new ArrayList<>(); 
 
         for(int i = 0; i < expression.length(); i++){
-            char op = expression.charAt(i); 
+            char curr = expression.charAt(i); 
+            if(curr == '-' || curr == '+' || curr == '*'){
+                List<Integer> left = diffWaysToCompute(expression.substring(0,i)); 
+                List<Integer> right = diffWaysToCompute(expression.substring(i+1)); 
 
-            if(!Character.isDigit(op)){
-                List<Integer> left = helper(expression.substring(0,i)); 
-                List<Integer> right = helper(expression.substring(i+1)); 
-                
+                //left = 2 
+                //right = [-17,-5] 
+                //mainStack : [-34, -10] 
+
+
                 for(int l : left){
                     for(int r : right){
-                        if(op == '+') res.add(l + r); 
-                        if(op == '-') res.add(l - r); 
-                        if(op == '*') res.add(l * r); 
+                        if(curr == '-'){
+                            answer.add(l - r); 
+                        } else if(curr == '+'){
+                            answer.add(l + r); 
+                        } else{
+                            answer.add(l * r); 
+                        }
                     }
                 }
+
             }
         }
 
-        memo.put(expression,res); 
+        if(answer.isEmpty()){
+            answer.add(Integer.parseInt(expression)); 
+        }
 
-        return res; 
+        return answer; 
     }
 }
