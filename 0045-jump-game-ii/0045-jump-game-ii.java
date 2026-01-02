a31c59e37d9dcf1455969de9462e32109209007f
유승선 <yssgood@naver.com>
2026-01-02 20:46:36 +0900

    Time: 1 ms (99.14%), Space: 47.3 MB (35.08%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,37 +1,17 @@
 class Solution {
-
-    class Pair{
-        int first, second; 
-
-        public Pair(int first, int second){
-            this.first = first; 
-            this.second = second; 
-        }
-    }
     public int jump(int[] nums) {
-        if(nums.length == 1
-        ) return 0; 
-        Queue<Pair> q = new LinkedList<>(); 
-        boolean[] visited = new boolean[nums.length]; 
+        int maxJump = 0; 
+        int currJump = 0; 
+        int jump = 0; 
 
-        q.offer(new Pair(0,0)); 
-        visited[0] = true; 
-        
-        while(!q.isEmpty()){
-            int size = q.size(); 
-            for(int i = 0; i < size; i++){
-                Pair p = q.poll(); 
-                int curr = p.first, dist = p.second; 
-                for(int j = 1; j <= nums[curr]; j++){
-                    if(curr + j >= nums.length-1) return dist+1; 
-                    if(!visited[curr + j]){
-                        visited[curr + j] = true; 
-                        q.offer(new Pair(curr + j, dist + 1)); 
-                    }
-                }
+        for(int i = 0; i < nums.length-1; i++){
+            maxJump = Math.max(maxJump, i + nums[i]); 
+            if(i == currJump){
+                currJump = maxJump; 
+                jump++; 
+                if(currJump >= nums.length) return jump; 
             }
         }
-
-        return 0; 
+        return jump; 
     }
 }
