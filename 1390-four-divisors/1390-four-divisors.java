ee09c0eb437518c1d5ff8ef590c7589be844335b
유승선 <yssgood@naver.com>
2026-01-04 21:16:05 +0900

    Time: 1962 ms (5.05%), Space: 46.2 MB (68.18%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+        
+        for (int num : nums) {
+            int divisorSum = getDivisorSum(num);
+            totalSum += divisorSum;
+        }
+        
+        return totalSum;
+    }
+    
+    private int getDivisorSum(int n) {
+        int count = 0;
+        int sum = 0;
+        
+        // check divisors up to sqrt(n)
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                sum += i;
+                
+                // // add the paired divisor if different
+                // if (i != n / i) {
+                //     count++;
+                //     sum += n / i;
+                // }
+                
+                // early exit if more than 4 divisors
+                if (count > 4) {
+                    return 0;
+                }
+            }
+        }
+        
+        return count == 4 ? sum : 0;
+    }
+}
