d971bd37a6c397cd527a0d0a9ad0e95873593779
유승선 <yssgood@naver.com>
2026-01-12 21:30:43 +0900

    Time: 0 ms (100%), Space: 47.6 MB (39.47%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -3,21 +3,19 @@ class Solution {
         if (root == null) return null; 
         
         if (root.val == key) {
-            // 자식 없거나 1개
             if (root.right == null) return root.left;
             if (root.left == null) return root.right;
             
-            // 자식 2개: 왼쪽 서브트리를 오른쪽 최솟값 아래에 붙임
-            TreeNode tmp = root.left; 
+            // 자식 2개: successor 값 복사 후 successor 삭제
             TreeNode minNode = root.right;
             while (minNode.left != null) {
                 minNode = minNode.left;
             }
-            minNode.left = tmp;
-            return root.right;
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+            return root;
         }
 
-        // BST 탐색
         if (key < root.val) {
             root.left = deleteNode(root.left, key);
         } else {
