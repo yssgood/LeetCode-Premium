2621de685d5d7374b67cd13dfb08d800e6874410
유승선 <yssgood@naver.com>
2026-01-13 20:38:19 +0900

    Time: 1 ms (57.38%), Space: 43.6 MB (55.95%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,34 +1,21 @@
 class Solution {
-    void dfs(String digits, int index, List<String> answer,StringBuilder sb,Map<Character,List<Character>> hashMap){
-        if(sb.length() >= digits.length()){
-            answer.add(new String(sb.toString())); 
+        private final Map<Character,List<Character>> hashMap = Map.of('2', Arrays.asList('a','b','c'), '3', Arrays.asList('d','e','f'), '4', Arrays.asList('g','h','i'), '5', Arrays.asList('j','k','l'), '6', Arrays.asList('m','n','o'), '7', Arrays.asList('p','q','r','s'), '8', Arrays.asList('t','u','v'), '9', Arrays.asList('w','x','y','z')); 
+    public List<String> letterCombinations(String digits) {
+        List<String> answer = new ArrayList<>(); 
+        StringBuilder sb = new StringBuilder(); 
+        dfs(digits,answer,0,sb);
+        return answer; 
+    }
+    public void dfs(String digits, List<String> answer, int index, StringBuilder sb){
+        if(index >= digits.length()){
+            answer.add(sb.toString()); 
             return; 
         }
 
         for(int i = 0; i < hashMap.get(digits.charAt(index)).size(); i++){
             sb.append(hashMap.get(digits.charAt(index)).get(i)); 
-            dfs(digits,index+1,answer,sb,hashMap); 
-            sb.deleteCharAt(index); 
+            dfs(digits, answer, index + 1, sb); 
+            sb.deleteCharAt(sb.length()-1); 
         }
-    }
-    public List<String> letterCombinations(String digits) {
-        if(digits.isEmpty()){
-            return Collections.emptyList(); 
-        }
-        List<String> answer = new ArrayList<>(); 
-        Map<Character, List<Character>> hashMap = new HashMap<>() {{
-            put('2', Arrays.asList('a', 'b', 'c'));
-            put('3', Arrays.asList('d', 'e', 'f'));
-            put('4', Arrays.asList('g', 'h', 'i'));
-            put('5', Arrays.asList('j', 'k', 'l'));
-            put('6', Arrays.asList('m', 'n', 'o'));
-            put('7', Arrays.asList('p', 'q', 'r', 's'));
-            put('8', Arrays.asList('t', 'u', 'v'));
-            put('9', Arrays.asList('w', 'x', 'y', 'z'));
-        }};
-        StringBuilder sb = new StringBuilder(); 
-        dfs(digits,0,answer,sb,hashMap); 
-
-        return answer; 
     }
 }
