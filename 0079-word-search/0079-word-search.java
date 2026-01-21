b49ffccfec664c74f562692c66901ffb87e6088d
유승선 <yssgood@naver.com>
2026-01-21 20:52:36 +0900

    Time: 137 ms (59.31%), Space: 43.1 MB (36.56%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,23 +1,22 @@
 class Solution {
-    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
-    public boolean dfs(char[][] board, int i, int j, int index, String word){
+    public boolean dfs(char[][] board, int i, int j, String word, int index){
         if(index >= word.length()) return true; 
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index)) return false; 
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) return false; 
 
         char tmp = board[i][j]; 
         board[i][j] = '-'; 
-
-        boolean flag = ((dfs(board,i+1,j,index+1,word) || (dfs(board,i-1,j,index+1,word)) || dfs(board,i,j+1,index+1,word) || dfs(board,i,j-1,index+1,word))); 
+        boolean flag = (dfs(board,i+1,j,word,index+1) || dfs(board,i-1,j,word,index+1) || dfs(board,i,j+1,word,index+1) || dfs(board,i,j-1,word,index+1)); 
 
         board[i][j] = tmp; 
-
         return flag; 
     }
     public boolean exist(char[][] board, String word) {
-        
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && dfs(board,i,j,0,word)) return true; 
+        int m = board.length;
+        int n = board[0].length; 
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0) && dfs(board,i,j,word,0)) return true; 
             }
         }
 
