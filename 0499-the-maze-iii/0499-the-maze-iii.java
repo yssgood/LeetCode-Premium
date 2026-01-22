d5f80eeb9d8a8c025890491a35aff6c4f047b4de
유승선 <yssgood@naver.com>
2026-01-22 21:17:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0499-the-maze-iii/0499-the-maze-iii.java
+++ 0499-the-maze-iii/0499-the-maze-iii.java
@@ -1,104 +1,71 @@
-class State{
-    int row; 
-    int col;
-    int dist; 
-    String path; 
-
-    public State(int row, int col, int dist, String path){
-        this.row = row;
-        this.col = col; 
-        this.dist = dist; 
-        this.path = path;  
-    }
-}
-
-
 class Solution {
-    int[][] directions = new int[][]{{0,-1},{-1,0},{0,1},{1,0}}; 
-    String[] textDirections = new String[]{"l","u","r","d"}; 
+    class Ball {
+        int x, y; 
+        int dist; 
+        StringBuilder instruction; 
+        public Ball(int x, int y, int dist, StringBuilder instruction){
+            this.x = x;
+            this.y = y; 
+            this.dist = dist;
+            this.instruction = instruction; 
+        }
+    }
 
-    int m; 
-    int n; 
-
+    int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    char[] letter = {'r','l','d','u'}; 
     public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
-        m = maze.length; 
-        n = maze[0].length; 
-        PriorityQueue<State> heap = new PriorityQueue<>((a,b) -> {
-            int distA = a.dist;
-            int distB = b.dist; 
-
-            if(distA == distB){
-                return a.path.compareTo(b.path); 
-            }
-
-            return distA - distB; 
-        });
-
-        boolean[][] seen = new boolean[m][n]; 
-        heap.add(new State(ball[0], ball[1], 0, "")); 
-
-        while(!heap.isEmpty()){
-            State curr = heap.remove(); 
-
-            int row = curr.row; 
-            int col = curr.col; 
-
-            if(seen[row][col]){
-                continue; 
-            }
-
-            if(row == hole[0] && col == hole[1]){
-                return curr.path; 
-            }
-
-            seen[row][col] = true; 
-
-            for(State nextState: getNeighbors(row, col, maze, hole)){
-                int nextRow = nextState.row; 
-                int nextCol = nextState.col;  
-                int nextDist = nextState.dist; 
-                String nextChar = nextState.path; 
-                heap.add(new State(nextRow, nextCol, curr.dist + nextDist, curr.path + nextChar)); 
+        int m = maze.length;
+        int n = maze[0].length; 
+        int[][][] visited = new int[m][n][4]; 
+        StringBuilder answer = new StringBuilder("~");
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                Arrays.fill(visited[i][j], Integer.MAX_VALUE); 
             }
         }
-
-        return "impossible"; 
-    }
-
-    private boolean valid(int row, int col, int[][] maze){
-        if(row < 0 || row >= m || col < 0 || col >= n){
-            return false; 
-        }
-
-        return maze[row][col] == 0; 
-    }
-
-    private List<State> getNeighbors(int row, int col, int[][] maze, int[] hole){
-        List<State> neighbors = new ArrayList<>(); 
 
         for(int i = 0; i < 4; i++){
-            int dy = directions[i][0]; 
-            int dx = directions[i][1]; 
+            visited[ball[0]][ball[1]][i] = 0; 
+        }
 
-            String direction = textDirections[i]; 
-             
-            int currRow = row; 
-            int currCol = col; 
-            int dist = 0; 
+        Queue<Ball> q = new LinkedList<>(); 
+        q.add(new Ball(ball[0], ball[1], 0, new StringBuilder())); 
 
-            while(valid(currRow + dy, currCol + dx, maze)){
-                currRow += dy; 
-                currCol += dx; 
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                Ball first = q.poll(); 
+                int x = first.x, y = first.y, dist = first.dist; 
+                StringBuilder inst = first.instruction; 
 
-                dist++; 
+                for(int k = 0; k < 4; k++){
+                    int nX = x + dir[k][0]; 
+                    int nY = y + dir[k][1]; 
+                    int nDist = dist; 
+                    StringBuilder nInst = new StringBuilder(inst).append(letter[k]); 
+                    while(nX >= 0 && nY >= 0 && nX < m && nY < n && maze[nX][nY] != 1){
+                        if(nX == hole[0] && nY == hole[1]){
+                            if(nInst.toString().compareTo(answer.toString()) < 0){
+                                answer = nInst; 
+                            }
+                        }
+                        nX += dir[k][0];
+                        nY += dir[k][1]; 
+                        nDist += 1; 
+                    }
 
-                if(currRow == hole[0] && currCol == hole[1]){
-                    break; 
+                    nX -= dir[k][0];
+                    nY -= dir[k][1]; 
+                    //nDist -= 1? 
+
+                    if(visited[nX][nY][k] > nDist){
+                        visited[nX][nY][k] = nDist; 
+                        q.add(new Ball(nX,nY,nDist,new StringBuilder(nInst))); 
+                    }
                 }
             }
-
-            neighbors.add(new State(currRow, currCol, dist, direction)); 
         }
-        return neighbors; 
+
+        return answer.toString().equals("~") ? "impossible" : answer.toString();
     }
 }
