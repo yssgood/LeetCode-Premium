b961ac8f3c5cd077330580af2e69984f206da53c
유승선 <yssgood@naver.com>
2026-02-01 12:19:52 +0900

    Time: 1 ms (99.2%), Space: 45.6 MB (30.8%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,11 +1,18 @@
 class Solution {
     public int minimumCost(int[] nums) {
-        int answer = nums[0]; 
-        List<Integer> arrList = new ArrayList<>();
-        for(int i = 1; i < nums.length; i++) arrList.add(nums[i]); 
-        Collections.sort(arrList);
-        answer += arrList.get(0);
-        answer += arrList.get(1); 
-        return answer; 
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        
+        // nums[1:]에서 최소 2개 찾기
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] < min1) {
+                min2 = min1;  // 기존 min1을 min2로
+                min1 = nums[i];
+            } else if(nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+        
+        return nums[0] + min1 + min2;
     }
 }
