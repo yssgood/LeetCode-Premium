bb4c26a95335e2ef7e33e93b7d460cbc3822dfdc
유승선 <yssgood@naver.com>
2026-02-10 22:52:38 +0900

    Time: 0 ms (100%), Space: 44.1 MB (12.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int k = m + n - 1; 
-        m--; 
+        m--;
         n--; 
         while(m >= 0 && n >= 0){
             if(nums1[m] <= nums2[n]){
@@ -10,6 +10,7 @@ class Solution {
                 nums1[k--] = nums1[m--]; 
             }
         }
+
         while(n >= 0){
             nums1[k--] = nums2[n--]; 
         }
