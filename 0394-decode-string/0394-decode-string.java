28881035552f86843100d79d931a5239077030ef
유승선 <yssgood@naver.com>
2026-02-10 22:27:36 +0900

    Time: 1 ms (83.55%), Space: 42.7 MB (84.55%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,33 +1,38 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<StringBuilder> stringStack = new Stack<>(); 
-        StringBuilder curr = new StringBuilder(); 
+        Stack<StringBuilder> sbStack = new Stack<>(); 
+        Stack<Integer> numStack = new Stack<>(); 
         int num = 0; 
-        for(char c : s.toCharArray()){
-            if(Character.isDigit(c)){
-                num = num * 10 + (c - '0'); 
-            } else if(c == '['){
-                numStack.push(num); 
-                stringStack.push(curr); 
-                curr = new StringBuilder(); 
-                num = 0; 
-            } else if(c == ']'){
-                int numTimes = numStack.pop(); 
-                StringBuilder tmp = new StringBuilder(); 
-                for(int i = 0; i < numTimes; i++){
-                    tmp.append(curr.toString()); 
-                }
-                StringBuilder prev = stringStack.pop(); 
-                prev.append(tmp); 
-                curr = prev; 
-                //stringStack.push(curr); 
-
-            } else{
-                curr.append(c); 
+        int index = 0; 
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(); 
+        while(index < n){
+            
+            if(Character.isDigit(s.charAt(index))){
+                num = (num * 10) + s.charAt(index) - '0'; 
             }
+
+            if(Character.isAlphabetic(s.charAt(index))){
+                sb.append(s.charAt(index)); 
+            }
+
+            if(s.charAt(index) == '['){
+                sbStack.push(sb); 
+                numStack.push(num); 
+                num = 0; 
+                sb = new StringBuilder(); 
+            } else if(s.charAt(index) == ']'){
+                StringBuilder prev = sbStack.pop(); 
+                int numTmp = numStack.pop(); 
+                for(int i = 0; i < numTmp; i++){
+                    prev.append(sb); 
+                }
+                sb = prev; 
+            }
+
+            index++; 
         }
 
-        return curr.toString(); 
+        return sb.toString(); 
     }
 }
