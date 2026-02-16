91f21abca19b4ba9151da8da68a861fc072f4bc3
유승선 <yssgood@naver.com>
2026-02-16 14:15:59 +0900

    Time: 12 ms (65.07%), Space: 45.3 MB (10.53%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.java
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -1,27 +1,15 @@
 class Solution {
     public boolean validWordSquare(List<String> words) {
-        
-        for(int i = 0; i < words.size(); i++){
-            String row = words.get(i); 
-            StringBuilder sb = new StringBuilder(); 
-            for(int j = 0; j < words.get(i).length(); j++){
-                if (j >= words.size() || i >= words.get(j).length()) {
-                    return false;
-                }
-                sb.append(words.get(j).charAt(i)); 
+        int m = words.size();  
+        for(int i = 0; i < m; i++){
+            String original = words.get(i); 
+            StringBuilder tmp = new StringBuilder(); 
+            for(int j = 0; j < m; j++){
+                if(i >= words.get(j).length()) break; 
+                tmp.append(words.get(j).charAt(i)); 
             }
-            //System.out.println(sb); 
-            if(!row.equals(sb.toString())) return false; 
+            if(!original.equals(tmp.toString())) return false; 
         }
-
         return true; 
     }
 }
-
-/*
-["ball",
- "asee",
- "let",
- "lep"]
-
-*/
