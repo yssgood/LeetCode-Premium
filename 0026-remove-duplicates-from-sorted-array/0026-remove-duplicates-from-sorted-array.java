a1c7d6dd1c57f3bad74d260ab6a9f8a77551a8a7
유승선 <yssgood@naver.com>
2026-07-06 22:00:25 +0900

    Time: 1 ms (78.53%), Space: 46.7 MB (62.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 1; 
+        int index = 1; 
         for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[i-1]){
-                nums[k++] = nums[i]; 
-            }
+            if(nums[i] != nums[index-1]) nums[index++] = nums[i]; 
         }
-        return k; 
+        return index;
     }
 }
