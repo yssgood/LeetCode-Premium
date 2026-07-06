c825cce28ac0bb3a4c25c85132eae1ec4b97bf9a
유승선 <yssgood@naver.com>
2026-07-06 21:57:00 +0900

    Time: 0 ms (100%), Space: 43.7 MB (8.82%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,11 +1,9 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; 
+        int index = 0; 
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != val){
-                nums[k++] = nums[i]; 
-            }
+            if(nums[i] != val) nums[index++] = nums[i]; 
         }
-        return k; 
+        return index; 
     }
 }
