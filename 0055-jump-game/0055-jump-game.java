cdf4ef00ddb2d8c7bc5dbc184de8c0c47d72d15a
유승선 <yssgood@naver.com>
2026-07-11 13:50:15 +0900

    Time: 2 ms (88.98%), Space: 47.8 MB (80.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int needJump = nums.length-1; 
-
-        for(int i = nums.length-1; i >= 0; i--){
-            if(i + nums[i] >= needJump){
-                needJump = i; 
-            } 
-            
+        if(nums.length == 1) return true; 
+        int maxJump = nums[0];  
+        for(int i = 0; i <= maxJump; i++){
+            maxJump = Math.max(maxJump, i + nums[i]); 
+            if(maxJump >= nums.length-1) return true; 
         }
-
-        //2 3 1 1 4 
-        //0  1  2  3  4
-
-        //3 2 1 0 4 
-        //4 4 4 4 0
-        return needJump > 0 ? false : true; 
+        return false; 
     }
 }
