dcf0f13095d45152e49947986acbfbe1ec88c4c0
유승선 <yssgood@naver.com>
2026-07-12 19:50:05 +0900

    Time: 0 ms (100%), Space: 42.9 MB (90.78%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,47 +1,45 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> answer = new ArrayList<>();
-        int currLength = words[0].length();
-        int last = 0;
+        List<String> answer = new ArrayList<>(); 
+        int i = 0;
+        while(i < words.length){
+            StringBuilder sb = new StringBuilder(); 
+            int curr = 0; 
+            int j = i; 
 
-        for (int i = 1; i <= words.length; i++) {
-            // i == words.length이면 마지막 줄 강제 처리
-            if (i == words.length || currLength + 1 + words[i].length() > maxWidth) {
-                StringBuilder sb = new StringBuilder();
-                int numWords = i - last;
-                int totalSpaces = maxWidth - (currLength - (numWords - 1));
-                // currLength에는 단어사이 공백1씩 포함되어 있으므로 빼줌
+            while(j < words.length && curr + words[j].length() + (j - i) <= maxWidth){
+                curr += words[j].length(); 
+                j++; 
+            }
 
-                if (numWords == 1 || i == words.length) {
-                    // 단어 1개이거나 마지막 줄 → 왼쪽 정렬
-                    for (int j = last; j < i; j++) {
-                        if (j > last) sb.append(' ');
-                        sb.append(words[j]);
-                    }
-                    while (sb.length() < maxWidth) sb.append(' ');
-                } else {
-                    // 균등 분배
-                    int gaps = numWords - 1;
-                    int eachSpace = totalSpaces / gaps;
-                    int remain = totalSpaces % gaps;
+            int wordCount = j - i; 
+            int spaceNeeded = maxWidth - curr; 
 
-                    for (int j = last; j < i; j++) {
-                        sb.append(words[j]);
-                        if (j < i - 1) {
-                            int spaces = eachSpace + (j - last < remain ? 1 : 0);
-                            for (int k = 0; k < spaces; k++) sb.append(' ');
-                        }
+            if(j == words.length || wordCount == 1){
+                for(int k = i; k < j; k++){
+                    sb.append(words[k]); 
+                    if(k < j - 1) sb.append(" "); 
+                }
+                sb.append(" ".repeat(maxWidth - sb.length())); 
+            }
+            else{
+                int gaps = wordCount - 1; 
+                int base = spaceNeeded / gaps; 
+                int extra = spaceNeeded % gaps; 
+
+                for(int k = i; k < j; k++){
+                    sb.append(words[k]);
+                    if(k < j - 1){
+                        int space = base + (k - i < extra ? 1 : 0); 
+                        sb.append(" ".repeat(space)); 
                     }
                 }
-
-                answer.add(sb.toString());
-                last = i;
-                currLength = (i < words.length) ? words[i].length() : 0;
-            } else {
-                currLength += 1 + words[i].length();
             }
+
+            answer.add(sb.toString()); 
+            i = j; 
         }
 
-        return answer;
+        return answer; 
     }
 }
