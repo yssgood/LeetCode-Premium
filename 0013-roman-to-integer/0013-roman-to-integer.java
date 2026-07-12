0ac1986290b3691978ce641dcd5cb640f5d98be6
유승선 <yssgood@naver.com>
2026-07-12 16:24:52 +0900

    Time: 5 ms (33.61%), Space: 46.4 MB (75.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,39 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> hashMap = new HashMap<>();
-        hashMap.put('I',1);
-        hashMap.put('V',5);
-        hashMap.put('X',10); 
-        hashMap.put('L',50);
-        hashMap.put('C',100);
-        hashMap.put('D',500);
-        hashMap.put('M',1000); 
+        Map<Character,Integer> hashMap = Map.of('I',1, 'V',5, 'X',10, 'L',50, 'C',100, 'D',500, 'M',1000); 
+        int answer = hashMap.get(s.charAt(0)); 
 
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add("IV");
-        hashSet.add("IX");
-        hashSet.add("XL");
-        hashSet.add("XC");
-        hashSet.add("CD");
-        hashSet.add("CM"); 
-        int answer = 0; 
+        for(int i = 1; i < s.length(); i++){
+            int lastChar = hashMap.get(s.charAt(i-1)); 
+            int currChar = hashMap.get(s.charAt(i)); 
 
-        for(int i = 0; i < s.length();){
-            if(i < s.length() - 1){
-                String sub = s.substring(i,i+2);
-                if(hashSet.contains(sub)){
-                    //System.out.println(sub); 
-                    answer += hashMap.get(s.charAt(i+1));
-                    answer -= hashMap.get(s.charAt(i)); 
-                    i += 2;
-                    continue; 
-                } 
-            } 
-            answer += hashMap.get(s.charAt(i)); 
-            //System.out.println(answer); 
-            i++; 
+            if(lastChar < currChar){
+                answer -= lastChar; 
+                currChar -= lastChar; 
+            }
+            
+            answer += currChar; 
         }
+        
 
-        return answer;
+        return answer; 
     }
 }
