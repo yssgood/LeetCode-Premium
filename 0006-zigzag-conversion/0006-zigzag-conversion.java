91ab8a1c5dd1cb9676833f1f7046477b4a517e77
유승선 <yssgood@naver.com>
2026-07-12 19:23:13 +0900

    Time: 6 ms (30.5%), Space: 47.4 MB (13.19%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,32 +1,33 @@
 class Solution {
     public String convert(String s, int numRows) {
-        Map<Integer, StringBuilder> hashMap = new HashMap<>(); 
-        int row = 0; 
-        boolean isEnd = false; 
+        if(numRows == 1) return s; 
+        StringBuilder[] sbArray = new StringBuilder[numRows]; 
+        for(int i = 0; i < numRows; i++){
+            sbArray[i] = new StringBuilder(); 
+        }
+
+        int curr = 0; 
+        int dir = 1; 
         for(char c : s.toCharArray()){
-            hashMap.computeIfAbsent(row, k -> new StringBuilder()).append(c); 
+            sbArray[curr].append(c); 
             
-            if(!isEnd){
-                row++; 
-            } else{
-                row--; 
-            }
-
-            if(row >= numRows){
-                row-=2; 
-                isEnd = !isEnd; 
-            } else if(row < 0){
-                row+=2;
-                isEnd = !isEnd; 
+            if(dir == 1){
+                if(curr + 1 >= numRows){
+                    curr--; 
+                    dir = -1; 
+                } else{
+                    curr++; 
+                }
+            } else if(dir == -1){
+                if(curr - 1 < 0){
+                    curr++; 
+                    dir = 1; 
+                } else{
+                    curr--; 
+                }
             }
         }
-
-
-        StringBuilder answer = new StringBuilder(); 
-        for(Map.Entry<Integer,StringBuilder> entry : hashMap.entrySet()){
-            answer.append(entry.getValue()); 
-        }
-
-        return answer.toString(); 
+        //System.out.print(Arrays.toString(sbArray)); 
+        return String.join("", sbArray); 
     }
 }
