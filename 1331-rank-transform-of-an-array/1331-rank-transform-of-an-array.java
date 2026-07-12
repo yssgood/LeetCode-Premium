acbf380be46ba3074f0ac3063e71d65af1463909
유승선 <yssgood@naver.com>
2026-07-12 10:05:38 +0900

    Time: 62 ms (9.85%), Space: 75.4 MB (57.67%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] copy = new int[arr.length]; 
-        int[] answer = new int[arr.length]; 
-        HashMap<Integer,Integer> hashMap = new HashMap<>(); 
-        for(int i = 0; i < arr.length; i++) copy[i] = arr[i]; 
-        Arrays.sort(copy); 
-        int count = 1; 
-        for(int i = 0; i < arr.length; i++){
-            if(!hashMap.containsKey(copy[i])){
-                hashMap.put(copy[i],count++); 
+        if(arr.length == 0) return arr; 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        Map<Integer,Integer> hashMap = new HashMap<>(); 
+
+        for(int n : arr) pq.offer(n); 
+
+        int rank = 1; 
+        int lastNum = pq.peek(); 
+
+        while(!pq.isEmpty()){
+            int curr = pq.poll(); 
+            if(curr != lastNum){
+                rank++; 
+                lastNum = curr; 
             }
+            hashMap.put(curr,rank); 
         }
 
+        int[] answer = new int[arr.length]; 
         for(int i = 0; i < arr.length; i++){
             answer[i] = hashMap.get(arr[i]); 
         }
