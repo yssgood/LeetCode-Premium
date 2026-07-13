9ae33693d7e360f66cef0d6687908de81e2070d4
유승선 <yssgood@naver.com>
2026-07-13 22:54:25 +0900

    Time: 0 ms (100%), Space: 43.3 MB (27.87%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,32 +1,36 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
         int m = board.length, n = board[0].length; 
+        int[][] dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}}; 
+
+
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                int liveCells = 0; 
-                for(int[] d : dir){
-                    int nX = d[0] + i; 
-                    int nY = d[1] + j; 
+                
+                int count = 0; 
 
-                    if(nX >= 0 && nY >= 0 && nX < m && nY < n){
-                        if(board[nX][nY] >= 1) liveCells++; 
-                    }
+                for(int k = 0; k < 8; k++){
+                    int nX = i + dir[k][0]; 
+                    int nY = j + dir[k][1]; 
+
+                    if(nX < 0 || nY < 0 || nX >= m || nY >= n) continue; 
+
+                    if(board[nX][nY] > 0) count++; 
                 }
 
-                if(board[i][j] == 1){
-                    if(liveCells < 2 || liveCells > 3) board[i][j] = 2; 
-                }
                 if(board[i][j] == 0){
-                    if(liveCells == 3) board[i][j] = -1; 
+                    if(count == 3) board[i][j] = -1; 
+                }
+                if(board[i][j] > 0){
+                    if(count < 2 || count > 3) board[i][j] = 2; 
                 }
             }
         }
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == 2) board[i][j] = 0; 
                 if(board[i][j] == -1) board[i][j] = 1; 
+                if(board[i][j] == 2) board[i][j] = 0; 
             }
         }
     }
