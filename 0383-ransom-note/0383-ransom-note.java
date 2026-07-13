6626c3240c1ef3deaf3b2eda6079498277ef03c3
유승선 <yssgood@naver.com>
2026-07-13 23:17:14 +0900

    Time: 1 ms (99.98%), Space: 46.2 MB (79.79%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer> hashMap = new HashMap<>();
-        for(char c : ransomNote.toCharArray()){
-            hashMap.put(c, hashMap.getOrDefault(c,0) + 1);
-        }
+        int[] count = new int[26]; 
         for(char c : magazine.toCharArray()){
-            if(hashMap.containsKey(c)) hashMap.put(c, hashMap.get(c) - 1); 
+            count[c - 'a']++; 
         }
-        for(var e : hashMap.entrySet()){
-            if(e.getValue() > 0) return false; 
+        for(char c : ransomNote.toCharArray()){
+            if(--count[c - 'a'] < 0) return false; 
         }
         return true; 
     }
