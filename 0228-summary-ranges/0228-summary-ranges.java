30edfd47e941d4b13a439a94306d13c26ef83473
유승선 <yssgood@naver.com>
2026-07-14 21:26:26 +0900

    Time: 0 ms (100%), Space: 42.6 MB (98.31%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> answer = new ArrayList<>(); 
-        int end = 0; 
+        List<String> answer = new ArrayList<>();
+        int start = 0, end = 0; 
+        StringBuilder sb = new StringBuilder(); 
         while(end < nums.length){
             int curr = nums[end]; 
-            int start = nums[end]; 
-            while(end + 1 < nums.length && nums[end+1] - curr == 1){
-                curr = nums[end+1];
-                end++;  
+            int last = nums[end]; 
+            while(end + 1 < nums.length && nums[end+1] - nums[end] == 1){
+                end++; 
+                last = nums[end]; 
             }
 
-            if(start == nums[end]){
-                answer.add(String.valueOf(nums[end])); 
+            if(curr != last){
+                sb.append(Integer.toString(curr)).append("->").append(Integer.toString(last));
             } else{
-                answer.add(String.valueOf(start) + "->" + String.valueOf(curr)); 
+                sb.append(Integer.toString(curr)); 
             }
 
+            answer.add(sb.toString()); 
+            sb = new StringBuilder(); 
             end++; 
         }
         return answer; 
