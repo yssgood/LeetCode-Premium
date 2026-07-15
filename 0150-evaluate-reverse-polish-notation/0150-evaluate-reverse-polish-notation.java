c7c304cacc8c6f865f433eff79154183ddeb3caf
유승선 <yssgood@naver.com>
2026-07-15 20:21:29 +0900

    Time: 8 ms (11.36%), Space: 45.1 MB (79.22%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,34 +2,21 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>(); 
         for(String s : tokens){
+            int result = 0; 
             if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")){
-                int num1 = stack.pop(); 
-                int num2 = stack.pop(); 
+                int right = stack.pop(); 
+                int left = stack.pop(); 
 
-                int result = 0; 
-                if(s.equals("+")){
-                    result = num1 + num2; 
-                } else if(s.equals("-")){
-                    result = num2 - num1; 
-                } else if(s.equals("*")){
-                    result = num1 * num2; 
-                } else{
-                    result = num2 / num1; 
-                }
-
-                //System.out.println(result); 
-
-                stack.add(result); 
-                
+                if(s.equals("+")) result = left + right; 
+                if(s.equals("-")) result = left - right; 
+                if(s.equals("*")) result = left * right; 
+                if(s.equals("/")) result = left / right; 
             } else{
-                if(s.charAt(0) ==  '-'){
-                    stack.add(Integer.parseInt(s.substring(1)) * -1);  
-                } else{
-                    stack.add(Integer.parseInt(s)); 
-                }
+                result = Integer.parseInt(s); 
             }
-        }
 
+            stack.push(result); 
+        }
         return stack.pop(); 
     }
 }
