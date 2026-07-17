18e479ba4dedccb5fc80c41237e5d70900a30102
유승선 <yssgood@naver.com>
2026-07-17 18:01:10 +0900

    Time: 1 ms (4.32%), Space: 46.8 MB (14.22%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,37 +1,43 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null) return null; 
+        Map<Integer, Node> hashMap = new HashMap<>();      // 인덱스 → 복사노드
+        Map<Integer, Node> randomMap = new HashMap<>();    // 인덱스 → 원본random
+        Map<Node, Integer> indexMap = new HashMap<>();     // 원본노드 → 인덱스 (추가!)
 
-        Map<Node,Node> hashMap = new HashMap<>(); 
-        Node curr = head; 
+        Node curr = head;
+        int index = 0;
 
-        while(curr != null){
-            hashMap.put(curr, new Node(curr.val)); 
-            curr = curr.next; 
+        while (curr != null) {
+            Node copy = new Node(curr.val);
+            hashMap.put(index, copy);
+            randomMap.put(index, curr.random);
+            indexMap.put(curr, index);      // 원본 노드의 위치 기록
+            index++;
+            curr = curr.next;
         }
 
-        curr = head; 
-        while(curr != null){
-            hashMap.get(curr).next = hashMap.get(curr.next); 
-            hashMap.get(curr).random = hashMap.get(curr.random); 
-            curr = curr.next; 
+        Node dummy = new Node(-1);
+        Node dummyCopy = dummy;
+
+        for (int i = 0; i < index; i++) {
+            Node next = null;
+            Node currNode = hashMap.get(i);
+            Node random = randomMap.get(i);
+
+            if (hashMap.containsKey(i + 1)) {
+                next = hashMap.get(i + 1);        // i → i+1 로 수정
+            }
+
+            if (random != null) {
+                random = hashMap.get(indexMap.get(random));   // 새 노드 X, 대응 복사본
+            }
+
+            currNode.next = next;
+            currNode.random = random;
+            dummyCopy.next = currNode;
+            dummyCopy = dummyCopy.next;
         }
 
-        return hashMap.get(head); 
+        return dummy.next;
     }
 }
