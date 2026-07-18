101a26d48e7ead38e55eb10de04bc6153720b4e3
유승선 <yssgood@naver.com>
2026-07-18 23:56:13 +0900

    Time: 47 ms (36.59%), Space: 46 MB (83.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,18 +3,16 @@ class Solution {
         int n = nums.length; 
         int[] dp = new int[n]; 
         Arrays.fill(dp,1); 
-
-        int answer = 1; 
-
-        for(int i = 0; i < n; i++){
+        int max_ = 1; 
+        for(int i = 1; i < n; i++){
             for(int j = 0; j < i; j++){
                 if(nums[j] < nums[i]){
                     dp[i] = Math.max(dp[i], dp[j] + 1); 
-                    answer = Math.max(answer, dp[i]); 
-                } 
+                    max_ = Math.max(dp[i], max_); 
+                }
             }
         }
 
-        return answer; 
+        return max_; 
     }
 }
