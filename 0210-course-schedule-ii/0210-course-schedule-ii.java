de83bd5e768e4e2e6aac6b5056b7bb2322d618b4
유승선 <yssgood@naver.com>
2026-07-18 20:17:37 +0900

    Time: 8 ms (17.04%), Space: 47.5 MB (23.76%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,38 +1,39 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer> answer = new ArrayList<>(); 
+        int[] inOrder = new int[numCourses]; 
         List<List<Integer>> adj = new ArrayList<>(); 
         for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>()); 
         }
 
-        int[] indegree = new int[numCourses]; 
+        for(int[] next : prerequisites){
+            int to = next[0]; 
+            int from = next[1]; 
 
-        for(int[] p : prerequisites){
-            int to = p[0];
-            int from = p[1]; 
-
-            adj.get(from).add(to);
-            indegree[to]++; 
+            inOrder[to]++; 
+            adj.get(from).add(to); 
         }
 
         Queue<Integer> q = new LinkedList<>(); 
 
         for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0){
-                q.add(i); 
+            if(inOrder[i] == 0){
+                q.add(i);
             }
         }
 
         if(q.isEmpty()) return new int[]{}; 
-        List<Integer> answer = new ArrayList<>(); 
+
+
         while(!q.isEmpty()){
             int size = q.size(); 
             for(int i = 0; i < size; i++){
-                int curr = q.poll(); 
-                answer.add(curr); 
-                for(int next : adj.get(curr)){
-                    indegree[next]--; 
-                    if(indegree[next] == 0){
+                int node = q.poll();
+                answer.add(node); 
+                for(int next : adj.get(node)){
+                    inOrder[next]--; 
+                    if(inOrder[next] == 0){
                         q.add(next); 
                     }
                 }
@@ -40,8 +41,7 @@ class Solution {
         }
 
         if(answer.size() != numCourses) return new int[]{}; 
-
-
-        return answer.stream().mapToInt(Integer::intValue).toArray(); 
+        
+        return answer.stream().mapToInt(i -> i).toArray(); 
     }
 }
