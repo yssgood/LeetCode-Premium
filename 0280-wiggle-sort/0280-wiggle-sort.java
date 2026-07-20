2b413d117b0390a33bfcd94468ae4cb2f42fb274
유승선 <yssgood@naver.com>
2026-07-20 22:10:13 +0900

    Time: 6 ms (34.39%), Space: 47.8 MB (19.75%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.java
+++ 0280-wiggle-sort/0280-wiggle-sort.java
@@ -1,16 +1,15 @@
 class Solution {
-    public void swap(int[] nums, int i, int j){
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp; 
-    }
     public void wiggleSort(int[] nums) {
-        Arrays.sort(nums); 
-        for(int i = 1; i < nums.length-1; i += 2){
-            swap(nums,i,i+1); 
-        }
-
         //1 2 3 4 5 6 
         //1 3 2 5 4 6
+
+        Arrays.sort(nums); 
+        for(int i = 2; i < nums.length; i+=2){
+            int tmp = nums[i-1]; 
+            nums[i-1] = nums[i]; 
+            nums[i] = tmp; 
+        }
     }
+
+
 }
