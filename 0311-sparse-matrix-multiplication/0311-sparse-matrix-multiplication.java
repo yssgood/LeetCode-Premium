f4e679e6b56e5a616ba0b3d0698f713a3ba335a0
유승선 <yssgood@naver.com>
2026-07-25 12:56:26 +0900

    Time: 3 ms (31.15%), Space: 45.3 MB (97.27%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
@@ -1,16 +1,20 @@
 class Solution {
     public int[][] multiply(int[][] mat1, int[][] mat2) {
-        int[][] answer = new int[mat1.length][mat2[0].length]; 
+        int m = mat1.length, k = mat1[0].length;
+        int n = mat2[0].length;
 
-        for(int i = 0; i < mat1.length; i++){
-            for(int j = 0; j < mat2[0].length; j++){
-                int sum = 0; 
-                for(int k = 0; k < mat1[0].length; k++){
-                    sum += mat1[i][k] * mat2[k][j]; 
+        int[][] answer = new int[m][n];   // m × n
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {        // 결과 열: 0~n
+                int sum = 0;
+                for (int l = 0; l < k; l++) {    // 내적 인덱스: 0~k
+                    sum += mat1[i][l] * mat2[l][j];
                 }
-                answer[i][j] = sum; 
+                answer[i][j] = sum;
             }
         }
-        return answer; 
+
+        return answer;
     }
 }
