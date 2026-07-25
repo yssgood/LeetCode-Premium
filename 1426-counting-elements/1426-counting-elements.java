a9abb149fe80e3cd99de94b9d2030a8e6ad99346
유승선 <yssgood@naver.com>
2026-07-25 11:56:16 +0900

    Time: 3 ms (12.18%), Space: 43.7 MB (5.45%) - LeetHub

--- 1426-counting-elements/1426-counting-elements.java
+++ 1426-counting-elements/1426-counting-elements.java
@@ -2,12 +2,10 @@ class Solution {
     public int countElements(int[] arr) {
         Map<Integer,Integer> hashMap = new HashMap<>(); 
         int answer = 0; 
-        for(int i = 0; i < arr.length; i++){
-            hashMap.merge(arr[i],1,Integer::sum); 
-        }
+        for(int n : arr) hashMap.merge(n,1,Integer::sum); 
         for(var e : hashMap.entrySet()){
-            if(hashMap.containsKey(e.getKey()-1)){
-                answer += hashMap.get(e.getKey()-1); 
+            if(hashMap.containsKey(e.getKey() + 1)){
+                answer += e.getValue(); 
             }
         }
         return answer; 
