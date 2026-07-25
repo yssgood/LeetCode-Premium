17e8c1321c79a3de1be99f642272394680ff6f9f
유승선 <yssgood@naver.com>
2026-07-25 11:47:01 +0900

    Time: 12 ms (5.68%), Space: 52.3 MB (6.11%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.java
+++ 0249-group-shifted-strings/0249-group-shifted-strings.java
@@ -1,38 +1,27 @@
 class Solution {
-    String convert(String s){
-        StringBuilder sb = new StringBuilder(); 
-        sb.append('0'); 
-        for(int i = 1; i < s.length(); i++){
-            //Character tmp = (char)(((s.charAt(i) - s.charAt(i-1) + 26) % 26) + '0'); 
-            //char tmp = (char)(((s.charAt(i) - s.charAt(i - 1) + 26) % 26) + '0');
-            int diff = s.charAt(i) - s.charAt(i-1) < 0 ? (s.charAt(i) - s.charAt(i-1)) + 26 : s.charAt(i) - s.charAt(i-1); 
-            
-            sb.append((char)(diff + '0')); 
-        }
-        return sb.toString(); 
-    }
     public List<List<String>> groupStrings(String[] strings) {
         List<List<String>> answer = new ArrayList<>(); 
-        HashMap<String,List<String>> hashMap = new HashMap<>(); 
+        Map<String,List<String>> hashMap = new HashMap<>();
+        
         for(String s : strings){
-            String pattern = convert(s); 
-            //System.out.println(pattern); 
-            if(hashMap.containsKey(pattern)){
-                List<String> tmp = hashMap.get(pattern); 
-                tmp.add(s); 
-                hashMap.put(pattern, tmp); 
-            } else{
-                List<String> ls = new ArrayList<>(); 
-                ls.add(s); 
-                hashMap.put(pattern, ls); 
-            }
+            String key = convert(s); 
+            hashMap.computeIfAbsent(key, k -> new ArrayList<>()).add(s); 
         }
 
-        for(String s : hashMap.keySet()){
-            List<String> ls = hashMap.get(s); 
-            answer.add(ls); 
+        for(var e : hashMap.entrySet()){
+            answer.add(e.getValue()); 
         }
 
         return answer; 
+    }
+
+    public String convert(String s){
+        String key = "1"; 
+        for(int i = 1; i < s.length(); i++){
+            int diff = (s.charAt(i) - s.charAt(i-1) + 26) % 26;   // 이 줄만 변경
+            key += String.valueOf(diff) + ","; 
+        }
+        //System.out.println(key); 
+        return key; 
     }
 }
