0d527fb7fade4435a69b3686e7ee84b53ae981b7
유승선 <yssgood@naver.com>
2026-07-25 12:11:18 +0900

    Time: 33 ms (5.05%), Space: 47.7 MB (8.08%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.java
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean validWordSquare(List<String> words) {
-        int m = words.size();  
-        for(int i = 0; i < m; i++){
-            String original = words.get(i); 
-            StringBuilder tmp = new StringBuilder(); 
-            for(int j = 0; j < m; j++){
+        int n = words.size(); 
+        for(int i = 0; i < n; i++){
+            String row = words.get(i); 
+            String col = ""; 
+            for(int j = 0; j < n; j++){
                 if(i >= words.get(j).length()) break; 
-                tmp.append(words.get(j).charAt(i)); 
+                col += words.get(j).charAt(i); 
             }
-            if(!original.equals(tmp.toString())) return false; 
+            if(!row.equals(col)) return false; 
         }
         return true; 
     }
