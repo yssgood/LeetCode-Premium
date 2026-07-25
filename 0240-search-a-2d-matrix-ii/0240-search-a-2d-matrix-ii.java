bc2ba8c17affaff89d25ca526d9e0a04df687198
유승선 <yssgood@naver.com>
2026-07-25 15:21:04 +0900

    Time: 3 ms (89.1%), Space: 48.1 MB (64.89%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i = 0; i < matrix.length; i++){
-            int left = 0, right = matrix[i].length-1; 
-            if(matrix[i][left] <= target && matrix[i][right] >= target){
+        int m = matrix.length, n = matrix[0].length; 
+        for(int i = 0; i < m; i++){
+            int[] candidate = matrix[i]; 
+            if(candidate[0] <= target && candidate[candidate.length-1] >= target){
+                int left = 0, right = candidate.length-1; 
                 while(left <= right){
-                    int mid = (right + left) / 2; 
+                    int mid = left + (right - left) / 2; 
+                    if(candidate[mid] == target) return true; 
 
-                    if(matrix[i][mid] == target){
-                        return true; 
-                    }
-
-                    if(matrix[i][mid] > target){
-                        right = mid - 1; 
-                    } else{
-                        left = mid + 1; 
-                    }
+                    if(candidate[mid] > target) right = mid - 1;
+                    else left = mid + 1; 
                 }
             }
         }
