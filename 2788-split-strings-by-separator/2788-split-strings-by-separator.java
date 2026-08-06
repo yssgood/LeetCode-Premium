c131a0c1786d24e6701851d387ee0933cc2f5be5
유승선 <yssgood@naver.com>
2026-08-06 22:31:43 +0900

    Time: 9 ms (61.23%), Space: 47.5 MB (52.97%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.java
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.java
@@ -1,20 +1,10 @@
 class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
         List<String> answer = new ArrayList<>(); 
-        for(String word : words){
-            StringBuilder sb = new StringBuilder(); 
-            for(char c : word.toCharArray()){
-                if(c != separator){
-                    sb.append(c); 
-                } else{
-                    if(sb.length() > 0) answer.add(sb.toString()); 
-                    sb = new StringBuilder(); 
-                }
-            }
-
-            if(sb.length() > 0) answer.add(sb.toString()); 
+        for(String s : words){
+            String[] arr = s.split("\\"+separator); 
+            for(String ss : arr) if(!ss.isEmpty()) answer.add(ss); 
         }
-
         return answer; 
     }
 }
