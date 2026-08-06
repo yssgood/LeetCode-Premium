492eb1ee9093032ab0749667e4abe316cdbfa9c3
유승선 <yssgood@naver.com>
2026-08-06 22:50:35 +0900

    Time: 1 ms (97.48%), Space: 47 MB (96.93%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,17 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int index = Integer.MAX_VALUE; 
         int[] prefix = new int[nums.length]; 
+
         for(int i = 1; i < nums.length; i++){
             prefix[i] = prefix[i-1] + nums[i-1]; 
         }
+
         int suffix = 0; 
-        int answer = -1; 
+
         for(int i = nums.length-1; i >= 0; i--){
-            if(prefix[i] == suffix){
-                answer = i; 
-            }
+            if(prefix[i] == suffix) index = Math.min(index, i); 
             suffix += nums[i]; 
         }
-        return answer; 
+
+        return index == Integer.MAX_VALUE ? -1 : index; 
     }
 }
