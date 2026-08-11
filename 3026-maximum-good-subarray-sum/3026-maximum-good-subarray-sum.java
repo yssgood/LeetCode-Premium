c8ef5a7fa0e284f7bc48bdaffb27e0b3179aeb3b
유승선 <yssgood@naver.com>
2026-08-11 22:27:46 +0900

    Time: 71 ms (65.66%), Space: 98.5 MB (21.05%) - LeetHub

--- 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.java
+++ 3026-maximum-good-subarray-sum/3026-maximum-good-subarray-sum.java
@@ -1,21 +1,25 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long preFix = 0; 
-        long max_ = Long.MIN_VALUE;  
+        long answer = Long.MIN_VALUE; 
         Map<Integer,Long> hashMap = new HashMap<>(); 
-        for(int i = 0; i < nums.length; i++){
-            preFix += nums[i]; 
 
-            if(hashMap.containsKey(nums[i] + k)){
-                max_ = Math.max(max_, preFix - hashMap.get(nums[i] + k)); 
-            }
-            if(hashMap.containsKey(nums[i] - k)){
-                max_ = Math.max(max_, preFix - hashMap.get(nums[i] - k));
+        long currSum = 0; 
+        for(int n : nums){
+            int target = n - k; 
+            if(hashMap.containsKey(target)){
+                //System.out.println(hashMap.get(target)); 
+                answer = Math.max(answer, (currSum + n) - hashMap.get(target)); 
             }
 
-            hashMap.put(nums[i], Math.min(hashMap.getOrDefault(nums[i], Long.MAX_VALUE), preFix - nums[i])); 
+            target = n + k; 
+            if(hashMap.containsKey(target)){
+                //System.out.println(hashMap.get(target)); 
+                answer = Math.max(answer, (currSum + n) - hashMap.get(target)); 
+            }
+
+            hashMap.put(n, Math.min(hashMap.getOrDefault(n,Long.MAX_VALUE),currSum)); 
+            currSum += n; 
         }
-
-        return max_ == Long.MIN_VALUE ? 0 : max_; 
+        return answer == Long.MIN_VALUE ? 0 : answer; 
     }
 }
