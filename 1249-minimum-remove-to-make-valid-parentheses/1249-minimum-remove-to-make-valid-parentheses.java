64cef8c8900c22e63398d5f5a6bfb94449e84661
유승선 <yssgood@naver.com>
2026-08-13 21:44:06 +0900

    Time: 571 ms (5.04%), Space: 48.4 MB (7.3%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,31 +1,28 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         Deque<Integer> stack = new ArrayDeque<>(); 
-        StringBuilder sb = new StringBuilder(s); 
+        Set<Integer> banned = new HashSet<>(); 
         for(int i = 0; i < s.length(); i++){
-            char curr = sb.charAt(i); 
+            char curr = s.charAt(i); 
             if(curr == ')'){
-                if(!stack.isEmpty()){
-                    stack.pop(); 
-                } else{
-                    sb.setCharAt(i,'?'); 
-                }
-            }
-            if(curr == '('){
+                if(!stack.isEmpty()) stack.pop(); 
+                else banned.add(i); 
+            } else if(curr == '('){
                 stack.push(i); 
             }
         }
 
         while(!stack.isEmpty()){
-            sb.setCharAt(stack.pop(),'?'); 
+            banned.add(stack.pop()); 
         }
 
-        for(int i = sb.length() - 1; i >= 0; i--){
-            if(sb.charAt(i) == '?') sb.deleteCharAt(i); 
+        String answer = ""; 
+        for(int i = 0; i < s.length(); i++){
+            if(!banned.contains(i)){
+                answer += s.charAt(i); 
+            }
         }
 
-        
-
-        return sb.toString(); 
+        return answer; 
     }
 }
