595843e8599fcea18a949c22a40f36b3e0494344
유승선 <yssgood@naver.com>
2026-08-13 22:05:39 +0900

    Time: 7 ms (99.49%), Space: 47.2 MB (45.29%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,54 +1,59 @@
 class Solution {
     int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}}; 
+    int N = 0; 
+    public void dfs(int[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid.length || grid[i][j] != 1) return; 
+        grid[i][j] = 2; 
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1); 
+    }
     public int shortestBridge(int[][] grid) {
-        int n = grid.length; 
-        Queue<int[]> q = new LinkedList<>(); 
-        boolean found = false; 
-        for(int i = 0; i < n && !found; i++){
-            for(int j = 0; j < n && !found; j++){
+        Deque<int[]> q = new ArrayDeque<>(); 
+        boolean flag = false; 
+        N = grid.length; 
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
                 if(grid[i][j] == 1){
-                    dfs(grid,i,j,q); 
-                    found = true; 
+                    dfs(grid,i,j); 
+                    flag = true;
+                    break; 
+                }
+            }
+            if(flag) break; 
+        }
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == 1){
+                    q.add(new int[]{i,j}); 
+                    grid[i][j] = -1; 
                 }
             }
         }
 
         int answer = 0; 
         while(!q.isEmpty()){
-            int size = q.size();
+            int size = q.size(); 
             for(int i = 0; i < size; i++){
                 int[] curr = q.poll(); 
-                int x = curr[0], y = curr[1], dist = curr[2]; 
-                if(grid[x][y] == 1) return dist; 
-                //grid[x][y] = 2; 
-                System.out.println(x + " " + y); 
+                int x=  curr[0], y = curr[1]; 
 
                 for(int[] d : dir){
                     int nX = x + d[0]; 
                     int nY = y + d[1]; 
-            
-
-                    if(nX >= 0 && nY >= 0 && nX < n && nY < n && grid[nX][nY] != 2){
-                        if(grid[nX][nY] == 1) return dist;
-                        
-                        grid[nX][nY] = 2; 
-                        q.offer(new int[]{nX,nY,dist+1}); 
+                    if(nX >= 0 && nY >= 0 && nX < N && nY < N && grid[nX][nY] != -1){
+                        if(grid[nX][nY] == 2) return answer; 
+                        q.add(new int[]{nX,nY}); 
+                        grid[nX][nY] = -1; 
                     }
                 }
-
             }
+            answer++; 
         }
 
-        return -1; 
-    }
-    void dfs(int[][] grid, int i, int j, Queue<int[]> q){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1) return; 
-        grid[i][j] = 2; 
-        q.offer(new int[]{i,j,0}); 
 
-        dfs(grid,i+1,j,q);
-        dfs(grid,i-1,j,q);
-        dfs(grid,i,j+1,q);
-        dfs(grid,i,j-1,q); 
+        return -1;
     }
 }
